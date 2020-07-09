9fec5756f1aced719491482f75c31f07dd43ce81
Min-Ho <zocracker@icloud.com>
2020-07-09 21:13:35 +0900

    키에러 전까지 모든 사항 수정 완료

--- students/10th/minholee/mysite/signup/models.py
+++ students/10th/minholee/mysite/signup/models.py
@@ -8,3 +8,6 @@ class Users(models.Model):
     password   = models.CharField(max_length = 200)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        db_table = 'users'
