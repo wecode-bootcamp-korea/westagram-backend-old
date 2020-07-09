7668c5dd3441d0ae6d278d76ea8fe713e631e8a5
Do Hwna Kim <ehghkswodnjs@naver.com>
2020-07-09 13:30:29 +0900

    필드명을 userid-> user로 변경

--- student/10th/kimdohwan/westagram/postings/models.py
+++ student/10th/kimdohwan/westagram/postings/models.py
@@ -3,7 +3,7 @@ from core import models as core_models
 
 
 class Posting(core_models.TempDate):
-    userid = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
     title = models.CharField(max_length=100, default="")
     content = models.CharField(max_length=1000, default="")
 
@@ -13,9 +13,8 @@ class Posting(core_models.TempDate):
     def __str__(self):
         return self.title
 
-
 class Comment(core_models.TempDate):
-    userid = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
     posting = models.ForeignKey(Posting, on_delete=models.CASCADE)
     content = models.TextField(max_length=300, default="")
 
@@ -25,8 +24,13 @@ class Comment(core_models.TempDate):
     def __str__(self):
         return self.userid
 
-
 class Love(core_models.TempDate):
     userid = models.ForeignKey("user.User", on_delete=models.CASCADE)
     posting = models.ForeignKey(Posting, on_delete=models.CASCADE)
     loveTF = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "love"
+
+    def __str__(self):
+        return self.posting
--- student/10th/kimdohwan/westagram/user/models.py
+++ student/10th/kimdohwan/westagram/user/models.py
@@ -13,8 +13,3 @@ class User(core_models.TempDate):
 
     def __str__(self):
         return self.name
-
-
-# select * from user
-# insert into user_user(name,email,password,created_at,updated_at) values("김도환","ehghksvjscl@nate.com","admin","2020-10-10","2020-10-10")
-# http -v http://127.0.0.1:8000/ userid="ehghks1" name="김도환" email="ehghks1@naver.com" password="1234"
