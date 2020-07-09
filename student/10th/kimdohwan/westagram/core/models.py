5b8671184fd87986a62ebe55f3d3f94db14827ca
Do Hwna Kim <ehghkswodnjs@naver.com>
2020-07-09 20:23:31 +0900

    줄간격 1칸 수정

--- student/10th/kimdohwan/westagram/core/models.py
+++ student/10th/kimdohwan/westagram/core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class TempDate(models.Model):
     created = models.DateField(auto_now_add=True, null=True)
     updated = models.DateField(auto_now=True, null=True)
