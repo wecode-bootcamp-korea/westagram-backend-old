13a2809a2eed1baa01c905d107b9f58cbb90cff0
Do Hwna Kim <ehghkswodnjs@naver.com>
2020-07-09 21:38:20 +0900

    URL PATH UPDATE

--- student/10th/kimdohwan/westagram/config/urls.py
+++ student/10th/kimdohwan/westagram/config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("user/", include("user.urls")),
-    path("post/", include("postings.urls")),
+    path("user", include("user.urls")),
+    path("post", include("postings.urls")),
 ]
