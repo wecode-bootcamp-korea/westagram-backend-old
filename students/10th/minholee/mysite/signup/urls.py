d104c9244c82bac8da18a785b65ba140f096f50b
Min-Ho <zocracker@icloud.com>
2020-07-09 23:05:11 +0900

    진짜 진짜 마지막 수정입니다.

--- students/10th/minholee/mysite/signup/urls.py
+++ students/10th/minholee/mysite/signup/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SignUp, SignIn
 
 urlpatterns = [
-    path('/signup', SignUp.as_view()),
-    path('/signin', SignIn.as_view())
+    path('signup', SignUp.as_view()),
+    path('signin', SignIn.as_view())
 ]
