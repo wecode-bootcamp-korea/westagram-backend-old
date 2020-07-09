c037d465d2526acf16eecaecbc8ae7a390317ce4
Min-Ho <zocracker@icloud.com>
2020-07-09 22:42:09 +0900

    url 에러 수정입니다.

--- students/10th/minholee/mysite/mysite/urls.py
+++ students/10th/minholee/mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("signup.urls")),
+    path("user", include("signup.urls")),
     path('admin/', admin.site.urls),
 ]
