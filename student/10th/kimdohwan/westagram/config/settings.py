5222ab1d1500106168938332fd3c2cddbb2a818b
Do Hwna Kim <ehghkswodnjs@naver.com>
2020-07-10 17:50:12 +0900

    settings 파일 주석 처리 완료

--- student/10th/kimdohwan/westagram/config/settings.py
+++ student/10th/kimdohwan/westagram/config/settings.py
@@ -77,12 +77,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = DBConnent.DATABASES
 
 # Password validation
