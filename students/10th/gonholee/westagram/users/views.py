8fd35c7a66cfd071282e4e240584f21e3c5beb48
Gonho LEE <sincerity410@gmail.com>
2020-07-13 18:07:29 +0900

    minor fix: 불필요한 띄어쓰기 수정 및 로직 일부수정(좋아요,Post-Like)

--- students/10th/gonholee/westagram/users/views.py
+++ students/10th/gonholee/westagram/users/views.py
@@ -51,7 +51,6 @@ class FollowUserView(View):
             if User.objects.filter(id=data['followed_user_id']).exists() and User.objects.filter(id=data['following_user_id']).exists():
                 following_users = list(Following.objects.filter(followed_user=data['followed_user_id']).values('following_user_id'))
 
-                print(following_users)
                 for following_user in following_users:
                     if int(data['following_user_id']) == following_user['following_user_id']:
                         return JsonResponse({'message':'Already Followed'}, status=401)
@@ -60,7 +59,6 @@ class FollowUserView(View):
                         followed_user_id     =   data['followed_user_id'],
                         following_user_id    =   data['following_user_id']
                 ).save()
-                    
                 return JsonResponse({'message':'SUCCESS'}, status=200)
 
             return JsonResponse({'message':'INVALID_USER'}, status=401)
