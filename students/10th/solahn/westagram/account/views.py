e756f12076f6cad2e5bf45129f1fbd80e38080b5
sol ahn <solahn@MacBook-Pro-5.local>
2020-07-14 22:54:53 +0900

    [안솔]Modified 리뷰 요청 반영

--- students/10th/solahn/westagram/account/views.py
+++ students/10th/solahn/westagram/account/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
 import json
 from django.views import View
 from django.http import JsonResponse
 from .models import Account
 
 class SignUpView(View):
-   
-    
     def post(self, request):
         data = json.loads(request.body)
         if Account.objects.filter(email = data['email']).exists():
@@ -24,7 +21,6 @@ class SignUpView(View):
         user_data = Account.objects.values()
         return JsonResponse({'users':list(user_data)},status=200)
 
-    
 class SignInView(View):
     def post(self, request):
         data = json.loads(request.body)
@@ -34,11 +30,9 @@ class SignInView(View):
                 if user.password == data['password']:
                     return JsonResponse({'message':f'{user.email}회원님 로그인 성공'}, status=200)
                 else:
-                    return JsonResponse({'message':'비밀번호 오류'}, status=401)
+                    return JsonResponse({'message':'UNAUTHORIZED'}, status=401)
                 return JsonResponse({'message':'INVALID_USER'}, status=400)
            
         except KeyError:
             return JsonResponse({'message':'KeyError'}, status=400)
                 
-
-
