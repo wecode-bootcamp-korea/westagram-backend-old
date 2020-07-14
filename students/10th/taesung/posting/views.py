90c4aa1906522fca6db6409c55adf94d810e7717
이태성 <tomleets@MacBook-Pro.local>
2020-07-14 18:04:24 +0900

    refactory user.view, posting.view

--- students/10th/taesung/posting/views.py
+++ students/10th/taesung/posting/views.py
@@ -3,6 +3,7 @@ import json
 from django.views import View
 from django.http  import JsonResponse
 
+from user.models  import User
 from .models      import (
     Article,
     Comment
@@ -14,11 +15,12 @@ class ArticleView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            Article(
-                user = User.objects.get(name= data['name']),
-                head = data['head'],
-                body = data['body']
-            ).save()
+            if User.objects.filter(name = data['name']).exists() and not Article.objects.filter(head = data['head']).exists():
+                Article(
+                    user = User.objects.get(name= data['name']),
+                    head = data['head'],
+                    body = data['body']
+                ).save()
             return JsonResponse({'message':'SUCCESS'}, status=200)
         except KeyError:
             return JsonResponse({'messgae' : 'FAIL'}, status=400)
@@ -28,7 +30,7 @@ class Comment(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            if User.objects.get(name=data['name']) and Article.objects.get(head = data['head']):
+            if User.objects.filter(name=data['name']).exists() and Article.objects.filter(head = data['head']).exists():
                 Comment(
                     content = data['content'],
                     name    = User.objects.get(name = data['name']),
--- students/10th/taesung/user/views.py
+++ students/10th/taesung/user/views.py
@@ -9,13 +9,15 @@ class SignUp(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
+            if User.objects.filter(email = data['email']).exists():
+                return JsonResponse({'message': "EXIST_EMAIL"}, status=400)
             if ('@' in data['email']) and (len(data['password']) >= 5):
                 User(
                     name = data['name'],
                     email = data['email'],
                     password = data['password']
                 ).save()
-                return JsonResponse({'message': 'SUCCESS'}, status=200)
+                return JsonResponse({'message': 'PERMISSION_MEMBERS'}, status=200)
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
 
@@ -27,7 +29,7 @@ class SignIn(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            if User.objects.get(name=data['name']) and User.objects.get(password=data['password']):
-                return JsonResponse({'message':'SUCCESS'}, status=200)
+            if User.objects.filter(name=data['name']).exists and User.objects.filter(password=data['password']).exists:
+                return JsonResponse({'message':'LOG_IN_SUCCESS'}, status=200)
         except:
             return JsonResponse({"message": "INVALID_USER"}, status=401)
