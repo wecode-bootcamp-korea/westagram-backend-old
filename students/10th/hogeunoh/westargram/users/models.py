0e10d718471d02148df42a1120a62b2f795724cd
dhgh95 <ghms2525@naver.com>
2020-07-15 21:36:30 +0900

    [오호근]Modify: 들여쓰기변경, 비밀번호해쉬추가

--- students/10th/hogeunoh/westargram/users/models.py
+++ students/10th/hogeunoh/westargram/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Users(models.Model):
-	name		= models.CharField(max_length =50)
 	email		= models.CharField(max_length =50)
 	password	= models.CharField(max_length =100)
 	created_at 	= models.DateTimeField(auto_now_add = True)
--- students/10th/hogeunoh/westargram/westargram/settings.py
+++ students/10th/hogeunoh/westargram/westargram/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from db import DATABASES as db
+import db
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_ps3-&k-0@_*##(gw2=*3)!1@d*r-e6nrqyf$(m1jjcpu@)k1t'
-
+#SECRET_KEY = '_ps3-&k-0@_*##(gw2=*3)!1@d*r-e6nrqyf$(m1jjcpu@)k1t'
+SECRET_KEY = db.SECRET['secret']
+ALGORITHM = db.ALGORITHM['algorithm']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'user'
+	'users',
+	'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
 #    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'westargram.urls'
@@ -81,7 +85,7 @@ DATABASES = {
     }
 }
 '''
-DATABASES = db
+DATABASES = db.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -120,3 +124,32 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#REMOVE_APPEND_SLASH_WARNING
+APPEND_SLASH = False
+
+##CORS
+CORS_ORIGIN_ALLOW_ALL=True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+		#만약 허용해야할 추가적인 헤더키가 있다면?(사용자정의 키) 여기에 추가하면 됩니다.
+)
