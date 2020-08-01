db2d8424be4180d60c74def8916c7d0af431af4b
이충희 <choonghee.dev@gmail.com>
2020-08-01 15:47:26 +0900

    불필요한 파일 삭제 및 코드 정렬

--- students/11th/choonghee-lee/account/views.py
+++ students/11th/choonghee-lee/account/views.py
@@ -1,41 +1,52 @@
 import bcrypt
 import json
 
-from django.http            import JsonResponse
-from django.views.generic   import View
+from django.http          import JsonResponse
+from django.views.generic import View
 
-from .codes         import (
+from .codes      import (
     ERROR_INVALID_INPUT,
     ERROR_INVALID_PHONE,
     ERROR_INVALID_EMAIL,
     SUCCESS_VALID_REQUEST,
 )
-from .models        import User
-from .validators    import UserRegisterValidator
+from .models     import User
+from .validators import UserRegisterValidator
 
 class UserRegisterView(View):
     @UserRegisterValidator
     def post(self, request, **kwargs):
-        phone_or_email  = kwargs['phone_or_email']
-        name            = kwargs['name']
-        username        = kwargs['username']
-        password        = kwargs['password']
-        result_code     = kwargs['result_code']
+        phone_or_email = kwargs['phone_or_email']
+        name           = kwargs['name']
+        username       = kwargs['username']
+        password       = kwargs['password']
+        result_code    = kwargs['result_code']
 
         if result_code == ERROR_INVALID_INPUT:
-            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+            return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
         elif result_code == ERROR_INVALID_PHONE:
-            return JsonResponse({'message':'INVALID_PHONE_NUMBER'}, status=400)
+            return JsonResponse(
+                {'message': 'INVALID_PHONE_NUMBER'}, 
+                status = 400,
+            )
         elif result_code == ERROR_INVALID_EMAIL:
-            return JsonResponse({'message':'INVALID_EMAIL'}, status=400)
+            return JsonResponse({'message': 'INVALID_EMAIL'}, status = 400)
         elif result_code == SUCCESS_VALID_REQUEST:    
-            has_same_phone_or_email=User.objects.filter(phone_or_email=phone_or_email)
-            has_same_username = User.objects.filter(username=username)
+            has_same_phone_or_email=User.objects.filter(
+                phone_or_email = phone_or_email
+            )
+            has_same_username = User.objects.filter(username = username)
 
             if has_same_phone_or_email or has_same_username:
-                return JsonResponse({'message': 'ALREADY_SIGNED_UP'}, status=409)
+                return JsonResponse(
+                    {'message': 'ALREADY_SIGNED_UP'},
+                    status=409,
+                )
             else:
-                bcrypted_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+                bcrypted_password = bcrypt.hashpw(
+                    password.encode('utf-8'), 
+                    bcrypt.gensalt()
+                )
                 User(
                     phone_or_email=phone_or_email,
                     name=kwargs['name'],
