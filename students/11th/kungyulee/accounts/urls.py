4dcb458a73ec8a55683b37be52430c907e199a36
kungyu <hismayfly@gmail.com>
2020-08-03 17:47:42 +0900

    added login view

--- students/11th/kungyulee/accounts/urls.py
+++ students/11th/kungyulee/accounts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path 
+from django.urls import path
 
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
-   path('register', RegisterView.as_view()),
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
 ]
--- students/11th/kungyulee/accounts/views.py
+++ students/11th/kungyulee/accounts/views.py
@@ -6,7 +6,6 @@ from django.core.exceptions import ValidationError
 
 from .models import User
 
-
 class RegisterView(View):
     def post(self, request):
         data = json.loads(request.body)
@@ -16,21 +15,33 @@ class RegisterView(View):
             phone_number    = data['phone_number'],
             password        = data['password']
             )
-            try:
-                registered_user.full_clean()
-            except ValidationError as e:
-                trace_back = traceback.format_exc()
-                print(f'{e} : {trace_back}')
-            else:
-                registered_user.save()
-                return JsonResponse({'message' : 'SUCCESS'}, status = 200)
-
-        except Exception as e:
+            registered_user.full_clean()
+        except ValidationError as e:
             trace_back = traceback.format_exc()
             print(f'{e} : {trace_back}')
+        else:
+            registered_user.save()
+            return JsonResponse({'message' : 'SUCCESS'}, status = 200)
 
-        return JsonResponse({'message' : 'try again'}, status = 200)
+        return JsonResponse({'message' : 'try again'}, status = 406)
 
     def get(self, request):
         return JsonResponse({'get' : 'success'}, status = 200)
 
+class LoginView(View):
+    def post(self, request):
+        data  = json.loads(request.body)
+        users = User.objects.all()
+
+        if not data['phone_number'] or not data['password']:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
+        if users.filter(phone_number = data['phone_number']):
+            user = users.get(phone_number=data['phone_number'])
+            if user.password == data['password']:
+                return JsonResponse({'message' : 'SUCCESS'}, status = 200)
+
+        return JsonResponse({'message' : 'INVALID_USER'}, status = 401)
+
+    def get(self, request):
+        return JsonResponse({'get' : 'success'}, status = 200)
