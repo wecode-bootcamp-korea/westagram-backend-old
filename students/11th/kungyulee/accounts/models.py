ecdd2db374549d329e7e0287d0c160ac2de2024e
kungyu <hismayfly@gmail.com>
2020-08-03 10:44:28 +0900

    added User model, validators and views

--- students/11th/kungyulee/accounts/models.py
+++ students/11th/kungyulee/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+from .validators import validate_number, validate_password
+
+class User(models.Model):
+    phone_number = models.CharField(
+        validators = [validate_number],
+        max_length = 13,
+        blank      = False,
+        unique     = True
+    )
+    password     = models.CharField(
+        validators = [validate_password],
+        max_length = 64,
+        blank      = False
+    )
--- students/11th/kungyulee/accounts/views.py
+++ students/11th/kungyulee/accounts/views.py
@@ -1,1 +1,36 @@
+import json, traceback
+
+from django.views import View
+from django.http import JsonResponse
+from django.core.exceptions import ValidationError
+
+from .models import User
+
+
+class RegisterView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        try:
+            registered_user = User(
+            phone_number    = data['phone_number'],
+            password        = data['password']
+            )
+            try:
+                registered_user.full_clean()
+            except ValidationError as e:
+                trace_back = traceback.format_exc()
+                print(f'{e} : {trace_back}')
+            else:
+                registered_user.save()
+                return JsonResponse({'message' : 'SUCCESS'}, status = 200)
+
+        except Exception as e:
+            trace_back = traceback.format_exc()
+            print(f'{e} : {trace_back}')
+
+        return JsonResponse({'message' : 'try again'}, status = 200)
+
+    def get(self, request):
+        return JsonResponse({'get' : 'success'}, status = 200)
 
