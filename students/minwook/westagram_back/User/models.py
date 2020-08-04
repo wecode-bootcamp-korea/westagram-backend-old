e7bb47cd4054f8c6d153e0a70f855ddd7908aac2
Minwook11 <alsdnr4874@gmail.com>
2020-08-04 17:05:52 +0900

    [Mission 2] 소스 코드 정리

--- students/minwook/westagram_back/User/models.py
+++ students/minwook/westagram_back/User/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length = 50)
+    name     = models.CharField(max_length = 50)
     password = models.CharField(max_length = 50, null = False)
-    email = models.CharField(max_length = 50)
-    phone = models.CharField(max_length = 30)
+    email    = models.CharField(max_length = 50)
+    phone    = models.CharField(max_length = 30)
