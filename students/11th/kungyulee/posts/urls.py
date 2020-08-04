53730822b3f13fc6fa42e81818b56b4c63513c34
kungyu <hismayfly@gmail.com>
2020-08-04 16:41:27 +0900

    edit views and models from posts

--- students/11th/kungyulee/posts/urls.py
+++ students/11th/kungyulee/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PostView,PostListView 
+from .views import PostView, CommentView, PostDetailView
 
 urlpatterns = [
-    path('create', PostView.as_view()),
-    path('list', PostListView.as_view()),
+    path('posting', PostView.as_view()),
+    path('comment', CommentView.as_view()),
+    path('posting/<int:pk>', PostDetailView.as_view()),
 ]
--- students/11th/kungyulee/posts/views.py
+++ students/11th/kungyulee/posts/views.py
@@ -1,11 +1,11 @@
-import json
+import traceback
 
-from django.shortcuts import render
-from django.views import View
-from django.http import JsonResponse
+from django.views        import View
+from django.http         import JsonResponse
+from django.forms.models import model_to_dict
 
 from accounts.models import User
-from .models import Post
+from .models         import Post, Comment
 
 class PostView(View):
     def post(self, request):
@@ -14,17 +14,46 @@ class PostView(View):
         try:
             user = User.objects.get(phone_number=data['phone_number'])
             post = Post(
-                author = user,
-                contents = data['contents'],
+                author    = user,
+                contents  = data['contents'],
                 image_url = data['image_url'],
             ).save()
             return JsonResponse({'message' : 'SUCCESS'}, status = 200)
-        except:
-            return JsonResponse({'message' : 'FAILED'}, status = 400)
+        except KeyError:
+            return JsonResponse({'message' : KeyError}, status = 400)
 
-class PostListView(View):
+        return JsonResponse({'message' : 'TRY AGAIN'}, status = 400)
+
     def get(self, request):
-        posts = Post.objects.all().values()
-        posts_list = list(posts)
-        return JsonResponse(posts_list, safe = False)
+        posts     = Post.objects.all().values()
+        post_list = list(posts)
+        return JsonResponse(post_list, safe = False)
 
+class PostDetailView(View):
+    def get(self, request, pk):
+        post      = Post.objects.get(pk = pk)
+        dict_post = model_to_dict(post)
+        return JsonResponse(dict_post, status = 200)
+
+class CommentView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        try:
+            user = User.objects.get(phone_number=data['phone_number'])
+            post = Post.objects.get(pk=data['pk'])
+            comment = Comment(
+                author   = user,
+                post     = post,
+                contents = data['contents'],
+            ).save()
+            return JsonResponse({'message' : 'SUCCESS'}, status = 200)
+        except KeyError:
+            return JsonResponse({'message' : KeyError}, status = 400)
+
+        return JsonResponse({'message' : 'TRY AGAIN'}, status = 400)
+
+    def get(self, request):
+        comments     = Comment.objects.all().values()
+        comment_list = list(comments)
+        return JsonResponse(comment_list, safe = False)
