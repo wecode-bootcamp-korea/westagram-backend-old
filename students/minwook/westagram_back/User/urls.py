fe44dd264fc11a0d93b78d3d989f422507a77a21
Minwook11 <alsdnr4874@gmail.com>
2020-08-05 17:10:26 +0900

    [Mission 3] 회원가입 엔드포인트 - 기본 기능 구현 - 이전 작업 부분 오타 수정

--- students/minwook/westagram_back/User/urls.py
+++ students/minwook/westagram_back/User/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUp
+from .views import SignUp, SignIn
 
 urlpatterns = [
-path('SignUp', SignUp.as_view())
+    path('SignUp', SignUp.as_view()),
+    path('SignIn', SignIn.as_view())
 ]
--- students/minwook/westagram_back/User/views.py
+++ students/minwook/westagram_back/User/views.py
@@ -1,8 +1,8 @@
 import json
 
-from django.views import View
-from django.http  import JsonResponse
-from django.db    import IntegrityError
+from django.views           import View
+from django.http            import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import User
 
@@ -31,7 +31,7 @@ class SignUp(View):
             return JsonResponse({'message':'MINIMUM_CONDITIONS_FAILED'}, status = 400)
 
         if email != '':
-            if ('@' not in email) and ('.' not in email)::
+            if ('@' not in email) and ('.' not in email):
                 return JsonResponse({'message':'EMAIL_FORMAT_FAILED'}, status = 400)
 
         if password != '':
@@ -48,3 +48,34 @@ class SignUp(View):
     def get(self, request):
         user_data = User.objects.values()
         return JsonResponse({'users':list(user_data)}, status = 200)
+
+class SignIn(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        try:
+            name     = data['name']
+            email    = data['email']
+            phone    = data['phone']
+            password = data['password']
+
+            if password == '':
+                return JsonResponse({'message':'NO_PASSWORD_INPUT'}, status = 400)
+
+            saved_password = ''
+            if name != '':
+                saved_password = (User.objects.filter(name = name).values().get())['password']
+            if email != '':
+                saved_password = (User.objects.filter(email = email).values().get())['password']
+            if phone != '':
+                saved_password = (User.objects.filter(phone = phone).values().get())['password']
+
+            if saved_password != '':
+                if password == saved_password:
+                    return JsonResponse({'message':'SUCCESS'}, status = 200)
+                return JsonResponse({'message':'INVALID_USER'}, status = 401)
+            return JsonResponse({'message':'INVALID_USER'}, status = 401)
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status = 400)
+        except User.DoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status = 401)
