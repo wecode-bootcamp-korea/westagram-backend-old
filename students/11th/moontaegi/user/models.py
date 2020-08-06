e640b3ae10aa56b9f63f6a6b5acc20321a6fd7f0
moontaeki <orangemush99@gmail.com>
2020-08-06 12:41:32 +0900

    Mission 2 signup completed

--- students/11th/moontaegi/user/models.py
+++ students/11th/moontaegi/user/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db   import models
+from .validation import validate_email, validate_password
 
-# Create your models here.
+
+class User(models.Model):
+    name         = models.CharField(max_length  = 20)
+    email        = models.EmailField(max_length = 50, validators = [validate_email])
+    password     = models.CharField(max_length  = 50, validators = [validate_password])
+    phone_number = models.CharField(max_length  = 50)
+
+    created_at   = models.DateTimeField(auto_now_add = True)
+    updated_at   = models.DateTimeField(auto_now     = True)
+
+
--- students/11th/moontaegi/user/views.py
+++ students/11th/moontaegi/user/views.py
@@ -1,1 +1,55 @@
+import json
+
+from django.core.exceptions import ValidationError
+from django                 import forms
+from django.views           import View
+from django.http            import JsonResponse
+from .models                import User
+
+class SignUpView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        users_info = User.objects.all()
+
+        """
+        1. 이메일이나 패스워드 키가 전달되지 않았을 시,
+        {"message": "KEY_ERROR"}, status code 400 을 반환
+        2. 회원가입시 이메일 @와 .이 필수 포함.
+        3. 회원가입시 비밀번호 8자리 이상이어야 함.
+        4. 회원가입시 동일한 전화번호나 사용자 이름, 이메일을 사용하지않음
+        """
+
+        try:
+            signup_user = User(
+                name         = data['name'],
+                email        = data['email'],
+                password     = data['password'],
+                phone_number = data['phone_number']
+            )
+            signup_user.full_clean()
+        except ValidationError:
+            return JsonResponse({'message': 'INVALID_PASSWORD_OR_EMAIL'}, status = 400)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        
+        if users_info.filter(name = data['name']):
+            return JsonResponse({'message': 'already exsit name'})
+
+        if users_info.filter(phone_number = data['phone_number']):
+            return JsonResponse({'message': 'already exsit phone_number'})
+        
+        if users_info.filter(email = data['email']):
+            return JsonResponse({'message': 'already exsit email'})
+
+        signup_user.save()
+        return JsonResponse({'message':'SUCCESS'}, status=200)
+
+    def get(self, request):
+        user_data = User.objects.values()
+
+        return JsonResponse({'users':list(user_data)},
+        status=200)
+
+
 
