202b52e3d4c616a3998c1c913a41c88637b0bb87
KIWOOK <1218kim23@gmail.com>
2020-08-06 12:56:52 +0900

    장고미션01 수정사항 반영

--- students/11th/Keywookkim/westagram/Users/models.py
+++ students/11th/Keywookkim/westagram/Users/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from pytz import timezone
 
 class Users(models.Model):
     user_id    = models.CharField(max_length = 30)
@@ -7,5 +9,11 @@ class Users(models.Model):
     phonenumber= models.CharField(max_length = 50)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+    @property
+    def created_at_korean(self):
+        korean_timezone = timezone(settings.TIME_ZONE)
+        return self.created_at.astimezone(korean_timezone)
+    
     class Meta:
         db_table = 'users'
+
