f671d6e12dbff108df3b72da33134bd52e17ba68
Soojeong Kim <ksooj.kim@gmail.com>
2020-09-01 15:48:28 +0900

    Mission 1: Django 초기세팅 - 불필요한 코드 제거 - user app 생성 및 추가

--- students/12th/Soojeong/project_westagram/user/models.py
+++ students/12th/Soojeong/project_westagram/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UserManager(models.Model):
+    login_name              = models.CharField(max_length=50)
+    phone_number_or_email   = models.CharField(max_length=12)
+    password                = models.CharField(max_length=50)
+    name                    = models.CharField(max_lenghth=50)
+    
---
+++ students/12th/Soojeong/project_westagram/user/views.py
@@ -0,0 +1,8 @@
+#https://docs.djangoproject.com/en/3.1/ref/validators/#django.core.validators.EmailValidator
+
+
+from .models import UserManager
+from django.http import Http404
+
+
+
