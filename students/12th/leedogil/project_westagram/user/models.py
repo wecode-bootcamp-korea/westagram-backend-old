3174d319b401dd49cf52c2a1957ea7de489ab789
admin <admin@adminui-MacBookAir.local>
2020-09-02 01:56:41 +0900

    Mission 1: Django 초기 세팅

--- students/12th/leedogil/project_westagram/user/models.py
+++ students/12th/leedogil/project_westagram/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+ phone      = models.CharField(max_length = 50)
+ name       = models.CharField(max_length = 50) 
+ email      = models.CharField(max_length = 50)
+ password   = models.CharField(max_length = 300)
+ created_at = models.DateTimeField(auto_now_add = True)
+ updated_at = models.DateTimeField(auto_now = True) 
