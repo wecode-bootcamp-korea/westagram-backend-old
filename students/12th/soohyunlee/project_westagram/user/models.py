1212e0fab2e866d64adffe6080e9dc050ae5a118
soohyun-lee <soohyun527@gmail.com>
2020-09-02 13:53:47 +0900

    Mission 2:회원가입 엔드포인트

--- students/12th/soohyunlee/project_westagram/user/models.py
+++ students/12th/soohyunlee/project_westagram/user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    name       = models.CharField(max_length = 50)
+    name       = models.CharField(max_length = 50, null=True)
     phone_num  = models.IntegerField()
-    email      = models.CharField(max_length = 100)
+    email      = models.CharField(max_length = 100, null=True)
     pw         = models.CharField(max_length = 200)
--- students/12th/soohyunlee/project_westagram/user/views.py
+++ students/12th/soohyunlee/project_westagram/user/views.py
@@ -1,5 +1,4 @@
 import json
-import re
 
 from django.views import View
 from django.http import JsonResponse
@@ -8,6 +7,18 @@ from .models import User
 class MainView(View):
     def post(self, request):
         data = json.loads(request.body)
+
+        if not '@' in data['email'] or not '.' in data['email']:
+            return JsonResponse({'message':'Email_Form_Error'}, status=400)
+        if len(data['pw']) < 8:
+            return JsonResponse({'message':'Password_Form_Error'}, status=400)
+        if (User.objects.filter(email = data['email']) or
+            User.objects.filter(name = data['name']) or 
+            User.objects.filter(phone_num = data['phone_num'])):
+            return JsonResponse({'message':'Already_in_use'}, status=400)
+        else:
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+
         User(
             name      = data['name'],
             email     = data['email'],
@@ -15,14 +26,6 @@ class MainView(View):
             phone_num = data['phone_num']
         ).save()
 
-        if not '@' in data['email'] and not '.' in data['email']:
-            return JsonResponse({'message':'Email_Form_Error'}, status=400)
-        if len(data['pw']) < 8:
-            return JsonResponse({'message':'Password_Form_Error'}, status=400)
-        # if User.objects.filter(name = data['name']).exist():
-        #     return JsonResponse({'message':'Already_in_use'}, status=400)
-        else:
-            return JsonResponse({'message':'SUCCESS'}, status=200)
 
     def get(self, request):
         user_data = Users.objects.values()
