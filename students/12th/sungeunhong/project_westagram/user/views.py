c5cddf810b7234bba797b63fb0b23a4de05d61a5
jake <hongse21@gamil.com>
2020-09-02 13:20:07 +0900

    Mission2:회원가입 엔드포인트(views.py ,models.py 수정, .modles Users impor가 안됨)

--- students/12th/sungeunhong/project_westagram/user/views.py
+++ students/12th/sungeunhong/project_westagram/user/views.py
@@ -2,35 +2,55 @@ import json
 
 from django.views import View
 from django.http  import JsonResponse
-from .models      import User
+from .models      import Users
 
 class Account(View):
     def post(self,request):
-        data      = json.loads(request.body)
-        user_db   = User.objects.all()
+        data = json.loads(request.body)
         try:
-            User(
+            if data['name'].exist():
+                return JsonResponse(
+                    {'message': 'ALREADY_EXISTS'},
+                    status = 400
+                    )
+            if data['phone_number'].exist():
+                return JsonResponse(
+                    {'message': 'ALREADY_EXISTS'},
+                    status = 400
+                    )
+            if data['email'].exist():
+                return JsonResponse(
+                    {'message': 'ALREADY_EXISTS'},
+                    status = 400
+                    ) 
+            
+            if not '@' in data['email'] or not '.' in data['email']:
+                return JsonResponse(
+                    {"message": "INVALID_EMAIL"}, 
+                    status = 400
+                    )
+            if len(data['password']) < 8:
+                return JsonResponse(
+                    {"message": "Password must be at least 8 digits."},
+                    status = 400
+                    )
+            
+            Users(
                 name         = data['name'],
                 phone_number = data['phone_number'],
                 email        = data['email'],
                 password     = data['password']
-            )
-        
-            if user_db.filter(name = data['name']).exist():
-                return JsonResponse({'message': 'ALREADY_EXISTS'},status = 400)
-            elif user_db.filter(phone_number = data['phone_number']).exist():
-                return JsonResponse({'message': 'ALREADY_EXISTS'},status = 400)
-            elif user_db.filter(eamil = data['email']).exist():
-                return JsonResponse({'message': 'ALREADY_EXISTS'},status = 400) 
-            elif not '@' in user_db.filter(email = data['email']) or not '.' in user_db.filter(email = data['email']):
-                return JsonResponse({"message": "INVALID_EMAIL"}, status = 400)
-            elif len(data['password']) < 8:
-                return JsonResponse({"message": "Password must be at least 8 digits."},status = 400)
-           
-            User.save()
-            return JsonResponse({'message': 'SUCCESS'}, status = 200) 
+            ).save()
+
+            return JsonResponse(
+                {'message': 'SUCCESS'}, 
+                status = 200
+                ) 
         
         except KeyError:   
-            return JsonResponse({'message': 'KEY_ERROR'},status = 400)
+            return JsonResponse(
+                {'message': 'KEY_ERROR'},
+                status = 400
+                )
                                    
     
