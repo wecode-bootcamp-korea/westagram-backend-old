4e5bcc586c216c877f9913c628ebb6dce408a397
Soojeong Kim <ksooj.kim@gmail.com>
2020-09-02 21:06:12 +0900

    Mission 2. 회원가입 기능 구현(진행중) - 회원가입 기능 구현(중복여부 확인 기능 제외)

--- students/12th/Soojeong/project_westagram/user/models.py
+++ students/12th/Soojeong/project_westagram/user/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
-class UserManager(models.Model):
-    login_name              = models.CharField(max_length=50)
-    phone_number_or_email   = models.CharField(max_length=12)
+class Users(models.Model):
+    user_name               = models.CharField(max_length=50)
+    phone_number            = models.CharField(max_length=12)
+    email                   = models.CharField(max_length=100)
     password                = models.CharField(max_length=50)
-    name                    = models.CharField(max_lenghth=50)
+    name                    = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user_name
+
+    class Meta:
+        db_table = "users"
     
--- students/12th/Soojeong/project_westagram/user/views.py
+++ students/12th/Soojeong/project_westagram/user/views.py
@@ -1,8 +1,46 @@
-#https://docs.djangoproject.com/en/3.1/ref/validators/#django.core.validators.EmailValidator
+import json
+from .models import Users
+from django.http import JsonResponse
+from django.views import View
 
 
-from .models import UserManager
-from django.http import Http404
+class SignUpView(View):
+
+    def post(self, request):
+        data = json.loads(request.body)
+        
+        signup_user(
+        	name 	     = data['name'],
+            	email           = data['email'],
+            	phone_number    = data['phone_number'],
+            	password        = data['password'],
+            	user_name       = data['user_name'],
+        )
+            
+        if email not in data.keys() or password not in data.keys():
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+        elif "@" not in email or "." not in email:
+            return JsonResponse({'message':'email address incorrect'}, status=400)
+        
+        elif len(password) < 8:
+            return JsonResponse({'message':'password must be longer than 8 characters'}, status=400)
+        
+        signup_user.objects.create()
+        return JsonResponse({'message':'SUCCESS'}, status=200)
 
 
+"""
+            기존 db에서 데이터를 가져와서 새로 등록하려는 이름이랑 비교해야 하는데 가져오는 걸 못하겠다 ㅠ 
+            if users.filter(name = data['name']).exist():
+                return JsonResponse({'message': 'name already registered'}, status=400)
 
+            elif users.filter(user_name = data['user_name']).exist():
+                return JsonResponse({'message':'username already in use'}, status=400)
+
+            elif users.filter(email = data['email']).exist():
+                return JsonResponse({'message':'email address already registered'}, status=400)
+
+            elif users.filter(phone_number = data['phone_number']).exist():
+                return JsonResponse({'message':'phone number already registered'}, status=400)
+"""
