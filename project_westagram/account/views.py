5bb1599a987f42507a79dcd0a39edb19adfa51f9
구동현 <gorusair@gmail.com>
2020-09-03 17:46:44 +0900

    Django Mission 2. 회원가입 기능 구현

--- project_westagram/account/views.py
+++ project_westagram/account/views.py
@@ -1,28 +1,58 @@
 import json
-from django.views import View
-from django.http  import JsonResponse
-from .models      import Account
+import bcrypt
+import jwt
+
+from project_westagram.settings import SECRET_KEY
+from django.views               import View
+from django.http                import JsonResponse,HttpResponse
+from .models                    import Account
 
 class SignUpView(View):
     def post(self, request):
         data = json.loads(request.body)
-        Account(
+        try:
+            if Account.objects.filter(email =data['email']).exists():
+                return HttpResponse(status=400)
+            password = data['password'].encode('utf-8')
+            password_crypt = bcrypt.hashpw(password, bcrypt.gensalt())
+            password_crypt = password_crypt.decode('utf-8')
+             Account(
                 email     =data['email'],
                 password  =data['password'],
                 ).save()
-        return JsonResponse({'message':'회원가입 완료'},status =200)
+                
+                return HttpResponse(status=200)
+            except keyError:
+                return JsonResponse({"message":"INVALID_KEYS"},status =400)
 
 class SignInView(View):
     def post(self, request):
         data = json.loads(request.body)
-        
-        if Account.objects.filter(email = data['email']).exists() :
-            user = Account.objects.get(email = data['email'])
-            if user.password == data['password'] :
-                return JsonResponse({'message':f'{user.email}님 로그인 성공!'}, status=200)
+
+        try:
+             if Account.objects.filter(email = data['email']).exists() :
+               user = Account.objects.get(email = data['email'])
+             if bcrypt.checkpw(data['password'].encode('utf-8'), user password.encode('utf-8')):
+                 token = jwt.encode({'email' : data['email']}, SECRET_KEY, algorithm = "HS256")
+                 token = token.decode('utf-8')
+
+                 return JsonResponse({"token" : token }, status=200)
+
             else :
-                return JsonResponse({'message':'비밀번호가 틀렸어요'},status = 200)
+                 return HttpResponse(status=401)
 
-        return JsonResponse({'message':'등록되지 않은 이메일 입니다.'},status=200)
+                 return HttpResponse(status=400)
 
+            except KeyError:
+                 return JsonResponse({"message":"INVALID_KEYS"}, status = 400)
 
+class TokenCheckView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+
+        user_token_info = jwt.decode(data['token'], SECRET_KEY, algorithm = 'HS256')
+
+        if Account.objects.filter(email=user_token_info['email']).exists() :
+            return HttpResponse(status=200)
+
+        return HttpResponse(status=403)
