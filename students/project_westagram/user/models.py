a4ff48858abcfc9d2f09a21f713b6a5cc239b825
Sungjin Kim <kimsj5259@gmail.com>
2020-09-03 13:49:03 +0900

    [mission 2]

--- students/project_westagram/user/models.py
+++ students/project_westagram/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    name         = models.CharField(max_length = 50)
+    email        = models.CharField(max_length = 50)
+    password     = models.CharField(max_length = 300)
+    phone_number = models.CharField(max_length = 100, null = True)
+    created_at   = models.DateTimeField(auto_now_add = True) # 생성된 시간
+    updated_at   = models.DateTimeField(auto_now = True) # update 된 정보
--- students/project_westagram/user/views.py
+++ students/project_westagram/user/views.py
@@ -1,3 +1,43 @@
-#from django.shortcuts import render
+import json
+from django.views import View
+from django.http  import JsonResponse
+from .models      import Users
+
+
+class SignUp(View):
+    def post(self, request):
+
+        data     = json.loads(request.body)
+        MIN_PWD  = 10
+
+        set_data = User.objects.values('name', 'email', 'phone_number')
+        
+        if filter_data in set_data:
+            return JsonResponse({'message': 'Already existed'}, status = 400)
+
+        if len(data['password']) < MIN_PWD:
+            return JsonResponse({'message':'Minimum length of pwd is 10'}, status=400)
+
+        if '@' or '.' not in data['email']:
+            return JsonResponse({'message':"please check if '@' or '.' are included properly"}, status =400)
+
+        if 'email' not in data.keys():
+            return JsonResponse({'message':'No such address'}, status = 400)
+
+        else:
+            Users(
+                name        = data['name'],
+                phone_number= data['phone_number'],
+                email       = data['email'],
+                password    = data['password'],
+            ).save()
+
+
+            return JsonResponse({'message':'SUCCESS'}, status=200)
+    
+    def get(self, request):
+        user_data = Users.objects.values()
+        return JsonResponse({"users":list(user_data)}, status=200)
+
 
 
