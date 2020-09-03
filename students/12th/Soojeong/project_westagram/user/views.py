6e0d5e3f07f69d66dc7ed0d2f006b7337194c39c
Soojeong Kim <ksooj.kim@gmail.com>
2020-09-03 21:10:39 +0900

    Mission 3. 로그인 기능 구현 - 이메일로만 로그인 가능 - 필수정보 미입력시 로그인 불가, 비밀번호 일치여부 확인 기능 구현

--- students/12th/Soojeong/project_westagram/user/views.py
+++ students/12th/Soojeong/project_westagram/user/views.py
@@ -27,40 +27,37 @@ class SignUpView(View):
         	user_name       = data['user_name'],
         ).save()
         
+        # 전번과 이메일을 null=True 로 했는데도 이메일이나 폰 넘버 중 하나만 쓰면 저장을 못함..!
         return JsonResponse({'message':'SUCCESS'}, status=200)
-        # null=True 로 했는데도 이메일이나 폰 넘버 중 하나만 쓰면 저장을 못함 남..! ㅠㅠ
+        
 
 
 class LogInView(View):
     def get(self, request):           
         data = json.loads(request.body)
-        print(data)
 
         # 계정정보 미입력    
-        #if ('phone_number' not in data.keys()) and ('email' not in data.keys()) and ('user_name' not in data.keys()):
-        #    return JsonResponse({'message':'아이디 입력하세요'}, status=400)
-        
         if (
             ('phone_number' and 
             'email' and
             'user_name')
             not in data.keys()
             ):
-            return JsonResponse({'message':'아이디 입력하세요'}, status=400)
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
 
         #비밀번호 미입력
         if 'password' not in data.keys():
-            return JsonResponse({'message':'비번입력 안함'}, status=400)       
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)       
 
         # 입력한 로그인수단이 존재하지 않는 경우 (우선 무조건 이메일로만 가입한다고 가정)
         if {'email':data['email']} not in list(Users.objects.values('email')):
-            return JsonResponse({'message':'이메일 주소 없음'}, status=401)
+            return JsonResponse({'message':'INVALID_USER'}, status=401)
 
-        # 이메일이 있는 경우, 비번이 틀리면 
-        if {'email':data['email']} in list(Users.objects.values('email')): # 입력한 이메일이 데이터에 있다면
-            user_info = Users.objects.filter(email=data['email']) # 해당 객체를 받아와서
-            if user_info.values('password')[0]['password'] != data['password']: # 입력한 비번과 객체의 비번이 맞나 확인
-                return JsonResponse({'message':'KEY ERROR'}, status=401) 
+        # 이메일이 있는 경우, 비번이 틀림
+        if {'email':data['email']} in list(Users.objects.values('email')): 
+            user_info = Users.objects.filter(email=data['email']) 
+            if user_info.values('password')[0]['password'] != data['password']:
+                return JsonResponse({'message':'KINVALID_USER'}, status=401) 
         
         return JsonResponse({'message':'SUCCESS'}, status=200) 
