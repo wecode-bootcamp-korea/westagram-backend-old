12d8d7d4eb249a5516319fda6f14d4176fd74e6f
DasomJung24 <jeongdasom6@gmail.com>
2020-09-03 16:17:14 +0900

    mission2: 회원가입구현 미완성

--- students/12th/dasomjung/project_westagram/user/models.py
+++ students/12th/dasomjung/project_westagram/user/models.py
@@ -6,3 +6,6 @@ class Users(models.Model):
     password         = models.CharField(max_length=300)
     created_at       = models.DateTimeField(auto_now_add=True)
     updated_at       = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
--- students/12th/dasomjung/project_westagram/user/views.py
+++ students/12th/dasomjung/project_westagram/user/views.py
@@ -9,38 +9,34 @@ class SignUp(View):
     def post(self, request):
         data = json.loads(request.body)
 
-        Users(
-            email           = data['email'],
-            password        = data['password']
-        ).save()
+        try:
+            if Users.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message': 'Already Exist'}, status = 400)
 
-        # 회원가입시 휴대폰번호와 이메일 중 이메일을 사용할 경우 전달이 안되었을 때 에러 반환
-        if not Users.objects.filter(email=email) == True:
+            Users.objects.create(
+                email = data['email'],
+                password = data['password']
+            ).save()
+
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
+        
+        except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        
    
         # 회원가입시 이메일을 사용할 경우 @ 과 . 이 포함 안된경우 에러 반환
         # if Users.email.filter(r'[^@.]$') == True:     
-        if not (Users.objects.get(email__contains='@') or Users.objects.get(email__contains='.')) == True:
+        # if not (Users.objects.get(email__contains='@') or Users.objects.get(email__contains='.')) == True:
         # shell 에서는 정상 작동 하는데 여기서는 안되네요.. 이미 저장된 정보만 가지고 할 수 있는 메소드 일까요?ㅠㅠ
-            return JsonResponse({'message': 'Incorrect format'}, status=400)    # 에러 반환 
+            # return JsonResponse({'message': 'Incorrect format'}, status=400)    # 에러 반환 
 
-        # 아이디(이메일)가 중복된 경우 에러 반환
-        if Users.objects.filter('email'==data['email']).exists() == True:
-            return JsonResponse({'message': 'Already Exist'}, status=409)    # 에러 반환
-        
-        # 비밀번호 전달이 안된 경우
-        if not Users.objects.filter(password=password) == True:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
-
+  
         # 비밀번호가 8자리 미만일 경우 에러 반환
-        if Users.password.filter(r'\d{,7}$') == True:
+        # if Users.password.filter(r'\d{,7}$') == True:
         # if Users.objects.filter(password__range(8)) == True:
         # 이게 더 맞을지...
-            return JsonResponse({'message': 'Too short'}, status=400)   # 에러 반환
+            # return JsonResponse({'message': 'Too short'}, status=400)   # 에러 반환
 
-
-
-        return JsonResponse({'message': 'SUCCESS'}, status=200)
 
     def get(self, request):
         user_data = Users.objects.values()
