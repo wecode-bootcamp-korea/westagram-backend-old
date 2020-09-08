87b5edef56b6c91c0c6841fb3d30d0732e254065
ogabin <ogabin@gmail.com>
2020-09-08 18:33:32 +0900

    [Mission 2] 회원가입 엔드포인트

--- students/12th/ogabin/project_westagram/user/models.py
+++ students/12th/ogabin/project_westagram/user/models.py
@@ -1,17 +1,10 @@
 from django.db import models
-from django.core.exceptions import ValidationError
-
-def validate_email(email):
-   if not '@' in email:
-       raise ValidationError("Not a valid email")
-   else:
-       return email
 
 class User(models.Model):
     name         = models.CharField(max_length = 20)
-    email        = models.CharField(max_length = 20, validators=[validate_email])
+    email        = models.CharField(max_length = 20)
     password     = models.CharField(max_length = 8)
-    phone_number = models.CharField(max_length = 20)
+    phone        = models.CharField(max_length = 20)
 
     class Meta:
         db_table = 'users'
--- students/12th/ogabin/project_westagram/user/views.py
+++ students/12th/ogabin/project_westagram/user/views.py
@@ -6,16 +6,13 @@ from .models      import User
 class UserView(View):
     def post(self, request):
         data = json.loads(request.body)
-        try:
-            User(
-                name         = data["name"],
-                email        = data["email"],
-                password     = data["password"],
-                phone_number = data["phone"],
-            ).save()
-            return JsonResponse({"message":"SUCCESS"}, status = 200)    
-        except KeyError:
-            return JsonResponse({"message":"KEY_ERROR"}, status = 400)
+        User(
+            name         = data["name"],
+            email        = data["email"],
+            password     = data["password"],
+            phone        = data["phone"],
+        ).save()
+        return JsonResponse({"message":"SUCCESS"}, status = 200)    
 
     def get(self, request):
         user_data = User.objects.values()
