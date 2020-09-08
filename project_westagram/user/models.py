12f422e2df9f6173f37cc65c37df7cf993b2fa6b
이도길 <dadk93@naver.com>
2020-09-09 01:23:53 +0900

    1

--- project_westagram/user/models.py
+++ project_westagram/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+   phoneNumber = models.CharField(max_length = 50)
+   name        = models.CharField(max_length = 50)
+   email       = models.CharField(max_length = 50)
+   password    = models.CharField(max_length = 50)
+
+  
