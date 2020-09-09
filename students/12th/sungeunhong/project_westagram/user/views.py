9aded2e81594e12c7d9af6de62ba5913ca86612e
jake <hongse21@gamil.com>
2020-09-09 22:13:22 +0900

    Add:토큰발행

--- students/12th/sungeunhong/project_westagram/user/views.py
+++ students/12th/sungeunhong/project_westagram/user/views.py
@@ -1,4 +1,6 @@
 import json 
+import bcrypt
+import jwt
 
 from django.views import View
 from django.http  import JsonResponse
@@ -7,8 +9,8 @@ from user.models  import User
 class AccountView(View):
     def post(self,request):
         data = json.loads(request.body)
-       
-        try :
+
+        try:
             if data['name'] and data['email'] and data['password']:
 
                 if '@' not in data['email'] or '.' not in data['email']:
@@ -31,14 +33,17 @@ class AccountView(View):
                     return JsonResponse(
                         {'message': 'ALREADY_EXISTS'},
                         status = 400
-                        )    
-           
+                        )
+                encoded_password = data['password'].encode('utf-8')
+                hashed_password = bcrypt.hashpw(encoded_password,bcrypt.gensalt())
+                data['password'] = hashed_password.decode('utf-8') 
+
             User.objects.create(
                 name         = data['name'],
                 email        = data['email'],
                 password     = data['password']
             ).save()
-
+          
             return JsonResponse(
                 {'message': 'SUCCESS'}, 
                 status = 200
@@ -55,11 +60,14 @@ class loginView(View):
         data = json.loads(request.body)
         
         try:
-            if User.objects.filter(name=data['name']).exists():
-                user = User.objects.get(name=data['name'])
-                if user.password == data['password']:
+            if User.objects.filter(email=data['email']).exists():
+                user = User.objects.get(email=data['email'])
+
+                if bcrypt.checkpw(data['password'].encode('utf-8'),user.password.encode('utf-8')):
+                    token = jwt.encode({'email': data['email']},'secret_key', algorithm = "HS256").decode('utf-8')
+                                                                                                      
                     return JsonResponse({
-                        'message':'SUCCESS'},
+                        'message':token},
                          status=200
                          ) 
                  
@@ -68,7 +76,7 @@ class loginView(View):
                          status=400
                          )
             return JsonResponse(
-                        {'message':'INVALID_NAME'},
+                        {'message':'INVALID_EMAIL'},
                          status=400
                          )
 
