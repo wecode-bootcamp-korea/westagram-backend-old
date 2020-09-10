9b2a587904a969434723527297357a00bd677c70
이도길 <dadk93@naver.com>
2020-09-10 18:40:06 +0900

    ???

--- students/12th/dogiri/project_westagram/user/models.py
+++ students/12th/dogiri/project_westagram/user/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class User(models.Model):
+class Users(models.Model):
   email        = models.CharField(max_length = 50)
   name         = models.CharField(max_length = 50)
   password     = models.CharField(max_length = 300)
--- students/12th/dogiri/project_westagram/user/views.py
+++ students/12th/dogiri/project_westagram/user/views.py
@@ -1,2 +1,29 @@
-from django.db import views
-from .         import User
+import json
+
+from django.http  import JsonResponse
+from django.views import View
+
+from .models      import Users
+
+class SignUp(View):
+  def post(self,request):
+    data = json.loads(request.body)
+   
+    if '@' and '.' not in data['email']
+      return JsonResponse({'message':'ERROR1'}  
+    elif len(data['password'] < 8
+      return JsonResponse({'message':'ERROR2'}
+    elif (Users.object.filter(name=data['name']).exist()
+
+    Users(
+      email        = data['email']
+      name         = data['name']
+      password     = data['password']
+      phone_number = data['phone_number']
+    ).save()
+
+    return JsonResponse({'message':'SUCCESS'},status=200)
+
+  def get(self.request):
+    user_data = Users.objects.values()
+    return JsonResponse({'users':list(user_data)},status=200)
