fe5cdd2456fec03cdd3046c4db7436ebd4ebe556
이도길 <dadk93@naver.com>
2020-09-11 01:05:54 +0900

    ???

--- students/12th/dogiri/project_westagram/user/models.py
+++ students/12th/dogiri/project_westagram/user/models.py
@@ -4,7 +4,7 @@ class Users(models.Model):
   email        = models.CharField(max_length = 50)
   name         = models.CharField(max_length = 50)
   password     = models.CharField(max_length = 300)
-  phone_number = models.CharField(max_length = 100, null = True)
+  phone_number = models.CharField(max_length = 100)
   created_at   = models.DateTimeField(auto_now_add = True)
   updated_at   = models.DateTimeField(auto_now = True)
 
--- students/12th/dogiri/project_westagram/user/views.py
+++ students/12th/dogiri/project_westagram/user/views.py
@@ -9,13 +9,18 @@ class SignUp(View):
   def post(self,request):
     data = json.loads(request.body)
    
-    if '@' and '.' not in data['email']
-      return JsonResponse({'message':'ERROR1'}  
-    elif len(data['password'] < 8
-      return JsonResponse({'message':'ERROR2'}
-    elif (Users.object.filter(name=data['name']).exist()
+    if '@' and '.' not in data['email']:
+      return JsonResponse({'message':'ERROR1'},status=404)  
+    elif len(data['password'] < 8:
+      return JsonResponse({'message':'ERROR2'},status=404)
+    elif (Users.object.filter(name=data['name']).exists() or
+      Users.object.filter(email=data['email']).exists() or
+      Users.object.filter(phone_number=data['phone_number']).exists()):
+      return JsonResponse({'message':'ERROR3'},status=404)
+    elif (data['email'] and data['password']) == False
+      return JsonResponse({'message':'key error'},status=404)      
 
-    Users(
+     Users(
       email        = data['email']
       name         = data['name']
       password     = data['password']
@@ -25,5 +30,18 @@ class SignUp(View):
     return JsonResponse({'message':'SUCCESS'},status=200)
 
   def get(self.request):
+    user_data = Users.objects.values()
+    return JsonResponse({'users':list(user_data)},status=200)
+
+class SignIn(View):
+  def post(self,request):
+    if (data['email'] and data['password'] == False
+      return JsonResponse({'message':'key error'},status=404)
+    elif (Users.object.filter(name=data['name']).exists() or
+      Users.object.filter(email=data['email']).exists())
+      return JsonResponse({'message':'invalid user'},status=404)
+    return JsonResponse({'message':'success'},status=200)
+
+  def get(self,request):
     user_data = Users.objects.values()
     return JsonResponse({'users':list(user_data)},status=200)
