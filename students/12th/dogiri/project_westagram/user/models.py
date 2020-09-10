4e1f14339d046ecca32f31b928bedc239b4b7770
이도길 <dadk93@naver.com>
2020-09-10 13:13:34 +0900

    ???

--- students/12th/dogiri/project_westagram/user/models.py
+++ students/12th/dogiri/project_westagram/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  email        = models.CharField(max_length = 50)
+  name         = models.CharField(max_length = 50)
+  password     = models.CharField(max_length = 300)
+  phone_number = models.CharField(max_length = 100, null = True)
+  created_at   = models.DateTimeField(auto_now_add = True)
+  updated_at   = models.DateTimeField(auto_now = True)
+
+  class meta:
+    db_table = 'users'
