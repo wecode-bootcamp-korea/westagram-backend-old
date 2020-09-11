4bf2f8c3f5a2a262a22e948f36226a2f89764112
Soojeong Kim <ksooj.kim@gmail.com>
2020-09-11 10:35:06 +0900

    [최종] 미션4까지 구현 - 로그인시 필수정보 입력여부, 중복체크, 비밀번호 길이, 이메일유효성 키에러 포함 - 비밀번호 암호화 및 토큰발생 가능

--- students/12th/Soojeong/project_westagram/user/views.py
+++ students/12th/Soojeong/project_westagram/user/views.py
@@ -15,26 +15,35 @@ class SignUpView(View):
             or 'name'  not in data.keys()\
             or 'password' not in data.keys():
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
-
-        elif ("@" and ".") not in data['email']:
-            return JsonResponse({'message':'INCORRECT_EMAIL_ADDRESS'}, status=400)
         
-        elif len(data['password']) < 8:
-           return JsonResponse({'message':'PASSWORD_TOO_SHORT'}, status=400) 
+        elif ("@" and ".") not in data['email']:
+            return JsonResponse({'message':'INVALIDE'}, status=400)
 
+        #중복가입 방지
+        elif (
+            ('phone_number' in data.keys()\
+            and {'phone_number': data['phone_number']} in list(Users.objects.values('phone_number')))\
+            or ('email' in data.keys()\
+            and {'email': data['email']} in list(Users.objects.values('email')))\
+            or ('user_name' in data.keys()\
+            and {'user_name': data['user_name']} in list(Users.objects.values('user_name')))\
+            ):
+            return JsonResponse({'message':'DUPLICATED_USER'}, status=401)
+
+        # 유저 정보 등록
         Users(
     	    name    		= data['name'],
-        	email           = data['email'],
+            email           = data['email'],
         	phone_number    = data['phone_number'],
-        	password        = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+            password        = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
         	user_name       = data['user_name'],
         ).save()
-        
+
         # 전번과 이메일을 null=True 로 했는데도 이메일이나 폰 넘버 중 하나만 쓰면 저장을 못함
         return JsonResponse({'message':'SUCCESS'}, status=200)
         
 class LogInView(View):
-    def get(self, request):           
+    def post(self, request):           
         data = json.loads(request.body)
 
         # 계정정보 미입력    
@@ -43,7 +52,7 @@ class LogInView(View):
             and 'email' not in data.keys()\
             and 'user_name' not in data.keys()
             ):                         
-            return JsonResponse({'message':'USER_KEY_ERROR'}, status=400)
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
         #비밀번호 미입력
         if 'password' not in data.keys():
@@ -58,7 +67,7 @@ class LogInView(View):
             or ('user_name' in data.keys()\
             and {'user_name': data['user_name']} not in list(Users.objects.values('user_name')))\
             ):
-            return JsonResponse({'message':'이메일/폰번/유저네임 등록안됨'}, status=401)
+            return JsonResponse({'message':'INVALID_USER'}, status=401)
         
         # 입력한 이메일과 비밀번호 일치하면 성공 - 토큰발행
         if {'email':data['email']} in list(Users.objects.values('email')): 
@@ -68,10 +77,9 @@ class LogInView(View):
 
             if bcrypt.checkpw(input_password, db_password):
                 access_token    = jwt.encode(   {'user_id': user_info.id}, SECRET_KEY, ALGORITHM )
-                token_decoded   = jwt.decode(   access_token, SECRET_KEY, ALGORITHM  )
                 return JsonResponse({'Authorized': access_token.decode('utf-8')}, status=200)
 
-        return JsonResponse({'message':'PASSWORD_NOT_MATCHING'}, status=401)
+        return JsonResponse({'message':'INVALID_USER'}, status=401)
 
         #우선 이메일로만 로그인 가능.
         """
