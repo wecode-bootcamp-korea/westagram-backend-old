67cf7e7b59943a534fde46c9a956037752ac470c
이도길 <dadk93@naver.com>
2020-09-11 11:19:31 +0900

    ???

--- students/12th/dogiri/project_westagram/user/views.py
+++ students/12th/dogiri/project_westagram/user/views.py
@@ -43,7 +43,7 @@ class SignIn(View):
 
     hash_password = Users.objects.get(password=data['password']
 
-    if (bcrypt.checkpw(data['password'].encode('utf-8'),hash_password):
+    if (bcrypt.checkpw(data['password'].encode('utf-8'),hash_password)):
       return JsonResponse({'message':'SUCCESS'}),status=200)
     else:
       return JsonResponse({'message':'INVALIE PASSOWRD'})
