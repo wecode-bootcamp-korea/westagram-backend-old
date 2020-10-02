c2e5c9b0fffaa2132eaf44dab8aae2826e66b3b9
ogabin <ogabin@gmail.com>
2020-10-02 13:55:40 +0900

    Refactor:SignInView 수정

--- students/12th/ogabin/project_westagram/project_westagram/urls.py
+++ students/12th/ogabin/project_westagram/project_westagram/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.urls import include, path
 
 urlpatterns = [
-    path('user/', include('user.urls')),
+    path('user', include('user.urls')),
 ]
--- students/12th/ogabin/project_westagram/user/views.py
+++ students/12th/ogabin/project_westagram/user/views.py
@@ -20,3 +20,20 @@ class SignUpView(View):
 
         except KeyError:
             return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+
+class SignInView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if User.objects.filter(
+                name = data["name"],
+                email = data["email"],
+                password = data["password"],
+                phone_number = data["phone_number"]
+                ).exists() == True:
+                return JsonResponse({"message" : "SUCCESS"}, status = 200)
+            else:    
+                return JsonResponse({"message" : 'VALIDATION_USER'}, status = 401)
+            
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
