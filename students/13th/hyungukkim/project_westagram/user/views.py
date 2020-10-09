c8f3cd7cacb8224889b997b06c505fa83afe802f
hukim <kho5420@gmail.com>
2020-10-09 16:18:37 +0900

    views.py 수정 httpie 테스트

--- students/13th/hyungukkim/project_westagram/user/views.py
+++ students/13th/hyungukkim/project_westagram/user/views.py
@@ -1,11 +1,12 @@
 import json
 
 from django.views import View
+from django.http import JsonResponse
 from user.models import Account
 
 class UserView(View):
 	def post(self, request):
-		data = json.load(requests.body)
+		data = json.loads(request.body)
 		
 		name = Account.objects.create(name=data['name'])
 		email = Account.objects.create(email=data['email'])
