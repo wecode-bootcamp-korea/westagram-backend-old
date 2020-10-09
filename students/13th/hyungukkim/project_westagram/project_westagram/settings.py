eccf000e2e8040c033158105472250b7c129771f
hukim <kho5420@gmail.com>
2020-10-09 15:45:51 +0900

    migrate

--- students/13th/hyungukkim/project_westagram/project_westagram/settings.py
+++ students/13th/hyungukkim/project_westagram/project_westagram/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project_westagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+		'USER': 'root',
+		'PASSWORD': 'q15396139!',
+		'HOST': '127.0.0.1',
+		'PORT': '3306',
     }
 }
 
