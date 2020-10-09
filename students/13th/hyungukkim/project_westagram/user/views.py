6ac9e0f53386dbac25d95561d6ba090a9002bd1a
hukim <kho5420@gmail.com>
2020-10-09 18:33:03 +0900

    views.py 정규표현식 추가

--- students/13th/hyungukkim/project_westagram/user/views.py
+++ students/13th/hyungukkim/project_westagram/user/views.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from django.views import View
 from django.http import JsonResponse
@@ -8,18 +9,34 @@ class UserView(View):
 	def post(self, request):
 		data = json.loads(request.body)
 
-		if (data['email'] == '') or (data['password'] == ''):
-			return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+		if (data['email'] == '') and (data['name'] == '') and (data['phone'] == ''):
+			return JsonResponse({'MESSAGE':'KEY_ERROR'}, status = 400)
 
-		if(len(data['password']) < 8):
-			return JsonResponse({'MESSAGE':'PASSWORD VALIDATION'}, status=400)
+		p = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+		if p.match(data['email'] != None) == False:
+			return JsonResponse({'MESSAGE':'EMAIL_VALIDATION'}, status = 400)
+
+		if Account.objects.filter(name = data['name']).exists() and (data['name'] != ''):
+			return JsonResponse({'MESSAGE':'NAME_DUPLICATED'}, status = 400)
+
+		if Account.objects.filter(email = data['email']).exists() and (data['email'] != ''):
+			return JsonResponse({'MESSAGE':'EMAIL_DUPLICATED'}, status = 400)
+
+		if Account.objects.filter(phone = data['phone']).exists() and (data['phone'] != ''):
+			return JsonResponse({'MESSAGE':'PHONE_DUPLICATED'}, status = 400)
+
+		if (data['email'] == '') or (data['password'] == ''):
+			return JsonResponse({'MESSAGE':'KEY_ERROR'}, status = 400)
+
+		if (len(data['password']) < 8):
+			return JsonResponse({'MESSAGE':'PASSWORD_VALIDATION'}, status = 400)
 
 		name = Account.objects.create(
-			name=data['name'], email=data['email'], phone=data['phone'], password=data['password']
+			name = data['name'], email = data['email'], phone = data['phone'], password = data['password']
 		)
 
-		return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+		return JsonResponse({'MESSAGE':'SUCCESS'}, status = 200)
 
 	def get(self, request):
 		account_data = Account.objects.values()
-		return JsonResponse({'account':list(account_data)}, status=200)
+		return JsonResponse({'account':list(account_data)}, status = 200)
