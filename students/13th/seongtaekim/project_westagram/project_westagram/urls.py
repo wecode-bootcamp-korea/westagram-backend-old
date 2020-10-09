2db1bc3ec5b373e6308c00d93c778394fc9950ab
kimst5418 <kimst5418@gmail.com>
2020-10-09 14:03:17 +0900

    [Mission 1] Django 초기 세팅 - Started django project - 13기 위스타 진행중

--- students/13th/seongtaekim/project_westagram/project_westagram/urls.py
+++ students/13th/seongtaekim/project_westagram/project_westagram/urls.py
@@ -1,4 +1,4 @@
-"""westagram URL Configuration
+"""project_westagram URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,9 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
 ]
