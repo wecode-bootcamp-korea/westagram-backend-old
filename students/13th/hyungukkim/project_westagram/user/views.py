750266ff2eefdb4ed63bf69dbfaac87d7296651a
hukim <kho5420@gmail.com>
2020-10-09 16:39:08 +0900

    views.py post() 수정

--- students/13th/hyungukkim/project_westagram/user/views.py
+++ students/13th/hyungukkim/project_westagram/user/views.py
@@ -8,9 +8,12 @@ class UserView(View):
 	def post(self, request):
 		data = json.loads(request.body)
 		
-		name = Account.objects.create(name=data['name'])
-		email = Account.objects.create(email=data['email'])
-		phone = Account.objects.create(phone=data['phone'])
-		password = Account.objects.create(password=data['password'])
+		name = Account.objects.create(
+			name=data['name'], email=data['email'], phone=data['phone'], password=data['password']
+		)
 
 		return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
+	def get(self, request):
+		account_data = Account.objects.values()
+		return JsonResponse({'account':list(account_data)}, status=200)
