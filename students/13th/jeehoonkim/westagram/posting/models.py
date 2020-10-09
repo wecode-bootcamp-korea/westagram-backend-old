772d12e8ec777ae112b55c68b13d563d335cceb2
jek <kore7290@gmail.com>
2020-10-09 17:54:03 +0900

    Add: added models for posting app

--- students/13th/jeehoonkim/westagram/posting/models.py
+++ students/13th/jeehoonkim/westagram/posting/models.py
@@ -3,3 +3,7 @@ from django.db import models
 from user.models import User
 class Posting(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
+    content=models.TextField(null=True)
+    image=models.URLField()
+    created_date=models.DateTimeField()
+    modified_date=models.DateTimeField(null=True)
