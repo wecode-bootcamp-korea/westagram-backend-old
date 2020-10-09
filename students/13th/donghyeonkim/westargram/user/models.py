fc6d03e9935bccaea93aa99bb55654840b40a40b
Dapane-dev <daphned@naver.com>
2020-10-09 15:15:03 +0900

    settings.py에 app 추가 및 사용자 테이블 생성

--- students/13th/donghyeonkim/westargram/user/models.py
+++ students/13th/donghyeonkim/westargram/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    email = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=11, unique=True)
+
+    class Meta:
+        db_table = 'user'
