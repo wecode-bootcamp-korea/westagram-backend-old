ce1594584aa9be0a5f588a95e8d45cdc3bffad28
hukim <kho5420@gmail.com>
2020-10-09 15:54:22 +0900

    models.py, views.py 수정

--- students/13th/hyungukkim/project_westagram/user/views.py
+++ students/13th/hyungukkim/project_westagram/user/views.py
@@ -1,1 +1,15 @@
+import json
 
+from django.views import View
+from user.models import Account
+
+class UserView(View):
+	def post(self, request):
+		data = json.load(requests.body)
+		
+		name = Account.objects.create(name=data['name'])
+		email = Account.objects.create(email=data['email'])
+		phone = Account.objects.create(phone=data['phone'])
+		password = Account.objects.create(password=data['password'])
+
+		return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
