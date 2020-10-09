68c9e446dc8d4e03756ad21ff47f03059224119d
hukim <kho5420@gmail.com>
2020-10-09 14:30:07 +0900

    urls, views 불필요한 코드 및 주석 삭제

--- project_westagram/settings.py
+++ project_westagram/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'user',
 	'corsheaders',
 ]
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'project_westagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+		'USER': 'root',
+		'PASSWORD': 'q15396139!',
+		'HOST': '127.0.0.1',
+		'PORT': '3306',
     }
 }
 
--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+	name = models.CharField(max_length=45)
+	email = models.CharField(max_length=45)
+	phone = models.CharField(max_length=45)
+	password = models.CharField(max_length=45)
+		
