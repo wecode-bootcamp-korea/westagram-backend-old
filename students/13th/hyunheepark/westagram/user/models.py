12eb46c97ccc606ad4da6f824834235c49553a67
Hyoniii <phhphh841@hanmail.net>
2020-10-10 17:57:29 +0900

    models.py 생성

--- students/13th/hyunheepark/westagram/user/models.py
+++ students/13th/hyunheepark/westagram/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_name = models.CharField(max_length=45)
+    user_email = models.EmailField(max_length=254)
+    user_password = models.CharField(max_length=25)
+
+    class Meta:
+        db_table = 'users'
