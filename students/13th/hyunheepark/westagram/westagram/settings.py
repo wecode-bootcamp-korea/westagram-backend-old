a2a5e22e9670b39f092f35d90a463da59813d378
Hyoniii <phhphh841@hanmail.net>
2020-10-10 18:47:54 +0900

    database 생성 & makemigration & migrate

--- students/13th/hyunheepark/westagram/westagram/settings.py
+++ students/13th/hyunheepark/westagram/westagram/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'westagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
 
