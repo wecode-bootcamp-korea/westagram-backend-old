2bc4810a5c1c8ec3b3d8277d663341d3745bc766
Min Gyeong-Min <tommin231@gmail.com>
2020-10-12 18:20:54 +0900

    Add : added utils.py which contains signin decorator

---
+++ students/13th/gyeongminmin/westagram/utils.py
@@ -0,0 +1,44 @@
+import os
+import jwt
+import json
+
+from pathlib import Path
+from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ImproperlyConfigured
+from auth.models import Users
+
+BASE_DIR = Path(__file__).resolve().parent
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as file:
+    secrets = json.loads(file.read())
+
+
+def get_secret(key, secrets=secrets):
+    try:
+        return secrets[key]
+    except KeyError:
+        raise ImproperlyConfigured('check secrets.json')
+
+
+def signin_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            token = request.headers.get('Auth', None)
+            key = get_secret('JWT_KEY')
+            algorithm = get_secret('JWT_ALGORITHM')
+
+            if token:
+                decode = jwt.decode(token, key, algorithm=algorithm)
+                user = Users.objects.get(id=decode['user'])
+                request.user = user
+        except jwt.DecodeError:
+            return JsonResponse({"message": "INVALID_TOKEN"}, status=403)
+        except Users.DoesNotExist:
+            return JsonResponse({'message': 'USER_DOES_NOT_EXIST'}, status=403)
+
+        return func(self, request, *args, **kwargs)
+
+    return wrapper
