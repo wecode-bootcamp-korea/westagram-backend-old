f46b42efb6f6ee8318e8ee77d5e73f5b6d79e76a
Hyoniii <phhphh841@hanmail.net>
2020-10-13 11:36:47 +0900

    posting/view PostView의 def post 수정

--- students/13th/hyunheepark/westagram/posting/views.py
+++ students/13th/hyunheepark/westagram/posting/views.py
@@ -8,11 +8,29 @@ from user.models    import User
 
 class PostView(View):
     def post(self,request):
-       # try:
-        data = json.loads(request.body)
-        user = data['user']
-        a = User.objects.get(name=user)
-        #return JsonResponse({'message':user},status=201)
+        try:
+            data = json.loads(request.body)
+                
+            #user_id = User.objects.get(name=data['name'])
+            user_id = data['user_id']
+            content = data['content']
+            img_url = data['img_url']
+       
+            if not data['img_url']:
+                 return JsonResponse({'MESSAGE':'이미지를 첨부하세요.'},status=400)
+            
+            else:
+                Post.objects.create(
+                    #user_id = user_id.id,
+                    user_id = user_id,
+                    content = content,
+                    img_url = img_url
+                )
+        
+            return JsonResponse({'MESSAGE':'SUCCESS'},status=201)
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KeyError'},status=400)
 
-        return JsonResponse({"user_name": a.name}, status=201)
+
+
 
