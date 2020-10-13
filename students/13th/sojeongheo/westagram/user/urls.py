97625f46dfdbc43e970ad63708cc7b273570ef68
sooo-20 <sojeong.heo@gmail.com>
2020-10-13 19:35:25 +0900

    Add: 로그인 엔드포인트 - views.py SignInView 생성 - urls.py /signin url 추가

--- students/13th/sojeongheo/westagram/user/urls.py
+++ students/13th/sojeongheo/westagram/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls    import path
-from .views         import SignUpView
+from .views         import SignUpView, SignInView
 
 urlpatterns = [
-        path('',SignUpView.as_view())
+        path('/signup',SignUpView.as_view()),
+        path('/signin',SignInView.as_view())
 ]
