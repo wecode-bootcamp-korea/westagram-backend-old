ed5ef773c3f01cdd6605e5718b0ff34f2e583a53
Hyoniii <phhphh841@hanmail.net>
2020-10-13 18:49:54 +0900

    암호화 & 토큰 발행

--- students/13th/hyunheepark/westagram/user/models.py
+++ students/13th/hyunheepark/westagram/user/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     name     = models.CharField(max_length=45,unique=True)
     phone    = models.CharField(max_length=30,unique=True)
     email    = models.EmailField(max_length=254,unique=True)
-    password = models.CharField(max_length=25)
+    password = models.CharField(max_length=100)
 
     class Meta:
         db_table = 'users'
--- students/13th/hyunheepark/westagram/user/views.py
+++ students/13th/hyunheepark/westagram/user/views.py
@@ -1,4 +1,6 @@
 import json
+import bcrypt
+import jwt
 
 from django.views import View
 from django.http  import JsonResponse,HttpResponse
@@ -33,16 +35,24 @@ class SignUpView(View):
                 return JsonResponse({'MESSAGE':'이미 가입된 전화번호  정보입니다'},status=400)
             
             else:
+                password         = data['password']
+                hashed_password  = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+                decoded_password = hashed_password.decode('utf-8') 
+                password         = decoded_password
+
                 User(
-                    name = data['name'],
-                    phone = data['phone'],
-                    email = data['email'],
-                    password = data['password']
+                    name     = data['name'],
+                    phone    = data['phone'],
+                    email    = data['email'],
+                    password = password
                     ).save()
                 return JsonResponse({'MESSAGE':'SUCCESS'},status=200)
 
-         except:
+         except KeyError:
              return JsonResponse({'MESSAGE':'KEYERROR'},status=400)
+         except ValueError:
+             return JsonResponse({'MESSAGE':'VALUEERROR'},status=400)
+
 
            
            
@@ -50,7 +60,7 @@ class SignInView(View):
     def post(self,request):
         #return JsonResponse({'MESSAGE':data},status=400)
         try:
-            data = json.loads(request.body)
+            data     = json.loads(request.body)
             
             if not data['name'] and not data['email'] and not data['phone']:
                 return JsonResponse({'MESSAGE':'계정을 입력하세요'},status=400)
@@ -61,37 +71,75 @@ class SignInView(View):
                 #return JsonResponse({'MESSAGE':'KEYERROR'},status=400)
             
             elif User.objects.filter(name=data['name']).exists():
-                user = User.objects.get(name=data['name'])
+                user        = User.objects.get(name=data['name'])
+                password    = data['password']
+                db_password = (user.password).encode()
+
+                if bcrypt.checkpw(password.encode('utf-8'),db_password):
                 
-                if user.password == data['password']:
-                    return JsonResponse({'MESSAGE':'SUCCESS'},status=200)
+                    #토큰 생성
+                    SECRET = 'secret'
+                    token  = jwt.encode({'id':user.id}, SECRET,algorithm = 'HS256')
+                    decoded_token  = token.decode('utf-8')
+                    print(decoded_token)
+                    
+                    return JsonResponse(
+                            {'MESSAGE':'SUCCESS','TOKEN':decoded_token},status=200)
+                
                 else:
                     return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
                 
             
             elif User.objects.filter(email=data['email']).exists():
-                user = User.objects.get(email=data['email'])
+                user        = User.objects.get(email=data['email'])
+                password    = data['password']
+                db_password = (user.password).encode()
+
+
+                if bcrypt.checkpw(password.encode('utf-8'),db_password):
+                    #토큰 생성
+                    SECRET = 'secret'
+                    token  = jwt.encode({'id':user.id}, SECRET,algorithm = 'HS256')
+                    decoded_token  = token.decode('utf-8')
+                    print(decoded_token)
+                    
+                    return JsonResponse(
+                            {'MESSAGE':'SUCCESS','TOKEN':decoded_token},status=200)
                 
-                if user.password == data['password']:
-                    return JsonResponse({'MESSAGE':'SUCCESS'},status=200)
                 else:
                     return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
                 
             elif User.objects.filter(phone=data['phone']).exists():
                 user = User.objects.get(phone=data['phone'])
+                password    = data['password']
+                db_password = (user.password).encode()
                 
-                if user.password == data['password']:
-                    return JsonResponse({'MESSAGE':'SUCCESS'},status=200)
+                if bcrypt.checkpw(password.encode('utf-8'),db_password):
+                    #토큰 생성
+                    SECRET = 'secret'
+                    token  = jwt.encode({'id':user.id}, SECRET,algorithm = 'HS256')
+                    decoded_token  = token.decode('utf-8')
+                    print(decoded_token)
+                    
+                    return JsonResponse(
+                            {'MESSAGE':'SUCCESS','TOKEN':decoded_token},status=200)
+                
+   
                 else:
-                    return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
+                    return JsonResponse(
+                            {'MESSAGE': 'INVALID_USER'}, status=401)
 
            # elif not User.objects.filter(name=data['name']).exists() and not User.objects.filter(email=data['email']).exists():
             #    return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
             else:
                 return JsonResponse({'MESSAGE': 'INVALID_USER(예외)'}, status=401)
 
-        except:
+        except KeyError:
             return JsonResponse({'MESSAGE':'KEYERROR'},status=400)
+        
+        except:
+            return JsonResponse({'MESSAGE':'ERROR'},status=400)
+
 
 
 
