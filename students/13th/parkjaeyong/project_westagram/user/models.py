219e4b9eb334b7c98dfb219fb8bc37e34476ad27
jaeyong <rmfrn2901@naver.com>
2020-10-13 12:47:39 +0900

    [박재용][Mission 2] class

--- students/13th/parkjaeyong/project_westagram/user/models.py
+++ students/13th/parkjaeyong/project_westagram/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    name  =models.CharField(max_length=30)
+    tel   =models.CharField(max_length=100)
+    email =models.EmailField(max_length=100)
+
+    class Meta:
+        db_table='user'
