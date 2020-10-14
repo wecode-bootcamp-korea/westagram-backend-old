9805181d8e9d2f8b3b892aff7d0f6dd64b35c395
Daniel Jeong <sh007.jeong@gmail.com>
2020-10-14 23:36:21 +0900

    Fix: 회원가입시 필수값 및 엔드포인트 변경 - FE에서 email(Account)과 pw만 보내므로 필수값을 해당 필드만으로 지정 - app 내 urls.py의 엔드포인트를 ''에서 '/signup'으로 변경
    Add: 로그인 기능 구현
    - Views.py: 로그인 클래스 생성 및 PW/token Hashing 로직 구현
    - urls.py:  로그인 엔드포인트 생성

--- students/13th/seunghojeong/project_westagram/project_westagram/user/views.py
+++ students/13th/seunghojeong/project_westagram/project_westagram/user/views.py
@@ -1,4 +1,6 @@
 import json
+import bcrypt
+import jwt
 
 from django.views import View
 from django.http  import JsonResponse, HttpResponse
@@ -13,11 +15,8 @@ class SignUpView(View):
             if Users.objects.filter(email=data['email']).exists():
                 return JsonResponse({"message": "EXISTS_EMAIL"}, status=400)
 
-            if Users.objects.filter(phone_number=data['phone_number']).exists():
-                return JsonResponse({"message": "EXISTS_PHONE_NUMBER"}, status=400)
-
-            if not data['email'] or not data['password']:
-                return JsonResponse({"message": "KEY_ERROR"}, status=400)
+#           if Users.objects.filter(phone_number=data['phone_number']).exists():
+#               return JsonResponse({"message": "EXISTS_PHONE_NUMBER"}, status=400)
 
             if len(data['password']) < 8:
                 return JsonResponse({"message": "PW_IS_TOO_SHORT"}, status=400)
@@ -31,19 +30,48 @@ class SignUpView(View):
 #               email        = data["email"],
 #               password     = data["password"],
 #               ).save()
+            hashed_pw = bcrypt.hashpw(
+                data["password"].encode("UTF-8"), bcrypt.gensalt()
+            )
 
             Users.objects.create(
-                name         = data["name"],
-                phone_number = data["phone_number"],
+            #    name         = data["name"],
+            #    phone_number = data["phone_number"],
                 email        = data["email"],
-                password     = data["password"],
+                password     = hashed_pw.decode("UTF-8")
             )
-            return JsonResponse({"message": "SUCCESS"}, status = 200)
+            return JsonResponse({"message": "SUCCESS"}, status=200)
 
-        except:
-            return JsonResponse({"message": "INVALID_ERROR"}, status = 400)
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
     def get(self, request):
         user_data = Users.objects.values()
-        return JsonResponse({"users":list(user_data)}, status = 200)
+        return JsonResponse({"users":list(user_data)}, status=200)
 
+class LoginView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        print(data)
+
+        try:
+            email_from_fe = data["email"]
+            pw_from_fe    = data["password"]
+
+
+            if Users.objects.filter(email=email_from_fe).exists():
+                user = Users.objects.get(email=email_from_fe)
+
+                if bcrypt.checkpw(pw_from_fe.encode("UTF-8"), user.password.encode("UTF-8")):
+                    token = jwt.encode({"email": email_from_fe}, "SECRET_KEY", algorithm = "HS256")
+                    token = token.decode("UTF-8")
+
+                    return JsonResponse({"message": "SUCCESS"}, status=200)
+#                   return JsonResponse({"token": token}, status=200)
+
+                return JsonResponse({"message": "INVALID_ERROR(WRONG_PW)"}, status=401)
+
+            return JsonResponse({"message": "INVALID_ERRPR(DOESNT EXIST ACCOUNT)"}, status=401)
+
+        except KeyError:
+            return JsonResponse({"messsage": "KEY_ERROR(ID OR PW MISSING)"}, status=400)
