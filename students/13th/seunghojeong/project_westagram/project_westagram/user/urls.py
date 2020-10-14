4ae58402242e14cc54906e607615954f1a6c0f8c
Daniel Jeong <sh007.jeong@gmail.com>
2020-10-14 23:36:21 +0900

    Fix: 회원가입시 필수값 및 엔드포인트 변경 - FE에서 email(Account)과 pw만 보내므로 필수값을 해당 필드만으로 지정 - app 내 urls.py의 엔드포인트를 ''에서 '/signup'으로 변경
    Add: 로그인 기능 구현
    - Views.py: 로그인 클래스 생성 및 PW/token Hashing 로직 구현
    - urls.py:  로그인 엔드포인트 생성

--- students/13th/seunghojeong/project_westagram/project_westagram/user/urls.py
+++ students/13th/seunghojeong/project_westagram/project_westagram/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views      import SignUpView
+from .views      import SignUpView, LoginView
 
 urlpatterns = [
-    path('', SignUpView.as_view())
+    path('/signup', SignUpView.as_view()),
+    path('/login',LoginView.as_view()),
 ]
