612d23ad63cf59a723591f8054bac6f116d19667
jaeyong <rmfrn2901@naver.com>
2020-10-14 11:15:11 +0900

    [박재용][Mission 3] test

--- students/13th/parkjaeyong/project_westagram/user/models.py
+++ students/13th/parkjaeyong/project_westagram/user/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Users(models.Model):
-    name  =models.CharField(max_length=30)
-    tel   =models.CharField(max_length=100)
-    email =models.EmailField(max_length=100)
+    name    = models.CharField(max_length=30)
+    tel     = models.CharField(max_length=100)
+    email   = models.EmailField(max_length=100)
+    password= models.IntegerField()
 
     class Meta:
         db_table='user'
