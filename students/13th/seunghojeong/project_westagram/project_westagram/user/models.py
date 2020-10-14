3e4bac04f914054e9dc1fb5d232cc498d1e05b40
Daniel Jeong <sh007.jeong@gmail.com>
2020-10-14 11:10:28 +0900

    Add: 회원가입 엔드포인트 구현 - models.py: 회원가입(정보) 클래스 생성 - view.py:   회원가입 함수 및 에러 메시지 작성 - urls.py:   view.py 호출문 작성

--- students/13th/seunghojeong/project_westagram/project_westagram/user/models.py
+++ students/13th/seunghojeong/project_westagram/project_westagram/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    name         = models.CharField(max_length = 50)
+    phone_number = models.CharField(max_length = 50)
+    email        = models.CharField(max_length = 200)
+    password     = models.CharField(max_length = 100)
+    created_at   = models.DateTimeField(auto_now_add = True)
+    updated_at   = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        db_table = 'Users'
