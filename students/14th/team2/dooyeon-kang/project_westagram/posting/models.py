bd96f2e590ebfb1ffa21d092f52862c606b98130
Dooyeon Kang <dyhouse0517@gmail.com>
2020-11-05 15:10:59 +0900

    [Mission 4-2] Posting model 생성

--- students/14th/team2/dooyeon-kang/project_westagram/posting/models.py
+++ students/14th/team2/dooyeon-kang/project_westagram/posting/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+#from user.models import User
 
-# Create your models here.
+class Posting(models.Model):
+    image_url   = models.CharField(max_length=1000)
+    description = models.TextField(null=True)
+    created_at  = models.DateTimeField(auto_now_add = True)
+    updated_at  = models.DateTimeField(auto_now = True)
+    user        = models.ForeignKey('user.User', related_name = 'user', on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'postings'
