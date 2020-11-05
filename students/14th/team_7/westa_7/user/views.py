fbaf33182bcddd0e011917035fe4f2be09633525
dev-swd <tladnjsen@gmail.com>
2020-11-05 11:08:28 +0900

    commit deleted

--- students/14th/team_7/westa_7/user/views.py
+++
@@ -1,8 +0,0 @@
-import json
-from django.views import View
-from django.http import JsonResponse
-from .models import Users
-
-class UsersView(View):
-    def get(self, request):
-        return JsonResponse({"users_view": "hello"}, status=200)
