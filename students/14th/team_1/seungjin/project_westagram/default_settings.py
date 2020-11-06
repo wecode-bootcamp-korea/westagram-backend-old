94f642af112750bd00b6d43a727791e6ca585ac2
jin <jinybear@gmail.com>
2020-11-06 15:13:46 +0900

    Add: default_settings.py, Update: settings.py

---
+++ students/14th/team_1/seungjin/project_westagram/default_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+#SECRET = {
+#        'secret':'wecode14',
+#}
