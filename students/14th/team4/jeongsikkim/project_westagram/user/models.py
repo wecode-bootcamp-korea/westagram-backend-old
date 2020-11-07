22001a5a55f41c78e8cbef60992dcc36ab4fa6ce
jeongsik <summering6@gmail.com>
2020-11-07 13:08:24 +0900

    updated mission 3

--- students/14th/team4/jeongsikkim/project_westagram/user/models.py
+++ students/14th/team4/jeongsikkim/project_westagram/user/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    user_id = models.CharField(max_length = 50)
+    user_id       = models.CharField(max_length = 50)
     mobile_number = models.CharField(max_length=13)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length = 50)
+    email         = models.EmailField(max_length=100)
+    password      = models.CharField(max_length = 50)
 
     class Meta:
         db_table = 'users'
--- students/14th/team4/jeongsikkim/project_westagram/user/views.py
+++ students/14th/team4/jeongsikkim/project_westagram/user/views.py
@@ -8,39 +8,51 @@ class UserView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
+
             if "@" not in data['email'] or "." not in data['email']:
                 return JsonResponse({"message":"email error!"}, status=400)
+
             elif len(data['password']) < 8:
                 return JsonResponse({"message":"password error!"},status=400)
 
-            elif User.objects.filter(Q(user_id = data['user_id']) |
-                                     Q(mobile_number = data['mobile_number']) |
-                                     Q(email = data['email'])):
+            elif User.objects.filter(
+                Q(user_id       = data['user_id']) |
+                Q(mobile_number = data['mobile_number']) |
+                Q(email         = data['email'])
+            ):
                 return JsonResponse({"message":"already in use"}, status=400)
 
             User.objects.create(
-                user_id = data['user_id'],
+                user_id       = data['user_id'],
                 mobile_number = data['mobile_number'],
-                email = data['email'],
-                password = data['password']
+                email         = data['email'],
+                password      = data['password']
             )
+
             return JsonResponse({"message":"Sign up SUCCESS!"}, status=200)
+
         except KeyError :
             return JsonResponse({"message":"KeyError!"}, status=400)
 
 class LogInView(View):
     def post(self,request):
         data = json.loads(request.body)
+
         if 'account' not in data:
             return JsonResponse({"message":"KeyError!"}, status=400)
+
         if 'password' not in data:
             return JsonResponse({"message":"KeyError!"}, status=400)
-        account = User.objects.filter(Q(user_id=data['account']) |
-                                       Q(mobile_number=data['account']) |
-                                       Q(email=data['account']))
-#        print(data)
+
+        account = User.objects.filter(
+            Q(user_id       = data['account']) |
+            Q(mobile_number = data['account']) |
+            Q(email         = data['account'])
+        )
+
         if not account:
             return JsonResponse({"message":"invalid Error!"}, status=401)
+
         if account.values()[0]['password'] != data['password']:
             return JsonResponse({"message":"invalid Error!"}, status=401)
 
