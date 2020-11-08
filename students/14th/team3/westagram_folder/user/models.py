a7043c5b945907886818ffc703a6c71d7c091430
Noah Shin <noahshin11@gmail.com>
2020-11-08 14:07:00 +0900

    edited all

--- students/14th/team3/westagram_folder/user/models.py
+++ students/14th/team3/westagram_folder/user/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
-class Users(models.Model):
-    user_name       = models.CharField(max_length=30)
+class Account(models.Model):
+    name            = models.CharField(max_length=30)
     phone_number    = models.CharField(max_length=30)
     email           = models.EmailField(max_length=30)
     password        = models.CharField(max_length=20)
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
+
     class Meta:
-        db_table    = 'users_info'
+        db_table    = 'accounts'
+
--- students/14th/team3/westagram_folder/user/views.py
+++ students/14th/team3/westagram_folder/user/views.py
@@ -1,50 +1,57 @@
 import json
 from django.views import View
-from django.http import JsonResponse
-from .models import Users
-# Create your views here.
+from django.http  import JsonResponse
+from .models  import Account
+# 치킨먹고싶다...
 class SignUpView(View):
-    def post(self, request):
+    def post(self, request): #포스트로 받을 시 저장
         data = json.loads(request.body)
-        User(
-            user_name    = data['user_name'],
-            phone_number = data['phone_number'],
-            email        = data['email'],
-            password     = data['password'],
-        )
-        try:
+        signupdb = Account.objects.all() #유저 테이블정보 다 가져와
+        try:        
+            user = Account(
+                name         = data['name'],
+                phone_number = data['phone_number'],
+                email        = data['email'],
+                password     = data['password'])
             if "@" not in data['email'] or "." not in data['email']:
-                return JsonResponse({"message" : "email_error"},     status = 400)
+                return JsonResponse({"message" : "email_error"}, status = 400)
             elif len(data['password']) < 8 :
-                return JsonResponse({"message" : "short_password"},  status = 400)
-            elif Users.objects.filter(email = data['email']).exists() or Users.objects.filter(user_name = data['user_name']).exists() or Users.objects.filter(phone_number = data['phone_number']).exists() :
+                return JsonResponse({"message" : "short_password"}, status = 400)
+            elif Account.objects.filter(email = data['email']).exists() or Account.objects.filter(name = data['name']).exists() or Account.objects.filter(phone_number = data['phone_number']).exists() :
                 return JsonResponse({"message" : "duplicated_information"}, status = 401)
             else:
-                Users.objects.create(
-                    user_name    = data['user_name'],
+                Account.objects.create(
+                    name         = data['name'],
                     phone_number = data['phone_number'],
                     email        = data['email'],
-                    password     = data['password'],
-                    )
-                return JsonResponse({"message" : "SUCCESS"},         status = 201)
+                    password     = data['password'])
+                return JsonResponse({"message" : "SUCCESS"},                status = 201)
         except KeyError:
-            return  JsonResponse({"message": "KEY_ERROR"}, status = 400)
+            return JsonResponse({"message" : "KEY_ERROR"},                  status = 400)
 
+    def get(self, request): #겟으로 받을시 유저테이블에 있는것 출력
+        account_data = Account.objects.values()
+        return JsonResponse({'Account Data' : list(account_data)}, status = 200)
 
+class LoginView(View):
+    def post(self, request):
+        data        = json.loads(request.body)
+        user_password = data['password']
 
-#            if User.objects.filter(email = data['email']) and User.objects.filter(password = data['password']) != True:
-#                #이메일이나 패스워드가 하나라도 전달되지 않았을 시 에러 반환
-#                return  JsonResponse({"message": "KEY_ERROR"}, status = 400)
-#            elif User.objects.filter(user_name = data['user_name']).exists() or User.objects.filter(phone_number = data['phone_number']).exists() or User.objects.filter(phone_number = data['phone_number']).exists() == True:
-#                return JsonResponse({"message" : "중복되는 정보입니다."}, status = 401)
-#            elif User.objects.filter(user_name = data['user_name']) and User.objects.filter(email = data['email']) == False:
-#                return JsonResponse 에러 처리
-#            elif "@" and "." not in User.objects.filter(email = data['email'])
-#                return 에러!!!
-#            elif User.objects.filter(password = data['password']) <= 8:
-#                return 에러 (password validation)
-#            else:
-#                User.objects.create(user_name = data['user_name'], email = data['email'], password = data['password'])
-#                return JsonResponse({"message" : "SUCCESS"}, status = 200)
-#            
-    
+        try:
+            if 'name' not in data and 'email' not in data and 'phone_number' not in data:
+                return JsonResponse({'message' : 'KEY_ERROR'},   status = 400)
+            elif 'password' not in data:
+                return JsonResponse({'message' : 'KEY_ERROR'},   status = 400)
+            elif Account.objects.filter(email = data['email']).exists(): #계정이 존재한다면
+                exist_user = Account.objects.get(email = data['email'])
+                if exist_user.password == data['password']: #존재계정의 패스워드와 입력된 패스워드가 같다면
+                    return JsonResponse({"message" : "SUCCESS"}, status = 200)
+                return JsonResponse({"message" : "INVALID_USER"}, status = 401)
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEY_ERROR'},  status = 400)
+
+    def get(self, request): #get메쏘드로 회원가입된 데이터들을 list형태로 반환
+        account_data = Account.objects.values()
+        return JsonResponse({'Login' : list(account_data)}, status = 200)
+        
