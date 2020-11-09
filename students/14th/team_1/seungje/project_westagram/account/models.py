9da91fcac0c8b889d37da9a4e80c2b75a9746cf2
parkseungje <tmdwprpdla@naver.com>
2020-11-09 12:31:19 +0900

    로그인창까지 생성

--- students/14th/team_1/seungje/project_westagram/account/models.py
+++ students/14th/team_1/seungje/project_westagram/account/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Account(models.Model):
 
     user_name = models.CharField(max_length=40,default='')
-    user_number = models.IntegerField(default='')
+    user_number = models.CharField(max_length=40,default='')
     user_email = models.CharField(max_length=40,default='')
 
-    user_password = models.CharField(max_length=40)
+    user_password = models.CharField(max_length=40,default='')
 
     class Meta:
         db_table = 'accounts'
--- students/14th/team_1/seungje/project_westagram/account/urls.py
+++ students/14th/team_1/seungje/project_westagram/account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import SignUp 
+from .views import SignUp,SignIn 
 urlpatterns=[
 path('signup', SignUp.as_view()),
-
+path('signin', SignIn.as_view())
 ]
