ec592acd3fcbe1cac777e9492cded455c8397605
GyuSeok <gyskjng@gmail.com>
2020-11-10 14:26:19 +0900

    MODIFY: 게시글 등록(posting app) 관련 수정     - 테이블 데이터 복수형으로 변경     - Comment 모델 attiribute명 수정 (data -> created_at)     - Post 모델 attiribute명 수정 (date -> created_at)     - 모델명과 테이블명 일치하도록 변경

--- students/14th/team_6/gyuseok/project_westagram/posting/models.py
+++ students/14th/team_6/gyuseok/project_westagram/posting/models.py
@@ -2,19 +2,21 @@ from django.db import models
 
 
 class Post(models.Model):
-    user     = models.ForeignKey('user.User',on_delete=models.CASCADE)
-    date     = models.DateTimeField(auto_now=True)
-    image    = models.CharField(max_length=1000)
-    content  = models.TextField(null=True)
+    user        = models.ForeignKey('user.User',on_delete=models.CASCADE)
+
+    created_at  = models.DateTimeField(auto_now_add=True)
+    image       = models.URLField(max_length=1000)
+    content     = models.TextField(null=True)
 
     class Meta:
-        db_table = 'board'
+        db_table = 'posts'
 
 class Comment(models.Model):
-    user     = models.ForeignKey('user.User', on_delete=models.CASCADE)
-    post     = models.ForeignKey('Post', on_delete=models.CASCADE)
-    content  = models.TextField(null=True)
-    data     = models.DateTimeField(auto_now_add=True)
+    user        = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    post        = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    created_at  = models.DateTimeField(auto_now_add=True)
+    content     = models.TextField(null=True)
 
     class Meta:
-        db_table = 'comment'
+        db_table = 'comments'
