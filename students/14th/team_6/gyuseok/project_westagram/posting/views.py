dc75ee80e3db3617e492f2abea30e27df9e99899
GyuSeok <gyskjng@gmail.com>
2020-11-10 18:59:58 +0900

    MODIFY: 게시글 등록(posting app) View 수정     - KEY_ERROR(헤더 검사) lambda 함수으로 변경     - 리턴 message 일관성 있게 변경 (NOT_EXIST_USER)     - 분리된 뷰 통합     - import 구분

--- students/14th/team_6/gyuseok/project_westagram/posting/views.py
+++ students/14th/team_6/gyuseok/project_westagram/posting/views.py
@@ -1,55 +1,51 @@
 import json
+
 from django.views import View
 from django.http  import JsonResponse
-from .models import Post, Comment
-from user.models import User
 from django.db.models import Q
 
-from django.template import loader
+from .models import Post, Comment
+from user.models import User
 
-class CreateBoardView(View):
+class BoardView(View):
     def post(self, request):
 
-        NECESSERY_KEYS = ('username', 'content', 'image_url')
+        NECESSERY_KEYS = ('user_id', 'content', 'image_url')
         data = json.loads(request.body)
 
-        for k in NECESSERY_KEYS:
-            if k not in data.keys():
-                return JsonResponse({'message': 'KEY_ERROR'}, status=400)
-
-
-        if User.objects.filter(name=data['username']).exists():
-            key = User.objects.get(name=data['username'])
-            Post.objects.create( content = data['content'],
-                                 image   = data['image_url'],
-                                 user    = key
-                               )
+        if list(filter(lambda x: x not in data.keys(),NECESSERY_KEYS)):
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        try:
+            post = Post.objects.get( content = data['content'],
+                                     image   = data['image_url'],
+                                     user    = data['user_id'],
+                                    )
+            post.save()
             return JsonResponse({'message': 'SUCCESS'}, status=201)
-        else:
-            return JsonResponse({'message': 'NOT EXIST USER'}, status=400)
 
+        except Post.DoesNotExist:
+            return JsonResponse({'message': 'NOT_EXIST_USER'}, status=400)
 
-class ReadBoardView(View):
     def get(self, request):
         return JsonResponse({'data' : list(Post.objects.values())}, status=200)
 
-class CreateCommentView(View):
+class CommentView(View):
     def post(self, request):
         NECESSERY_KEYS = ('user_id', 'post_id', 'content')
         data = json.loads(request.body)
 
         for k in NECESSERY_KEYS:
             if k not in data.keys():
-                return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+                return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
 
         if User.objects.filter(id=data['user_id']).exists() and \
            Post.objects.filter(id=data['post_id']).exists():
 
            Comment.objects.create(
-                user    = User.objects.get(id=data['user_id']),
-                post    = Post.objects.get(id=data['post_id']),
+                user    = data['user_id'],
+                post    = data['post_id'],
                 content = data['content'],
            )
            return JsonResponse({'message': 'SUCCESS'}, status=201)
-        else:
-           return JsonResponse({'message': 'NOT EXIST USER'}, status=400)
+
+        return JsonResponse({'message': 'NOT_EXIST_USER'}, status=400)
