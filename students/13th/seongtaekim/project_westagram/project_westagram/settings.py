d90d070488be945313b480fca6e14aee09fc9aa0
soheon-lee <lsheon93@gmail.com>
2020-11-30 21:25:09 +0900

    wip

--- students/13th/seongtaekim/project_westagram/project_westagram/settings.py
+++ students/13th/seongtaekim/project_westagram/project_westagram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n@l&t&qc&x@k0$tknc)6cnv%l^adf=4c6ugc6yr!j9@z^4ofdi'
+SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
