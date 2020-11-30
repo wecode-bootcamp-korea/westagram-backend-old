81bc4367e1b5b059b3d64554e64dedeec63e8e99
soheon-lee <lsheon93@gmail.com>
2020-11-30 21:23:16 +0900

    wip

--- posting/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls    import path
-from .views         import Post, Comment
-
-urlpatterns =[
-    path('post/',Post.as_view()),
-    path('comment/',Comment.as_view()),
-]
--- user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls    import path
-from .views         import SignUp, Login
-
-urlpatterns =[
-    path('signup/',SignUp.as_view()),
-    path('login/',Login.as_view()),
-]
