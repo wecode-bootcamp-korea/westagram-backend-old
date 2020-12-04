c999e2ac8f9bb7bc521932bc56ba9e6259a0d79d
kst6294 <kst6294@gmail.com>
2020-12-04 16:13:09 +0900

    third commit

--- students/15th/team1/suntae_kim/posting/models.py
+++ students/15th/team1/suntae_kim/posting/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+
+from user.models import User
 
 # Create your models here.
+
+
+class Board(models.Model):
+#    user = models.ForeignKey('User', on_delete=models.CASCADE)
+#    submit_time = models.DateTimeField(default = timezone.now, null=True)
+    username = models.CharField(max_length=45, null=True)
+    image_url = models.CharField(max_length=1000, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    class Meta:
+        db_table = 'boards'
+
--- students/15th/team1/suntae_kim/posting/views.py
+++ students/15th/team1/suntae_kim/posting/views.py
@@ -2,18 +2,33 @@ import json
 
 from django.http import JsonResponse
 from django.views import View
+from django.utils import timezone
 
 from user.models import User
+from posting.models import Board
 
 # Create your views here.
 
 
 class Posting(View):
-    def posting(self, request):
+    def post(self, request):
+        data = json.loads(request.body)
         try:
-            data = json.loads(request.body)
+            user_info = data['username']
+            image_url = data['image_url']
+            # get datas from body of request
+            # user_info related with User model  of foreignKey from user.models.py
+#            user_id =  User.objects.get(username = data['username']).id
+#            Board.objects.create(user=user_id, image_url=image_url)
 
-            username = data['username']
-            password = data['password']
+            u1 = User.objects.get(id=1)
+
+
+
+
+            Board.objects.create(username=user_info, image_url=image_url)
+
+            return JsonResponse({u1 : 'SUCCESS'})
         except:
-            pass
+            return JsonResponse({u1 : 'ERROR'})
+
