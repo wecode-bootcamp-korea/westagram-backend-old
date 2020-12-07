1044dfa883b8f26e167e160e3bbad646faf529cc
mincheolkk <kmc04080408@gmail.com>
2020-12-07 20:03:57 +0900

    sign up 구현됨

--- students/15th/team3/user/models.py
+++ students/15th/team3/user/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class SignUp(models.Model):
     email = models.EmailField(max_length=45,unique=False,null=True)
     tel = models.CharField(max_length=20,unique=False,null=True)
-    name = models.CharField(max_length=30,unique=False,null=True)
+    name = models.CharField(max_length=30,null=True)
     password = models.CharField(max_length=45,null=True)
 
 
