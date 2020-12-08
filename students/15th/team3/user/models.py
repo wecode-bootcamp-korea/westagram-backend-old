cba53b50c60eff0a3df2a13373f80ead02732408
mincheolkk <kmc04080408@gmail.com>
2020-12-08 23:47:48 +0900

    sign in 구현됨

--- students/15th/team3/user/models.py
+++ students/15th/team3/user/models.py
@@ -4,10 +4,11 @@ from django.db import models
 class SignUp(models.Model):
     email = models.EmailField(max_length=45,unique=False,null=True)
     tel = models.CharField(max_length=20,unique=False,null=True)
-    name = models.CharField(max_length=30,null=True)
+    name = models.CharField(max_length=30,blank=True,null=True)
     password = models.CharField(max_length=45,null=True)
 
 
 
     class Meta:
         db_table = 'signups'
+
--- students/15th/team3/user/views.py
+++ students/15th/team3/user/views.py
@@ -24,11 +24,12 @@ class SignUpView(View):
                         if len(data['password']) < 8:
                             return JsonResponse({"message": "your password is dangerous."}, status= 401)
 
-                        if '@' not in data['email']:
+                        if ('@' not in data['email']) or ('.' not in data['email']):
                             return JsonResponse({'message':'its not email format'},status=401)
 
-                        if SignUp.objects.filter(email = data['email']).exists() and SignUp.objects.filter(name = data['name']).exists() and SignUp.objects.filter(tel = data['tel']).exists():
+                        if SignUp.objects.filter(name = data['name']).exists() and SignUp.objects.filter(email = data['email']).exists() and SignUp.objects.filter(tel = data['tel']).exists():
                             return JsonResponse({'message':'your information is already registered.'},status=400)
+
 
 
                         SignUp(
@@ -43,4 +44,33 @@ class SignUpView(View):
                 return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
 
+
+class SignIn(View):
+    def post(self,request):
+        data = json.loads(request.body)
+        data['email'] = data.get('email')
+        data['tel'] = data.get('tel')
+        data['name'] = data.get('name')
+
+        try:
+            if data['name'] is not None:
+                if SignUp.objects.filter(name=data['email'], password=data['name']).exists():
+                    return JsonResponse({'message': 'SUCCESS'}, status=200)
+                else:
+                    return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+            if data['email'] is not None:
+                if SignUp.objects.filter(email=data['email'], password=data['password']).exists():
+                    return JsonResponse({'message': 'SUCCESS'}, status=200)
+                else:
+                    return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+            if data['tel'] is not None:
+                if SignUp.objects.filter(tel=data['email'], password=data['password']).exists():
+                    return JsonResponse({'message': 'SUCCESS'}, status=200)
+                else:
+                    return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+        except:
+             return JsonResponse ({'message': 'KEY_ERROR'}, status=400)
 
