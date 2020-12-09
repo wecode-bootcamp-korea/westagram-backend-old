6e5680622fc8de5f4343b072de242373d1c9b3e6
YEOJU SEOK <seok@YEOJUui-MacBookPro.local>
2020-12-09 23:37:14 +0900

    signin/singup

--- students/15th/team3/yeoju-seok/westagram/settings.py
+++ students/15th/team3/yeoju-seok/westagram/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'user',
 ]
 
 MIDDLEWARE = [
