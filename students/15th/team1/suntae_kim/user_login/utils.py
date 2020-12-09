29f379d93304711b9cf7dbe0e282ba9ca3d236ec
kst6294 <kst6294@gmail.com>
2020-12-09 10:50:08 +0900

    login endpoint complete

---
+++ students/15th/team1/suntae_kim/user_login/utils.py
@@ -0,0 +1,35 @@
+import json
+import os
+import sys
+import jwt
+import bcrypt
+
+from user.models import User
+from django.http import JsonResponse
+
+import user_login
+from my_settings import SECRET_KEY
+
+def LoginAuthorization(func):
+    def wrapper(self, request):
+        try:
+            data = json.loads(request.body)
+
+            username = data['username']
+            password = data['password'].encode('utf-8')
+
+            user_info = User.objects.get( username = username )
+            user_id = user_info.id
+            user_password = user_info.password
+#            hashed_password = user_info.password.encode('utf-8')
+            hashed_password = user_password.encode('utf-8')
+
+            if bcrypt.checkpw(password, hashed_password):
+                access_token = jwt.encode({'id' : user_id}, SECRET_KEY, algorithm = 'HS256').decode('utf-8')
+                return JsonResponse({'Error' : hashed_password.decode('utf-8')})
+            else:
+                return JsonResponse({'MESSAGE' : 'ERROR'})
+        except:
+            return JsonResponse({'MESSAGE' : 'INVALID_USER'})
+        return func(self, request)
+    return wrapper
--- students/15th/team1/suntae_kim/user_login/views.py
+++ students/15th/team1/suntae_kim/user_login/views.py
@@ -8,34 +8,19 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views import View
 
-# sys.path.append(
-#    sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-# )
-
-#from suntae_kim.user.models import User
 from user.models import User
+from user_login.utils import LoginAuthorization
 
 class UserLogin(View):
     def __init__(self):
         pass
+
+    @LoginAuthorization
     def post(self, request):
         try:
             data = json.loads(request.body)
             username        = data['username']
-            password        = data['password'].encode('utf-8')
-            user_info       = User.objects.get(username = username)
-            user_id         = user_info.id
-            hashed_password = user_info.password.encode('utf-8')
+            password        = data['password']
 
-            if bcrypt.checkpw(password, hashed_password):
-                SECRET = 'this is my secret'
-                access_token = jwt.encode({'id' : user_id}, SECRET, algorithm = 'HS256').decode('utf-8')
-
-
-                return JsonResponse({'Token' : access_token })
-
-            else:
-                return JsonResponse({'MESSAGE' : 'INVALID_USER'})
-
-        except KeyError:
+        except ValueError:
             return JsonResponse({'MESSAGE' : 'KEY_ERROR'})
