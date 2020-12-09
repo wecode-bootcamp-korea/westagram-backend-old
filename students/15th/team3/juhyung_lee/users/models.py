4ce292136cffc6138d4c88afb8b04a37260cc09a
Jacob <jacob.juhyung.lee@gmail.com>
2020-12-09 18:53:41 +0900

    인증 & 인가 완료

--- students/15th/team3/juhyung_lee/users/models.py
+++ students/15th/team3/juhyung_lee/users/models.py
@@ -3,7 +3,7 @@ class User(models.Model):
     username       = models.CharField(max_length=20, null=True, blank=True)
     email          = models.CharField(max_length=30, null=True, blank=True)
     phone_number   = models.CharField(max_length=15, null=True, blank=True)
-    password       = models.CharField(max_length=20)
+    password       = models.CharField(max_length=256)
 
     class Meta:
         db_table = 'users'
--- students/15th/team3/juhyung_lee/users/views.py
+++ students/15th/team3/juhyung_lee/users/views.py
@@ -1,10 +1,13 @@
 import json
+import bcrypt
+import jwt
 
-from django.views     import View
-from django.http      import JsonResponse
-from django.db.models import Q
+from django.views       import View
+from django.http        import JsonResponse
+from django.db.models   import Q
 
-from .models          import User
+from westagram.settings import SECRET_KEY
+from .models            import User
 
 
 class SignUpView(View):
@@ -25,11 +28,13 @@ class SignUpView(View):
             if len(data['password']) < minimum_password:
                 return JsonResponse({'message': 'TOO_SHORT_PASSWORD'}, status = 400)
 
+            hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt())
+
             User(
                 username        = data['username'],
                 email           = data['email'],
                 phone_number    = data['phone_number'],
-                password        = data['password'],
+                password        = hashed_password.decode('utf-8'),
             ).save()
             return JsonResponse({'message': 'SUCCESS'}, status = 200)
 
@@ -45,10 +50,16 @@ class SignInView(View):
         data['phone_number'] = data.get('phone_number')
 
         try:
+        #    if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+        #        token = jwt.encode({'username' : data['username']}, SECRET_KEY, algorithm = "HS256")
+        #        token = token.decode('utf-8')
+
             if data['username'] is not None:
                 if User.objects.filter(username=data['username']).exists():
                     user = User.objects.get(username=data['username'])
-                    if user.password == data['password']:
+                    if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                        token = jwt.encode({'username' : data['username']}, SECRET_KEY, algorithm = "HS256")
+                        token = token.decode('utf-8')
                         return JsonResponse({'message': 'SUCCESS'}, status = 200)
                     elif user.password != data['password']:
                        return JsonResponse({'message': 'INVALID_USER'}, status = 400)
@@ -56,19 +67,21 @@ class SignInView(View):
             if data['email'] is not None:
                 if User.objects.filter(email=data['email']).exists():
                     user = User.objects.get(email=data['email'])
-                    if user.password == data['password']:
+                    if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                        token = jwt.encode({'email' : data['email']}, SECRET_KEY, algorithm = "HS256")
+                        token = token.decode('utf-8')
                         return JsonResponse({'message': 'SUCCESS'}, status = 200)
                     elif user.password != data['password']:
                         return JsonResponse({'message': 'INVALID_USER'}, status = 400)
-
 
             if data['phone_number']is not None:
                 if User.objects.filter(phone_number=data['phone_number']).exists():
                     user = User.objects.get(phone_number=data['phone_number'])
-                    if user.password == data['password']:
+                    if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                        token = jwt.encode({'phone_number' : data['phone_number']}, SECRET_KEY, algorithm = "HS256")
+                        token = token.decode('utf-8')
                         return JsonResponse({'message': 'SUCCESS'}, status = 200)
                     elif user.password != data['password']:
                         return JsonResponse({'message': 'INVALID_USER'}, status = 400)
-
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
