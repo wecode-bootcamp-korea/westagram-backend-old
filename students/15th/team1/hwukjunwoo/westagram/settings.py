7280c97e3a48ce8a3b89096cacf74ab094d620ed
hwukjunwoo <hwukjunwoo@gmail.com>
2020-12-09 14:06:46 +0900

    PR revised version

--- students/15th/team1/hwukjunwoo/westagram/settings.py
+++ students/15th/team1/hwukjunwoo/westagram/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*'] 
 
 # Application definition
 
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'user',
 ]
 
 MIDDLEWARE = [
