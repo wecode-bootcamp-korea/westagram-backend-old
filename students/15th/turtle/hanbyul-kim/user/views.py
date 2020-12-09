2f85970596d5dbed891fe35f2190d68bc235b52e
Luceta <57941164+Luceta@users.noreply.github.com>
2020-12-10 00:32:21 +0900

    westagram log-in endpoint

--- students/15th/turtle/hanbyul-kim/user/views.py
+++ students/15th/turtle/hanbyul-kim/user/views.py
@@ -50,3 +50,39 @@ class UserView(View):
         except User.DoesNotExist:
             return JsonResponse({'message':'ACCOUNT_DOES_NOT_EXIST'}, status=400)
 
+
+class SigninView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+        try:
+            data['name']    = data.get('name')
+            data['mobile_number'] = data.get('mobile_number')
+            data['email'] = data.get('email')
+            data['password'] = data.get('password')
+
+            if data['name'] is None and data['mobile_number'] is None:
+                if User.objects.filter(email=data['email']).exists():
+                    user = User.objects.get(email=data.get('email'))
+                    if user.password == data['password']:
+                        return JsonResponse({"message":"SUCCESS"}, status=200)
+                    return JsonResponse({"message":"password is not valid"},status=400)
+
+            #번호만 잇는 경우
+            if data['name'] is None and data['email'] is None:
+                if User.objects.filter(mobile_number=data.get("mobile_number")).exists():
+                    user = User.objects.get(mobile_number= str(data['mobile_number']))
+
+                    if user.password == data['password']:
+                        return JsonResponse({"message":"SUCCESS"}, status=200)
+                    return JsonResponse({"message":"number is not valid"},status = 400)
+
+           # 이름만 잇는 경우
+            if data['email'] is None and data['mobile_number'] is None:
+                if User.objects.filter(name=data['name']).exists():
+                    user = User.objects.get(name=data['name'])
+                    if user.password == data['password']:
+                        return JsonResponse({"message":"SUCCESS"}, status=200)
+                    return JsonResponse({"message":"name is not valid"},status =400)
+
+        except KeyError as e:
+            return JsonResponse({"message":e.message},status=400)
