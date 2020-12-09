9d86fe79338d09b5f13104ca4aa5f19bcc0967f6
kst6294 <kst6294@gmail.com>
2020-12-09 21:09:02 +0900

    user login update

--- students/15th/team1/suntae_kim/user/utils.py
+++ students/15th/team1/suntae_kim/user/utils.py
@@ -17,18 +17,20 @@ def LoginAuthorization(func):
             username = data['username']
             password = data['password'].encode('utf-8')
 
-            user_info = User.objects.get( username = username )
-            user_id = user_info.id
-            user_password = user_info.password
-#            hashed_password = user_info.password.encode('utf-8')
+            user_info       = User.objects.get( username = username )
+            user_id         = user_info.id
+            user_password   = user_info.password
             hashed_password = user_password.encode('utf-8')
 
             if bcrypt.checkpw(password, hashed_password):
                 access_token = jwt.encode({'id' : user_id}, SECRET_KEY, algorithm = 'HS256').decode('utf-8')
-                return JsonResponse({'MESSAGE' : hashed_password.decode('utf-8')})
+                return JsonResponse({'TOKEN' : hashed_password.decode('utf-8')})
+
             else:
                 return JsonResponse({'MESSAGE' : 'ERROR'})
         except:
             return JsonResponse({'MESSAGE' : 'INVALID_USER'})
+
         return func(self, request)
+
     return wrapper
--- students/15th/team1/suntae_kim/user/views.py
+++ students/15th/team1/suntae_kim/user/views.py
@@ -10,10 +10,7 @@ from user.models import User
 from user.utils import LoginAuthorization
 
 
-# Create your views here.
-
 class SignUpView(View):
-# 회원가입 로직 작성필요
     def post(self, request):
         try:
             data     = json.loads(request.body)
@@ -22,24 +19,19 @@ class SignUpView(View):
 
             # 정규 표현식
             username_expression = "^[A-Za-z0-9_]*$"
-            password_expression = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,18}"
             email_expression    = "[^@]+@[^@]+\.[^@]+"
+            password_expression = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,18}"
             mobile_expression   = "(010)(-{1}\d{4}-{1}\d{4})"
 
-            #  username check and validation
             if (not re.match(email_expression,username) and not re.match(mobile_expression, username)) and not re.match(username_expression, username):
-                return JsonResponse({'MESSAGE' : 'USERNAME_ERROR'})
+                return JsonResponse({'MESSAGE' : 'USERNAME_OR_PASSWORD_ERROR'}, status=400)
 
-            # password validation
-            elif not re.search(password_expression, password):
-                return JsonResponse({'MESSAGE' : 'PASSWORD_ERROR'})
+            if not re.search(password_expression, password):
+                return JsonResponse({'MESSAGE' : 'USERNAME_OR_PASSWORD_ERROR'}, status=400)
 
-            # 요청한 데이터가 데이터베이스에 있는지 확인 하고, 있는 경우 에러가 발생하게 함.
-            # 요청한 데이터가 데이터베이스에 있는지 조회 구현
-            elif User.objects.filter(username = username).count() >= 1:
-                return JsonResponse({'MESSAGE' : 'ACCOUNT_ERROR'})
+            if User.objects.get(username = username).count() >= 1:
+                return JsonResponse({'MESSAGE' : 'ACCOUNT_ALREADY_EXIST'}, status=400)
 
-                    # 위의 모든 경우가 괜찮은 경우 데이터 베이스 생성
             else:
                 user = User.objects.create(
                     username     = username,
