b38144ee96de38f4e0d294797bf7399a2a70bedd
lyla0427 <hillary0427@gmail.com>
2020-12-10 14:54:25 +0900

    코드 컨벤션에 따라 코드 수정

--- students/15th/team1/seungyun-lee/user/views.py
+++ students/15th/team1/seungyun-lee/user/views.py
@@ -1,19 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
 import json
 import bcrypt
 import jwt
 
-from django.views import View
-from django.http  import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
+from django.shortcuts import render
 
-from .models import User
+from .models     import User
 from my_settings import SECRET
-
-
-
-
 
 
 class UserView(View):
@@ -24,18 +18,15 @@ class UserView(View):
             if (data['username'] == '' and data['email'] == '' and data['phonenumber'] == '') or data['password'] == '':
                 raise KeyError
 
-            if User.objects.filter(username=data['username'], email=data['email'], phonenumber=data['phonenumber'], password=data['password']).exists():
+            if User.objects.filter(username=data['username'], email=data['email'], phonenumber=data['phonenumber']).exists():
                 raise ValueError
 
-            password = data['password']
+            password         = data['password']
             hashed_passwords = User.objects.values_list('password', flat=True).distinct()
 
             for i in hashed_passwords:
-                print(i)
-                print(type(i))
                 if bcrypt.checkpw(password.encode('utf-8'), i.encode('utf-8')):
                     raise ValueError
-
 
             if len(data['password']) < 8: #password validation
                 raise ValueError 
@@ -47,13 +38,13 @@ class UserView(View):
                         if i not in data['email'] :
                             raise ValueError
             
-            password    = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+            password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
 
             User.objects.create(
-                username= data['username'], 
-                email= data['email'], 
+                username    = data['username'], 
+                email       = data['email'], 
                 phonenumber = data['phonenumber'], 
-                password = password.decode('utf-8')
+                password    = password.decode('utf-8')
                 )
             return JsonResponse({'message': 'SUCCESS'}, status = 200)
     
@@ -66,33 +57,34 @@ class UserView(View):
 
     
     def get(self, request):
-        data = json.loads(request.body)
-        user_info = list(data.keys())
+        data       = json.loads(request.body)
+        login_info = list(data.keys())
 
         try:
-            if 'password' not in user_info and len(user_info) < 2:
-                print('hi')
+            if 'password' not in login_info and len(login_info) < 2:
                 raise KeyError
 
-            for i in user_info:
+            for i in login_info:
                 if i != 'password':
                     login_name = i
             
             password = data['password']
             if login_name == 'username':
                 hashed_password = User.objects.get(username=data[login_name])
+                user_data       = User.objects.get(username=data[login_name])
             elif login_name == 'email':
                 hashed_password = User.objects.get(email=data[login_name])
+                user_data       = User.objects.get(email=data[login_name])
             elif login_name == 'phonenumber':
                 hashed_password = User.objects.get(phonenumber=data[login_name])
+                user_data       = User.objects.get(phonenumber=data[login_name])
             
             if bcrypt.checkpw(password.encode('utf-8'), hashed_password.password.encode('utf-8')) == False:
                 raise ValueError
 
-            token = jwt.encode({'password' : data['password']}, SECRET, algorithm = "HS256")
-            token = token.decode('utf-8')                         
-
-            return JsonResponse({'token' : token}, status = 200)
+            id_token = jwt.encode({'id' : user_data.id}, SECRET, algorithm = "HS256")
+            id_token = id_token.decode('utf-8')
+            return JsonResponse({'token' : id_token}, status = 200)
 
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
