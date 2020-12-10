14a52c00c74f261ea46d0ad53467312b176d0803
Luceta <57941164+Luceta@users.noreply.github.com>
2020-12-11 00:06:56 +0900

    login & signup endpoint with jwt,bcrypt

--- students/15th/turtle/hanbyul-kim/user/views.py
+++ students/15th/turtle/hanbyul-kim/user/views.py
@@ -1,10 +1,12 @@
 
 import json, re
+import bcrypt
+import jwt
 
 from django.http        import JsonResponse
 from django.views       import View
-from django.db import IntegrityError
-from django.db.models import Q
+from django.db          import IntegrityError
+from django.db.models   import Q
 
 from user.models        import User
 
@@ -32,13 +34,17 @@ class UserView(View):
             if User.objects.filter(Q(name=data['name']) & Q(email=data['email']) & Q(mobile_number=data['mobile_number'])).exists():
                 return JsonResponse({'message': 'It is already_exist'}, status = 400)
 
+            hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+            password= hashed_password.decode('utf-8')
 
             User.objects.create(
                 name         = data['name'] ,
                 mobile_number = data['mobile_number'] ,
                 email        = data['email'] ,
-                password     = data['password'])
-            return JsonResponse({'message' : '성공'}, status=200)
+                password     = password,
+                )
+
+            return JsonResponse({'message' : 'SUCCESS'}, status=200)
         except IntegrityError:
             return JsonResponse(stats=400)
 
@@ -57,29 +63,33 @@ class SigninView(View):
             data['name']    = data.get('name')
             data['mobile_number'] = data.get('mobile_number')
             data['email'] = data.get('email')
-            data['password'] = data.get('password')
+            user_password = data.get('password')
+            SECRET = 'wecode'
 
             if data['name'] is None and data['mobile_number'] is None:
                 if User.objects.filter(email=data['email']).exists():
                     user = User.objects.get(email=data.get('email'))
-                    if user.password == data['password']:
-                        return JsonResponse({"message":"SUCCESS"}, status=200)
-                    return JsonResponse({"message":"password is not valid"},status=400)
+                    if bcrypt.checkpw(user_password.encode('utf-8'), user.password.encode('utf-8')):
+                        token = jwt.encode({"name":data['name']},SECRET,algorithm = "HS256")
+                        return JsonResponse({'token':token.decode('utf-8')}, status = 200)
+                    return JsonResponse({"message":"password is not valid"},status =400)
 
             #번호만 잇는 경우
             if data['name'] is None and data['email'] is None:
                 if User.objects.filter(mobile_number=data.get("mobile_number")).exists():
                     user = User.objects.get(mobile_number= str(data['mobile_number']))
-                    if user.password == data['password']:
-                        return JsonResponse({"message":"SUCCESS"}, status=200)
-                    return JsonResponse({"message":"password is not valid"},status = 400)
+                    if bcrypt.checkpw(user_password.encode('utf-8'), user.password.encode('utf-8')):
+                        token = jwt.encode({"name":data['name']},SECRET,algorithm = "HS256")
+                        return JsonResponse({'token':token.decode('utf-8')}, status = 200)
+                    return JsonResponse({"message":"password is not valid"},status =400)
 
             # 이름만 잇는 경
             if data['mobile_number'] is None and data['email'] is None:
                 if User.objects.filter(name= data['name']).exists():
                     user = User.objects.get(name=data['name'])
-                    if user.password == data['password']:
-                        return JsonResponse({"message":"SUCCESS"}, status=200)
+                    if bcrypt.checkpw(user_password.encode('utf-8'), user.password.encode('utf-8')):
+                        token = jwt.encode({"name":data['name']},SECRET,algorithm = "HS256")
+                        return JsonResponse({'token':token.decode('utf-8')}, status = 200)
                     return JsonResponse({"message":"password is not valid"},status =400)
 
         except KeyError as e:
