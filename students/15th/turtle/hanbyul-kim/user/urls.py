6345e7f2f7b1417f8192d32a0f69db3d2576156b
Luceta <57941164+Luceta@users.noreply.github.com>
2020-12-10 17:50:26 +0900

    combined function from other branch

--- students/15th/turtle/hanbyul-kim/user/urls.py
+++ students/15th/turtle/hanbyul-kim/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from user.views import UserView
+from user.views import UserView, SigninView
 
 urlpatterns = [
     path('/sign-up',UserView.as_view(), name ='sign-up'),
+    path('/sign-in',UserView.as_view(), name ='sign-in'),
 ]
