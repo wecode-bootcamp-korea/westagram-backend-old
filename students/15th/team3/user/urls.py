07158d2387d4ce22f37694567a2fa2be4b9f35df
mincheolkk <kmc04080408@gmail.com>
2020-12-10 17:17:58 +0900

    토근발행완료

--- students/15th/team3/user/urls.py
+++ students/15th/team3/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from user.views import SignUpView
+from user.views import SignUpView, SignIn
 
 
 urlpatterns = [
-    path('', SignUpView.as_view())
+    path('/signup', SignUpView.as_view()),
+    path('/signin', SignIn.as_view())
 ]
--- students/15th/team3/user/views.py
+++ students/15th/team3/user/views.py
@@ -8,36 +8,33 @@ from .models   import SignUp
 from django.views    import View
 
 class SignUpView(View):
-
-
         def post(self, request):
             data = json.loads(request.body)
 
-            data['email'] = data.get('email')
-            data['tel'] = data.get('tel')
-            data['name'] = data.get('name')
-
-
+            email    = data.get('email')
+            tel      = data.get('tel')
+            name     = data.get('name')
+            password = data['password']
 
             try:
-                 if (data['name'] is not None) or (data['tel'] is not None) or (data['email'] is not None):
-                    if data['password'] is not None:
+                 if (name is not None) or (tel is not None) or (email is not None):
+                    if password is not None:
 
-                        if len(data['password']) < 8:
+                        if len(password) < 8:
                             return JsonResponse({"message": "your password is dangerous."}, status= 401)
 
-                        if ('@' not in data['email']) or ('.' not in data['email']):
+                        if ('@' not in email) or ('.' not in email):
                             return JsonResponse({'message':'its not email format'},status=401)
 
-                        if SignUp.objects.filter(name = data['name']).exists() and SignUp.objects.filter(email = data['email']).exists() and SignUp.objects.filter(tel = data['tel']).exists():
+                        if SignUp.objects.filter(name = name).exists() and SignUp.objects.filter(email = email).exists() and SignUp.objects.filter(tel = tel).exists():
                             return JsonResponse({'message':'your information is already registered.'},status=400)
 
-                        hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+                        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
                         SignUp(
-                            tel=data['tel'],
-                            email=data['email'],
-                            name=data['name'],
+                            tel=tel,
+                            email=email,
+                            name=name,
                             password=hashed_password.decode()
                         ).save()
                         return JsonResponse({"message": "SUCCESS"}, status=200)
@@ -50,37 +47,39 @@ class SignUpView(View):
 class SignIn(View):
     def post(self,request):
         data = json.loads(request.body)
-        email = data.get('email')
-        tel = data.get('tel')
-        name = data.get('name')
-        password = data['password']
+
+        email    = data.get('email')
+        #tel      = data.get('tel')
+        #name     = data.get('name')
+        #password = data['password']
 
         try:
-            if SignUp.objects.filter(name=name).exists():
-                user = SignUp.objects.get(name=name)
-                if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                    access_token = jwt.encode({'id':user.id},SECRET_KEY, algorithm='HS256')
-                    return JsonResponse({'message': 'SUCCESS'}, status=200)
-                else:
-                    return JsonResponse({'message': 'INVALID_USER'}, status=401)
+            # if SignUp.objects.filter(name=name).exists():
+            #     user = SignUp.objects.get(name=name)
+            #     if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+            #         #access_token = jwt.encode({'id':user.id},SECRET_KEY, algorithm='HS256')
+            #         return JsonResponse({'message': 'SUCCESS'}, status=200)
+            #     else:
+            #         return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
             if SignUp.objects.filter(email=email).exists():
                 user = SignUp.objects.get(email=email)
-                if bcrypt.checkpw(password.encode('utf-8'),
+                if bcrypt.checkpw(data['password'].encode('utf-8'),
                                       user.password.encode('utf-8')):
-                    access_token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm='HS256')
-                    return JsonResponse({'message': 'SUCCESS'}, status=200)
+                    SECRET ='secret'
+                    token = jwt.encode({'id': user.id}, SECRET, algorithm='HS256')
+                    return JsonResponse({'message': 'SUCCESS','access_token':token.decode()}, status=200)
                 else:
                     return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
-            if SignUp.objects.filter(tel=tel).exists():
-                user = SignUp.objects.get(tel=tel)
-                if bcrypt.checkpw(password.encode('utf-8'),
-                                  user.password.encode('utf-8')):
-                    access_token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm='HS256')
-                    return JsonResponse({'message': 'SUCCESS'}, status=200)
-                else:
-                    return JsonResponse({'message': 'INVALID_USER'}, status=401)
+            # if SignUp.objects.filter(tel=tel).exists():
+            #     user = SignUp.objects.get(tel=tel)
+            #     if bcrypt.checkpw(data['password'].encode('utf-8'),
+            #                       user.password.encode('utf-8')):
+            #         #access_token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm='HS256')
+            #         return JsonResponse({'message': 'SUCCESS'}, status=200)
+            #     else:
+            #         return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
         except:
              return JsonResponse ({'message': 'KEY_ERROR'}, status=400)
