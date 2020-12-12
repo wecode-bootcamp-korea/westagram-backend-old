0b031f2b3e33c8fdc3b59feda984537b7e771d1b
Jacob <jacob.juhyung.lee@gmail.com>
2020-12-12 14:18:47 +0900

    리뷰 수정 완료 (models.py 제외, decorator 미구현)

--- students/15th/team3/juhyung_lee/users/models.py
+++ students/15th/team3/juhyung_lee/users/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+
 class User(models.Model):
     username       = models.CharField(max_length=20, null=True, blank=True)
     email          = models.CharField(max_length=30, null=True, blank=True)
     phone_number   = models.CharField(max_length=15, null=True, blank=True)
-    password       = models.CharField(max_length=256)
+    password       = models.CharField(max_length=256, null=True, blank=True)
 
     class Meta:
         db_table = 'users'
