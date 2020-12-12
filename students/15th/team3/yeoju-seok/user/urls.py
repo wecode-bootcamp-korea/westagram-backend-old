a075a08dbc85ba864a56252c81f9b746a405c33a
YEOJU SEOK <seok@YEOJUui-MacBookPro.local>
2020-12-12 21:10:06 +0900

    pw,email validation적용

--- students/15th/team3/yeoju-seok/user/urls.py
+++ students/15th/team3/yeoju-seok/user/urls.py
@@ -3,7 +3,6 @@ from .views import AccountView,LoginView
 
 
 urlpatterns = [
- #    path('admin/', admin.site.urls),
     path('/account', AccountView.as_view()),
     path('/login', LoginView.as_view())
 ]
--- students/15th/team3/yeoju-seok/user/views.py
+++ students/15th/team3/yeoju-seok/user/views.py
@@ -1,6 +1,7 @@
 import json
 import bcrypt
 import jwt
+import re
 
 from django.http import JsonResponse
 from django.views import View
@@ -11,12 +12,31 @@ from my_settings import SECRET
 class AccountView(View):
     def post(self, request):
         data = json.loads(request.body)
+        email_validation = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        pw_validation = '^[A-Za-z0-9@#$%^&+=]{8,}$'
+
         try:
-            Account.objects.create(
-                email = data['email'],
-                password = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt()).decode()
-            )
-            return JsonResponse({"message":"SUCCESS"},status=200)
+            if not re.match(email_validation, data['email']):
+                return JsonResponse(
+                    {"message":"INVALID_MAIL"},status=401
+                )
+
+            if not re.match(pw_validation, data['password']):
+                return JsonResponse(
+                    {"message":"INVALID_PW"},status=401
+                )
+            if Account.objects.filter(email=data['email']).exists():
+                return JsonResponse(
+                    {"message":"USER_EXIST"},status=400
+                )
+
+            else:
+                Account.objects.create(
+                        email = data['email'],
+                        password  = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt()).decode()
+                )
+                return JsonResponse({"message":"SUCCESS"},status=200)
+
         except:
             return JsonResponse({"message":"KEY_ERROR"},status=401)
 
@@ -32,9 +52,9 @@ class LoginView(View):
                 if password_check == True:
                     access_token = jwt.encode({'id' : user_id},SECRET,algorithm = 'HS256').decode('utf-8')
 
-                    return JsonResponse({"message":"SUCESS", "TOKEN" : access_token},status=200)
+                    return JsonResponse({"message":"SUCCESS", "TOKEN" : access_token},status=200)
 
-                return JsonResponse({"message":"INVALID_PASSWORD"},status=401)
+                return JsonResponse({"message":"INVALID_PASSWORD"},status=402)
 
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"},status=401)
