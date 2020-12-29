084d9ef35cad5938284c10e0c9529374eaec504f
Jun Young Jang <junyoungjang@Junui-MacBookPro.local>
2020-12-29 21:30:08 +0900

    Add: 16기 장준영 장고 프로젝트 Mission 2

--- students/16th/team1/junyoungjang/user/models.py
+++ students/16th/team1/junyoungjang/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name        = models.CharField(max_length  = 20)
+    email       = models.EmailField(max_length = 100)
+    password    = models.CharField(max_length  = 30)
+    nickname    = models.CharField(max_length  = 150)
+    phonenumber = models.CharField(max_length  = 15)
+
+    class Meta:
+        db_table = "users"
---
+++ students/16th/team1/junyoungjang/user/views.py
@@ -0,0 +1,55 @@
+import json
+
+from django.http  import JsonResponse
+from django.views import View
+
+from .models      import User
+
+
+class RegisterView(View):
+    def post(self,request):
+
+        try:
+            MIN_EMAIL_LENGTH = 8
+            data             = json.loads(request.body)
+            name             = data['name']
+            phonenumber      = data['phonenumber'].replace("-",'')
+            email            = data['email']
+            password         = data['password']
+            nickname         = data['nickname']
+            
+            if email.find("@") == -1 or email.find(".") == -1:
+                return JsonResponse({'MESSAGE :':"INVAILD EMAIL ADDRESS!"},status = 400)
+            
+            if len(password)<MIN_EMAIL_LENGTH:
+                return JsonResponse({'MESSAGE :':"PASSWORD TOO SHORT!"},status = 400)
+
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({'MESSAGE :':"EMAIL ALREADY EXISTS!"},status = 400)
+
+            if User.objects.filter(phonenumber=phonenumber).exists():
+                return JsonResponse({'MESSAGE :':"PHONENUMBER ALREADY EXISTS!"},status = 400)
+
+            if User.objects.filter(nickname=nickname).exists():
+                return JsonResponse({'MESSAGE :':"NICKNAME ALREADY EXISTS!"},status = 400)
+            
+            User.objects.create(
+                name        = name,
+                phonenumber = phonenumber, 
+                email       = email, 
+                password    = password,
+                nickname    = nickname
+            )
+            
+            return JsonResponse({'MESSAGE :':"SUCCESS "},status = 200)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE :':"KEY_ERROR"},status = 400)
+
+        except BlankFieldException as e:
+            return JsonResponse({'MESSAGE :':e.__str__()},status = 400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE :':"INVAILD_USER"},status = 400)
+
+
