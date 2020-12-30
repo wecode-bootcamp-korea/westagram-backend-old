fe3562c568d730c7ff6d4c4106d3f1df546b8dd1
konhee-kim <kghee218@gmail.com>
2020-12-31 02:23:20 +0900

    validation function for user sign-up

--- students/16th/team2/konhee-kim/user/models.py
+++ students/16th/team2/konhee-kim/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class User(models.model)
+#    mobile_number = models.IntegerField() to be added
+    email         = models.EmailField() # shall be unique
+    full_name     = models.CharField()
+    username      = models.CharField()
+    password      = models.CharField() # shall be change as hashed password later
+
+    class Meta:
+        db_table = 'users'
 
--- students/16th/team2/konhee-kim/user/views.py
+++ students/16th/team2/konhee-kim/user/views.py
@@ -1,1 +1,41 @@
+import json
+import re
+
+from django.http  import JsonResponse
+from django.views import View
+
+from user.models  import User
+
+class SignUpView(View):
+
+    def post(self, request):
+        data   = json.loads(request.body)
+        a_user = User.objects.create(   # email or phone number -> tb selected
+            email     = data['email']
+            full_name = data['full_name']
+            username  = username['username']
+            password  = password['password']
+        )
+
+        if TEST: # if posted message is okay
+            return JsonResponse({'MESSAGE': 'SUCCESS'}, status=200)
+        if TEST: # if posted message is not okay
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+    def email_validate(self, email):
+        """
+        for email validation. check email_addresss include '@' & '.'
+        return : boolean
+        """
+        try:
+            return re.match(r".+@.+\..+", email).group(0) == email:
+        except:
+            return False:
+
+    def password_validate(self, password):
+        """
+        for password validation. check password > 8 characters
+        return : boolean
+        """
+        return re.match(r".*", password).span()[1] >= 8 
 
