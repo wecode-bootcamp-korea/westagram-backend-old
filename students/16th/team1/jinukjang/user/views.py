cd5b41719ebb266909da662fe79e32b97a70383c
장진욱 <wfo3977@gmail.com>
2020-12-30 17:53:53 +0900

    MODIFY: 장진욱 MISSION 2, 3 코딩컨벤션 수정

--- students/16th/team1/jinukjang/user/views.py
+++ students/16th/team1/jinukjang/user/views.py
@@ -17,43 +17,37 @@ class SignupView(View):
 
         data = json.loads(request.body)
 
-        username, email, phone = None, None, None
-
-        if 'username' in data:
-            username = data['username']
-
-        if 'email' in data:
-            email = data['email']
-
-        if 'phone' in data:
-            phone = data['phone']
-
+        # username, email, phone이 입력되지 않았다면 None
+        username = data['username'] if 'username' in data else None
+        email    = data['email']    if 'email'    in data else None
+        phone    = data['phone']    if 'phone'    in data else None
+        
         # username, email, phone이 모두 입력되지 않았거나, 비밀번호가 입력되지 않았을때 
         if (username or email or phone) == None or 'password' not in data:
             return JsonResponse({'MESSAGE': 'KEY ERROR'}, status=400)
+
+        if username != None and User.objects.filter(username=username).exists():
+            return JsonResponse({'MESSAGE :':"이미 존재하는 사용자 이름입니다."},status = 400)
+
+        if email    != None and ('@' not in email or '.' not in email):
+            return JsonResponse({'MESSAGE :':"EMAIL VALIDATION"},status = 400)
+        
+        if email    != None and User.objects.filter(email=email).exists():
+            return JsonResponse({'MESSAGE :':"이미 존재하는 이메일입니다."},status = 400)
+
+        if phone    != None and User.objects.filter(phone=phone).exists():
+            return JsonResponse({'MESSAGE :':"이미 존재하는 전화번호입니다."},status = 400)
 
         password = data['password']
 
         if len(password) < MIN_PASSWORD_LENGHT:
             return JsonResponse({'MESSAGE :':"PASSWORD VALIDATION"},status = 400)
 
-        if email != None and ('@' not in email or '.' not in email):
-            return JsonResponse({'MESSAGE :':"EMAIL VALIDATION"},status = 400)
-        
-        if email != None and User.objects.filter(email=email).exists():
-            return JsonResponse({'MESSAGE :':"이미 존재하는 이메일입니다."},status = 400)
-
-        if username != None and User.objects.filter(username=username).exists():
-            return JsonResponse({'MESSAGE :':"이미 존재하는 사용자 이름입니다."},status = 400)
-
-        if phone != None and User.objects.filter(phone=phone).exists():
-            return JsonResponse({'MESSAGE :':"이미 존재하는 전화번호입니다."},status = 400)
-
         User.objects.create(
             username = username,
             email    = email,
-            password = password,
-            phone    = phone
+            phone    = phone,
+            password = password
         )
     
         return JsonResponse({'MESSAGE': 'SUCCESS'}, status=200) 
@@ -62,29 +56,28 @@ class LoginView(View):
     def get(self,request):
         data = json.loads(request.body)
 
+        login_user = User.objects.all()
+        
         username, email, phone = None, None, None
-
+        
         if 'username' in data:
-            username = data['username']
+            username   = data['username']
+            login_user = login_user.filter(username=username)
 
-        if 'email' in data:
-            email = data['email']
+        if 'email'    in data:
+            email      = data['email']
+            login_user = login_user.filter(email=email)
 
-        if 'phone' in data:
-            phone = data['phone']
+        if 'phone'    in data:
+            phone      = data['phone']
+            login_user = login_user.filter(phone=phone)
 
         # username, email, phone이 모두 입력되지 않았거나, 비밀번호가 입력되지 않았을때 
         if (username or email or phone) == None or 'password' not in data:
             return JsonResponse({'MESSAGE': 'KEY ERROR'}, status=400)
 
-        password = data['password']
-        
-        
-        users = User.objects.filter(password=password)
-
-        for user in users:
-            if user.username == username or user.email == email or user.phone == phone:
-                return JsonResponse({'MESSAGE': 'SUCCESS'}, status=200)
+        if login_user[0].password == data['password']:
+            return JsonResponse({'MESSAGE': 'SUCCESS'}, status=200)
 
         return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
          
