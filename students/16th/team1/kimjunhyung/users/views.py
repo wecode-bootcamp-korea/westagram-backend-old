cbc2171f7541d92485d0cb63ca643c97d9a8b0f0
Ddalkigum <sol35352000@gmail.com>
2020-12-30 16:28:35 +0900

    ADD: 팔로우 기능

--- students/16th/team1/kimjunhyung/users/views.py
+++ students/16th/team1/kimjunhyung/users/views.py
@@ -1,12 +1,14 @@
 import json
 import re
 import bcrypt
+import jwt
 
 from django.views   import View
 from django.http    import JsonResponse
 
-from .models            import User
-from decorators.utils   import check_blank
+from .models            import User, Follow
+from decorators.utils   import check_blank, login_required
+from my_settings        import SECRET_KEY
 
 class UserSignUpView(View):
     @check_blank
@@ -41,8 +43,30 @@ class UserSignInView(View):
             user = User.objects.get(email = email)
             password_check = bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8"))
             if password_check:
-                return JsonResponse({"message":"SUCCESS"}, status = 200)    
+                encoded_jwt = jwt.encode(
+                    {"id":user.id}, key = SECRET_KEY, algorithm = "HS256"
+                )
+                return JsonResponse({"message":"SUCCESS", "token":encoded_jwt}, status = 200)    
             return JsonResponse({"message":"PASSWORD_IS_WRONG"}, status = 401)
+        except User.DoesNotExist:
+            return JsonResponse({"message":"USER_DOES_NOT_EXIST"}, status = 401)
+
+class FollowUser(View):
+    @login_required
+    def post(self, request):
+        data                = json.loads(request.body)
+        follower_email      = data["email"]
+        following_email     = data["following_email"]
+        follower            = User.objects.prefetch_related("follow").get(email = follower_email)
+
+        try:
+            following   = User.objects.prefetch_related("following").get(email = following_email)
+            follow      = Follow.objects.filter(follow_user = follower, following_user = following)
+            if follow.exists():
+                follow.delete()
+                return JsonResponse({"message":"UNFOLLOW"}, status = 200)
+            follow.create(follow_user = follower, following_user =following)
+            return JsonResponse({"message":"FOLLOW"}, status = 200)
         except User.DoesNotExist:
             return JsonResponse({"message":"USER_DOES_NOT_EXIST"}, status = 401)
         
