9ee8e7e498e3e36293104ed1534bf02df84bea6a
redsaumon <ywhong830@gmail.com>
2020-12-30 13:39:29 +0900

    fix: 16기 홍연우 회원가입 엔드포인트

--- students/16th/team1/yeonu_hong/user/views.py
+++ students/16th/team1/yeonu_hong/user/views.py
@@ -5,22 +5,28 @@ from .models      import User
 
 # 회원가입
 class SignUpView(View):
-    def post(self,request): 
+    def post(self, request): 
         try:
-            data      = json.loads(request.body) # JSON으로 들어와서 python dict로 바꿔줌
+            data      = json.loads(request.body)
             name      = data['name']
             phone     = data['phone']
             email     = data['email']
             password  = data['password']
-            name_dup  = User.objects.filter(name=data["name"])
-            phone_dup = User.objects.filter(phone=data["phone"])
-            email_dup = User.objects.filter(email=data["email"])
+
+            if User.objects.filter(name=data["name"]).exists():
+                name_dup  = User.objects.filter(name=data["name"])
+                return JsonResponse({'message': '이미 사용중인 name'}, status=400)
+            if User.objects.filter(phone=data["phone"]):
+                phone_dup = User.objects.filter(phone=data["phone"])
+                return JsonResponse({'message': '이미 사용중인 phone'}, status=400)
+            if User.objects.filter(email=data["email"]):
+                email_dup = User.objects.filter(email=data["email"])
+                return JsonResponse({'message': '이미 사용중인 email'}, status=400)
+
 
             if len(password) > 8:
                 return JsonResponse({'message': '비밀번호 길이는 8글자 이상'}, status=400)
 
-            if name_dup == name or phone_dup == phone or email_dup == email:
-                return JsonResponse({'message': '이미 사용중인 값'}, status=400)
 
             User(
                 name     = name,
@@ -32,7 +38,5 @@ class SignUpView(View):
             
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
-        # except KeyError as e:
-        #     return JsonResponse({'message':e.args[0]}, status=400)
 
-        
+
