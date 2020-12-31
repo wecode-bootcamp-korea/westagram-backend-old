e2bc0d6877f31a86b3c0ffb28f9319c31ee11f8b
hamin <hamsterhamin@gmail.com>
2020-12-31 20:15:41 +0900

    mission 2: 회원가입 엔드포인트

--- students/16th/team2/hamin-jo/westagram/user/models.py
+++ students/16th/team2/hamin-jo/westagram/user/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class User(models.Model):
     name     = models.CharField(max_length=30)
-    email    = models.CharField(max_length=50)
-    phone    = models.IntegerField(max_length=11)
     password = models.CharField(max_length=30)
+    email    = models.CharField(max_length=50)
+    phone    = models.IntegerField()
--- students/16th/team2/hamin-jo/westagram/user/views.py
+++ students/16th/team2/hamin-jo/westagram/user/views.py
@@ -6,4 +6,30 @@ from django.http import JsonResponse
 from user.models import User
 
 class UserView(View):
-    def sign_in(self, request):
+    def post(self, request):
+        data     = json.loads(request.body)
+        name     = data.get('name')
+        password = data.get('password')
+        email    = data.get('email')
+        phone    = data.get('phone')
+
+        if email == None and phone == None and name == None:
+            return JsonResponse({'MESSAGE': 'KEY_ERRORS'}, status=400)
+
+        if email != None:
+            if '@' not in email or '.' not in email:
+                return JsonResponse({'MESSAGE': 'INVALID_EMAIL'}, status=400)
+
+        if password == None:
+            return JsonResponse({'MESSAGE': 'KEY_ERRORS'}, status=400)
+        
+        if len(password) < 8:
+            return JsonResponse({'MESSAGE': 'INVALID_PASSWORD'}, status=400)
+        
+        if User.objects.filter(name=name).exists() or User.objects.filter(phone=phone).exists() or User.objects.filter(email=email).exists():
+            return JsonResponse({'MESSAGE': 'EXIST_USER'}, status=400)
+
+        User.objects.create(name=name, password=password, email=email, phone=phone)
+        
+        return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+        
