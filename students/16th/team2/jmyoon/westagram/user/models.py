3ed8e61a17c6ee54172ef5ac68e0150a4e8c1a70
jmyoon <wjdals.yoon14@gmail.com>
2020-12-31 22:54:21 +0900

    회원가입 엔드포인트

--- students/16th/team2/jmyoon/westagram/user/models.py
+++ students/16th/team2/jmyoon/westagram/user/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-class Account(models.Model):
-    email      = models.CharField(max_length = 40)
+class User(models.Model):
+    email      = models.CharField(max_length = 40, unique = True)
+    phone      = models.CharField(max_length = 40, unique = True)
     name       = models.CharField(max_length = 40)
-    phone      = models.CharField(max_length = 40)
+    user_name  = models.CharField(max_length = 40, unique = True)
     password   = models.CharField(max_length = 200)
     
     class Meta :
-        db_table = 'accounts'
+        db_table = 'users'
--- students/16th/team2/jmyoon/westagram/user/views.py
+++ students/16th/team2/jmyoon/westagram/user/views.py
@@ -2,4 +2,55 @@ import json
 
 from django.http import JsonResponse
 from django.views import View
+from user.models import User
+
+class SignupView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        email      = data['email']
+        phone      = data['phone']
+        name       = data['name']
+        user_name  = data['user_name']
+        password   = data['password']
+
+
+        try:
+            if ('@' not in email or '.' not in email):
+                return JsonResponse(
+                    {'MESSAGE' : '잘못된 email형식'}, 
+                    status = 400
+                )
+            elif len(password) < 8 :
+                return JsonResponse(
+                    {'MESSAGE' : 'password는 8자리 이상'}, 
+                    status = 400
+                )
+            elif (User.objects.filter(email = email).exists() 
+            or User.objects.filter(user_name = user_name).exists()):
+                return JsonResponse(
+                    {'MESSAGE' : '이미 사용중'}, status = 400
+                )
+            else :
+                User.objects.create(
+                    email      = email,
+                    phone      = phone,
+                    name       = name,
+                    user_name  = user_name,
+                    password   = password 
+                ).save()
+                return JsonResponse(
+                    {'MESSAGE' : 'SignUp SUCCESS'}, status = 200
+                )
+        except KeyError:
+            return JsonResponse(
+                {'MESSAGE' : 'KeyERROR'}, status = 400
+            )
+            
+
+
+
+        email
+
+
 
