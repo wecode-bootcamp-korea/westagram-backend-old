a44ae10e6312f5dcb018a3398f20509940fd77c7
konhee-kim <kghee218@gmail.com>
2021-01-01 01:02:29 +0900

    Log-In view 테스트 완료

--- students/16th/team2/konhee-kim/user/urls.py
+++ students/16th/team2/konhee-kim/user/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from user.views import SignUpView, LogInView
 
 urlpatterns = [
-    path('/sign-up', SignUpView.as_view())
-    path('/log-in', LogInView.as_view())
-
+    path('/sign-up', SignUpView.as_view()),
+    path('/log-in' , LogInView.as_view())
 ]
