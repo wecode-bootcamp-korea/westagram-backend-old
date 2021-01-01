afb5daa65a6f568cc46dab83fbee4292776697d9
hamin <hamsterhamin@gmail.com>
2021-01-01 18:12:06 +0900

    Mission3: 로그인 엔드포인

--- students/16th/team2/hamin-jo/westagram/user/urls.py
+++ students/16th/team2/hamin-jo/westagram/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from user.views import UserView
+from user.views import UserView, LoginView
 
 urlpatterns=[
-    path('', UserView.as_view())
+    path('', UserView.as_view()),
+    path('/login', LoginView.as_view())
 ]
