5949974de443ac8b2c50390e77ffb94776225942
sue517 <csooa9305@gmail.com>
2021-01-01 18:26:14 +0900

    [16기 최수아] Add : [Mission 2] 회원가입 엔드포인트

--- students/16th/team2/suaChoi/user/models.py
+++ students/16th/team2/suaChoi/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email      = models.CharField(max_length=800)
+    name       = models.CharField(max_length=100)
+    password   = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "users"
--- students/16th/team2/suaChoi/user/views.py
+++ students/16th/team2/suaChoi/user/views.py
@@ -1,1 +1,50 @@
+import json
+import re
+
+from django.http  import JsonResponse
+from django.views import View
+
+from .models import User
+
+class UserView(View):
+
+    def post(self, request):
+         data = json.loads(request.body)
+         name = data['name']
+         email = data['email']
+         password = data['password']
+
+         email_rule = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+
+         try:
+             # KEY ERROR
+             if not email and password:
+                 return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+             # too short password
+             if len(password) <  8:
+                 return JsonResponse({"message":"NOT_VALID_PASSWORD"}, status=400)
+
+             # bend email rule
+             if not email_rule.match(email):
+                 return JsonResponse({"message": "NOT_VALID_EMAIL"}, status=400)
+
+             # exist email
+             if User.objects.filter(email=email).exists():
+                 return JsonResponse({"message": "EXIST_EMAIL"}, status=400)
+
+             if not User.objects.filter(email=email).exists():
+                 User.objects.create(
+                     name = name,
+                     email = email,
+                     password = password
+                 )
+                 return JsonResponse({"message": "SUCCESS"}, status=200)
+
+         except:
+             return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+    def get(self, request):
+        user = User.objects.values()
+        return JsonResponse({"user": list(user)}, status=200)
 
