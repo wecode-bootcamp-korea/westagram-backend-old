a646b9b102d080bdf2b43108d4465bf808e635bb
jmyoon <wjdals.yoon14@gmail.com>
2021-01-05 00:34:02 +0900

    회원가입, 로그인 views.py수정

--- students/16th/team2/jmyoon/westagram/user/views.py
+++ students/16th/team2/jmyoon/westagram/user/views.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from django.http   import JsonResponse
 from django.views  import View
@@ -14,12 +15,16 @@ class SignupView(View):
         user_name  = data['user_name']
         password   = data['password']
 
+        # email 정규표현식
+        vali_email = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        validate_email=re.compile(vali_email)
+
         try:
             if not (email and user_name and name and password and phone):
                 return JsonResponse(
                     {'MESSAGE' : 'KeyERROR'}, status = 400)
-
-            elif ('@' not in email or '.' not in email):
+            
+            elif not validate_email.match(email):
                 return JsonResponse(
                     {'MESSAGE' : '잘못된 email형식'}, status = 400)
 
@@ -39,7 +44,7 @@ class SignupView(View):
                     name       = name,
                     user_name  = user_name,
                     password   = password 
-                ).save()
+                )
                 return JsonResponse(
                     {'MESSAGE' : 'SignUp SUCCESS'}, status = 200)
 
@@ -53,17 +58,24 @@ class SigninView(View):
         data = json.loads(request.body)
 
         # 변수지정했을때 KeyError발생, 왜지?
-        # email      = data['email']
-        # phone      = data['phone']
-        # user_name  = data['user_name']
-        # signin_password   = data['password']
-
+        # 변수지정을 해줬을 때 에러가 뜬 이유 : 변수를 지정해 줬으나 
+        # http로 받아온 정보에는 해당 되는 변수가 없었기 때문에 에러가 발생한 것
+        # 받아온 데이터를 변수로 지정하고 싶다면 아래와 같이 해당 데이터가 있을 때 변수로 가져오는 형식으로 바꾸면 된다.
+        if 'email' in data.keys():
+            email      = data['email']
+        if 'phone' in data.keys():
+            phone      = data['phone']
+        if 'user_name' in data.keys():
+            user_name  = data['user_name']
+        if 'password' in data.keys():
+            password = data['password']
+       
         try:
             # 이메일로 로그인 시
             if 'email' in data.keys():
             #if data.get('email')is not None:
-                if User.objects.filter(email = data['email']).exists():
-                    if User.objects.get(email = data['email']).password == data['password']:
+                if User.objects.filter(email = email).exists():
+                    if User.objects.get(email = email).password ==  password:
                         return JsonResponse({'MESSAGE' : 'SignIn SUCCES'}, status = 200)
                     else:
                         return JsonResponse({'MESSAGE' : 'INCORRECT PASSWORD'}, status = 400)
@@ -73,8 +85,8 @@ class SigninView(View):
             # 휴대폰번호로 로그인 시
             elif 'phone' in data.keys():
             #elif data.get('phone')is not None:
-                if User.objects.filter(phone = data['phone']).exists():
-                    if User.objects.get(phone = data['phone']).password == data['password']:
+                if User.objects.filter(phone = phone).exists():
+                    if User.objects.get(phone = phone).password == password:
                         return JsonResponse({'MESSAGE' : 'SignIn SUCCES'}, status = 200)
                     else:
                         return JsonResponse({'MESSAGE' : 'INCORRECT PASSWORD'}, status = 400)
@@ -84,8 +96,8 @@ class SigninView(View):
             # 사용자이름으로 로그인 시
             elif 'user_name' in data.keys():
             #elif data.get('user_name')is not None:
-                if User.objects.filter(user_name = data['user_name']).exists():
-                    if User.objects.get(user_name = data['user_name']).password == data['password']:
+                if User.objects.filter(user_name = user_name).exists():
+                    if User.objects.get(user_name = user_name).password == password:
                         return JsonResponse({'MESSAGE' : 'SignIn SUCCES'}, status = 200)
                     else:
                         return JsonResponse({'MESSAGE' : 'INCORRECT PASSWORD'}, status = 400)
