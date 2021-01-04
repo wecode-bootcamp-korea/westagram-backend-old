b7be503fec38e2cda07cfae7687cf3577f1d5123
Sam <klioop2@gmail.com>
2021-01-05 00:00:17 +0900

    ADD: 로그인뷰 인증인가 구현

--- students/16th/team1/sungyong_Lee/account/views.py
+++ students/16th/team1/sungyong_Lee/account/views.py
@@ -1,9 +1,14 @@
 import json, re
 
+import bcrypt, jwt
+
 from django.views     import View
 from django.http      import JsonResponse
 from django.db.models import Q
 from .models          import User
+from my_settings      import SECRET_KEY
+
+
 
 
 class SignUpView(View):
@@ -14,10 +19,11 @@ class SignUpView(View):
         MIN_LENGTH_PASSWORD = 8
 
         try:
-            name         = data["name"]
-            email        = data["email"]
-            phone_number = data["phone_number"]
-            password     = data["password"]
+            name             = data["name"]
+            email            = data["email"]
+            phone_number     = data["phone_number"]
+            password         = data["password"]
+            password_encoded = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
             if User.objects.filter(
                 Q(name=name) and Q(email=email) and Q(phone_number=phone_number)
@@ -58,7 +64,7 @@ class SignUpView(View):
                 name         = name,
                 email        = email,
                 phone_number = phone_number,
-                password     = password,
+                password     = password_encoded,
             )
 
             return JsonResponse({"message": "SUCCESS"}, status=200)
@@ -87,9 +93,11 @@ class LoginView(View):
 
             if user.exists():
                 if password != user[0].password:
-                    return JsonResponse({"message": "INVALID_USER"}, status=401)
+                    return JsonResponse({"message": "INCORRECT_PASSWORD"}, status=401)
                 else:
-                    return JsonResponse({"message": "SUCCESS"}, status=200)
+                    jwt_encoded = jwt.encode( {'user_id': user[0].id }, SECRET_KEY, algorithm='HS256'  )
+                    return JsonResponse({"message": "SUCCESS",
+                                         "jwt"    : jwt_encoded }, status=200)
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
