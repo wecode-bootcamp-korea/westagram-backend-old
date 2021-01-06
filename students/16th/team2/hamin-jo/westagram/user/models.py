eca4c4ec66372eacf8cdfd6ee394479e655306d6
hamin <hamsterhamin@gmail.com>
2021-01-06 18:20:51 +0900

    암호화 및 토큰발행 추가

--- students/16th/team2/hamin-jo/westagram/user/models.py
+++ students/16th/team2/hamin-jo/westagram/user/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class User(models.Model):
+    email    = models.CharField(max_length=50, null=False)
+    password = models.CharField(max_length=200, null=False)
     name     = models.CharField(max_length=30, null=True)
-    password = models.CharField(max_length=30, null=False)
-    email    = models.CharField(max_length=50, null=True)
     phone    = models.CharField(max_length=30, null=True)
 
     def __str__(self):
--- students/16th/team2/hamin-jo/westagram/user/views.py
+++ students/16th/team2/hamin-jo/westagram/user/views.py
@@ -1,5 +1,7 @@
 import json
 import re
+import bcrypt
+import jwt
 
 from django.views import View
 from django.http import JsonResponse
@@ -8,93 +10,66 @@ from user.models import User
 
 class UserView(View):
     def post(self, request):
-        data           = json.loads(request.body)
-        name           = data.get('name')
-        password       = data.get('password')
-        email          = data.get('email')
-        phone          = '0'+data.get('phone')
-        email_regex    = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-        password_regex = '[A-Za-z0-9@#$]{8,12}'
-        name_db        = User.objects.filter(name=name)
-        email_db       = User.objects.filter(email=email)
-        phone_db       = User.objects.filter(phone=phone)
+        try:
+            data           = json.loads(request.body)
+            name           = data.get('name')
+            phone          = data.get('phone')
+            password       = data.get('password')
+            email          = data.get('email')
+            email_regex    = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+            password_regex = '[A-Za-z0-9@#$]{8,12}'
+            encoded_pw     = password.encode('utf-8')
+            hashed_pw      = bcrypt.hashpw(encoded_pw, bcrypt.gensalt())
+            decoded_pw     = hashed_pw.decode('utf-8')
 
+            if 'email' in data.keys():
+                if not (re.search(email_regex,email)):
+                    return JsonResponse({'MESSAGE': 'INVALID_EMAIL'}, status=400)
 
-        # email, phone, name 중 1개이상 입력 확인
-        if not email and not phone and not name:
+                if User.objects.filter(email=email).exists():
+                    return JsonResponse({'MESSAGE': 'EXIST_USER'}, status=400)
+            else:
+                return JsonResponse({'MESSAGE': 'KEY_ERRORS; email'}, status=400)
+
+            if 'password' in data.keys():
+                if not (re.search(password_regex, password)):
+                    return JsonResponse({'MESSAGE': 'INVALID_PASSWORD'}, status=400)
+            else:
+                return JsonResponse({'MESSAGE': 'KEY_ERRORS; password'}, status=400)
+
+            if phone:
+                if User.objects.filter(phone=phone).exists():
+                    return JsonResponse({'MESSAGE': 'EXIST_USER'}, status=400)
+
+            User.objects.create(name= name, password= decoded_pw, email= email, phone= phone)
+
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+
+        except KeyError:
             return JsonResponse({'MESSAGE': 'KEY_ERRORS'}, status=400)
-        
-        # password 입력확인
-        if not password:
-            return JsonResponse({'MESSAGE': 'KEY_ERRORS'}, status=400)
-
-        # password 조건확인
-        if not (re.search(password_regex, password)):
-            return JsonResponse({'MESSAGE': 'INVALID_PASSWORD'}, status=400)
-
-        # email 입력시, 조건확인과 기존회원 유무확인
-        if email:
-            if not (re.search(email_regex,email)):
-                return JsonResponse({'MESSAGE': 'INVALID_EMAIL'}, status=400)
-            if email_db.exists():
-                return JsonResponse({'MESSAGE': 'EXIST_USER'}, status=400) 
-
-        # name 입력시 기존회원 유무확인
-        if name:
-           if name_db.exists():
-               return JsonResponse({'MESSAGE': 'EXIST_USER'}, status=400)
-        
-        # phone 입력시 기존회원 유무확인
-        if phone:
-            if phone_db.exists():
-                return JsonResponse({'MESSAGE': 'EXIST_USER'}, status=400)
-     
-        User.objects.create(name= name, password= password, email= email, phone= phone)
-        return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
-
 
 class LoginView(View):
     def post(self, request):
-        data        = json.loads(request.body)
-        name        = data.get('name')
-        email       = data.get('email')
-        phone       = data.get('phone')
-        password    = data.get('password')
+        try:
+            data        = json.loads(request.body)
+            email       = data.get('email')
+            password    = data.get('password').encode('utf-8')
+            
+            if email:
+                if User.objects.filter(email= email).exists():
+                    user       = User.objects.get(email= email)
+                    user_id    = user.id
+                    user_pw    = user.password
+                    
+                    if bcrypt.checkpw(password, user_pw.encode('utf-8')):
+                        print(234)
+                        token  = jwt.encode( {'user-id':user_id}, 'secret', algorithm='HS256')
+                        
+                        return JsonResponse({"token":token}, status= 200)
 
-        # email, phone, name 중 1개이상 입력 확인
-        if not email and not phone and not name:
+                return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
             return JsonResponse({'MESSAGE': 'KEY_ERRORS'}, status=400)
 
-        # password 입력확인
-        if not password:
-            return JsonResponse({'MESSAGE': 'KEY_ERRORS'}, status=400)
-
-        # name 입력 시 회원 유무 확인 후 password 매칭 확인
-        if not name:
-            if User.objects.filter(name= name).exists():
-                user = User.objects.get(name= name)
-                if user.password != password:
-                    return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
-            else:
-                return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
-
-        # email 입력 시 회원 유무 확인 후 password 매칭 확인
-        if not email:
-            if User.objects.filter(email= email).exists():
-                user = User.objects.get(email= email)
-                if user.password != password:
-                    return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
-            else:
-                return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
-                
-        # phone 입력 시 회원 유무 확인 후 password 매칭 확인
-        if not phone:
-            if User.objects.filter(phone= phone).exists():
-                user = User.objects.get(phone= phone)
-                if user.password != password:
-                    return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
-            else:
-                return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
-        
-        return JsonResponse({"message":"SUCCESS"}, status= 200) 
+        except:
+             return JsonResponse({'MESSAGE': 'KEY_ERRORSSSS'}, status=400)
         
