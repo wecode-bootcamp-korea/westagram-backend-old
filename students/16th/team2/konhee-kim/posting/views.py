79af880e6439764a7cb9a20ed34f06bb3d552b06
konhee-kim <kghee218@gmail.com>
2021-01-08 02:02:33 +0900

    posting view

--- students/16th/team2/konhee-kim/posting/views.py
+++ students/16th/team2/konhee-kim/posting/views.py
@@ -4,20 +4,45 @@ import re
 from django.http  import JsonResponse
 from django.views import View
 
-from user.utils import login_required
-# from posting.models  import # specify models
+from user.utils      import login_required
+from user.models     import User
+from posting.models  import Article
 
 
 class PostingView(View):
+
     @login_required
     def post(self, request):
-        #data = json.loads(request.body)
-        #print(request.headers["Authorization"].split(' ').pop())
-        #print(data)
+        
+        try:
+            data       = json.loads(request.body)
+            user_id    = request.user_id
+            content    = data["content"]
+            image_urls = data["image_urls"] # list
 
-        return JsonResponse({'MESSAGE': 'LOGINED'})
+            for image_url in image_urls:
+                created_article = Article.objects.create(
+                        user      = User.objects.get(id=user_id),
+                        content   = content,
+                        image_url = image_url
+                        )
+            
+            return JsonResponse({'MESSAGE': 'POSTED'}, status=200)
+        
+        except:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
 
 class SeeAllArticlesView(View):
     
     def get(self, request):
         print("get!!")
+
+
+
+
+
+
+
+
+
