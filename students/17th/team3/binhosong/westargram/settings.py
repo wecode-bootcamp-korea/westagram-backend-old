04286cd402a13d79d3c91c84191b5ce6cb0b57eb
binogood <binogood68@gmai.com>
2021-01-28 16:03:12 +0900

    Modify: 주석처리

--- students/17th/team3/binhosong/westargram/settings.py
+++ students/17th/team3/binhosong/westargram/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
+#    'django.contrib.admin',
+#    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
