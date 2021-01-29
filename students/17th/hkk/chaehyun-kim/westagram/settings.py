a7f0778b7d3ffbe15821533b2dc14e09568a40db
JELLO-Kim <cogus950403@gmail.com>
2021-01-29 16:15:55 +0900

    REVISE : SECRET KEY

--- students/17th/hkk/chaehyun-kim/westagram/settings.py
+++ students/17th/hkk/chaehyun-kim/westagram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mjy8+8)f@w7=s4y@2o-yee*_*y8*-5_6q7ds-7)ld+2_ph^9$y'
+SECRET_KEY = my_settings.SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'user',
 ]
 
 MIDDLEWARE = [
