e66583f1506227bd585b331546b4a18b13e34175
JELLO-Kim <cogus950403@gmail.com>
2021-01-29 16:24:41 +0900

    ERROR CHECK : RUNSERVER ERROR

--- students/17th/hkk/chaehyun-kim/westagram/settings.py
+++ students/17th/hkk/chaehyun-kim/westagram/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import my_settings
 from pathlib import Path
 
+import my_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
