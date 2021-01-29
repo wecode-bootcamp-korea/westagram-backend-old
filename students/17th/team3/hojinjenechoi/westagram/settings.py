c3eea5466d6a1190d4a51046460e6978487cbec8
Hojin Jene Choi <hojinjenechoi@Hojins-MacBook-Pro.local>
2021-01-30 00:09:46 +0900

    Modified: 보안 비밀번호, 데이터 베이스 숨김

--- students/17th/team3/hojinjenechoi/westagram/settings.py
+++ students/17th/team3/hojinjenechoi/westagram/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import my_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd!wbz2g+%j(92g)xbte&fqpowckq6=++a%%6#5!s#v6f@v56+v'
+SECRET_KEY = my_settings['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'westagram.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+        'USER': 'root',
+        'PASSWORD': '4950',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
