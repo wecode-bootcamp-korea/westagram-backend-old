5e00b056f55fd60169f0bae1b45fdc8f49a02132
JELLO-Kim <cogus950403@gmail.com>
2021-01-29 16:49:35 +0900

    "CREATE : users table"

--- students/17th/hkk/chaehyun-kim/user/models.py
+++ students/17th/hkk/chaehyun-kim/user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+   email    = models.EmailField(max_length=100)
+   password = models.CharField(max_length=20)
+
+   def __str__(self):
+       return f'{self.email}'
+
+   class Meta:
+       db_table = 'users'
+
