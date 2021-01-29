99c9d041ad153ac889410804342e0938c4bbaa3c
namujinju <namujinju@gmail.com>
2021-01-29 12:42:37 +0900

    Modify: .gitignore에 my_settings.py 추가

--- students/17th/team1/jaehyeonpark/westagram/wsgi.py
+++ students/17th/team1/jaehyeonpark/westagram/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for westagram project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
 
 import os
 
