e09f0c32c0a995cd1f55ba0bf0b70fdae59db181
ique-coder <tjdwns0309@gmail.com>
2021-01-31 02:32:01 +0900

    Modify: Add create_at, update_at attribute to Accounts class of models.py

--- students/17th/team3/sungjounjang/user/models.py
+++ students/17th/team3/sungjounjang/user/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 class Accounts(models.Model):
-    email        = models.EmailField(max_length=100)
-    name         = models.CharField(max_length=50)
-    nickname     = models.CharField(max_length=50)
-    password     = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=20)
+    email        = models.EmailField(max_length=300)
+    name         = models.CharField(max_length=300)
+    nickname     = models.CharField(max_length=300)
+    password     = models.CharField(max_length=300)
+    phone_number = models.CharField(max_length=300)
+    create_at    = models.DateTimeField(auto_now_add=True)
+    update_at    = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
 
     class Meta:
-        db_table = "accounts"
+        db_table = "accounts" 
 
