d3454fb210165ec1cc3f7cdb75334fea4feec151
JELLO-Kim <cogus950403@gmail.com>
2021-02-01 01:20:18 +0900

    REVISE : user/models.py delete null

--- students/17th/hkk/chaehyun-kim/user/models.py
+++ students/17th/hkk/chaehyun-kim/user/models.py
@@ -5,8 +5,8 @@ from django.db      import models
 class User(models.Model):
    email        = models.EmailField(max_length=100)
    password     = models.CharField(max_length=20)
-   name         = models.CharField(max_length=10, default='stranger')
-   phone_number = models.CharField(max_length=20, default=000)
+   name         = models.CharField(max_length=10)
+   phone_number = models.CharField(max_length=20)
 
    def __str__(self):
        return f'{self.name}'
