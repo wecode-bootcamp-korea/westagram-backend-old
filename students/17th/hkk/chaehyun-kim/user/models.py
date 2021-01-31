d6d9750c3c934054d34b4010e2f398af97d3acbd
JELLO-Kim <cogus950403@gmail.com>
2021-02-01 01:17:41 +0900

    CREATE : user/models.py (name, phone_number, email, password)

--- students/17th/hkk/chaehyun-kim/user/models.py
+++ students/17th/hkk/chaehyun-kim/user/models.py
@@ -1,13 +1,15 @@
-from django.db import models
+from django.db      import models
 
 # Create your models here.
 
 class User(models.Model):
-   email    = models.EmailField(max_length=100)
-   password = models.CharField(max_length=20)
+   email        = models.EmailField(max_length=100)
+   password     = models.CharField(max_length=20)
+   name         = models.CharField(max_length=10, default='stranger')
+   phone_number = models.CharField(max_length=20, default=000)
 
    def __str__(self):
-       return f'{self.email}'
+       return f'{self.name}'
 
    class Meta:
        db_table = 'users'
