381f9693c0f054755e363859f4f121364659d810
sayxyoung <hyjlove11@gmail.com>
2021-01-31 22:37:38 +0900

    Add : user/models.py       class생성(User)       attribute : name, user_name, email, password

--- students/17th/team-one/heeyoungjeong/user/models.py
+++ students/17th/team-one/heeyoungjeong/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name      = models.CharField(max_length=20)
+    user_name = models.CharField(max_length=20)
+    email     = models.EmailField(max_length=254)
+    password  = models.CharField(max_length=20)
+
+
+
+
+
