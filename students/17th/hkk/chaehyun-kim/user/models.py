91daa091ce8c4836d579b1db6d3f2cb1ded16933
JELLO-Kim <cogus950403@gmail.com>
2021-02-01 17:06:12 +0900

    "ADD - user/models.py create_at field - user/models.py update_at field"

--- students/17th/hkk/chaehyun-kim/user/models.py
+++ students/17th/hkk/chaehyun-kim/user/models.py
@@ -1,12 +1,13 @@
 from django.db      import models
-
-# Create your models here.
+from django.utils   import timezone
 
 class User(models.Model):
    email        = models.EmailField(max_length=100, unique=True)
    password     = models.CharField(max_length=300)
    name         = models.CharField(max_length=10, unique=True)
    phone_number = models.CharField(max_length=20, unique=True)
+   create_at    = models.DateTimeField(auto_now_add=True)
+   update_at    = models.DateTimeField(auto_now=True)
 
    def __str__(self):
        return f'{self.name}'
