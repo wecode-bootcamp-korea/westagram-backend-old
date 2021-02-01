a990ac4056be5eaddce5c93ea0e07efa471c2b07
Hojin Jene Choi <hojinjenechoi@Hojins-MacBook-Pro.local>
2021-02-01 19:56:06 +0900

    Modify: views.py 에러 메세지 수정, 빈줄 삭제

--- students/17th/team3/hojinjenechoi/user/models.py
+++ students/17th/team3/hojinjenechoi/user/models.py
@@ -5,7 +5,7 @@ from django.core.validators import RegexValidator
 # Create your models here.
 class User(models.Model):
     email           = models.EmailField(max_length=200, unique=True, verbose_name='Username')#, validators=[validate_email])
-    password        = models.CharField(max_length=200, verbose_name='Password') #, validators=[validate_password])
+    password        = models.CharField(max_length=300, verbose_name='Password') #, validators=[validate_password])
     nickname        = models.CharField(max_length=50)
     phone           = models.CharField(null=True, max_length=11, validators=[RegexValidator(r'^\d{3}-\d{3}-\d{4}$')])
     registered_time = models.DateTimeField(auto_now_add=True)
