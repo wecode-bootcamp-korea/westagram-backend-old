91378b7cec99d72b11850a7dbfd3529c358e31c8
namujinju <namujinju@gmail.com>
2021-02-01 15:57:11 +0900

    Modify: password max_length 변경 - 비밀번호 암호화를 위한 비밀번호 최대 길이 변경

--- students/17th/team1/jaehyeonpark/user/models.py
+++ students/17th/team1/jaehyeonpark/user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     email    = models.EmailField(max_length=50, null=False, unique=True)
-    password = models.CharField(max_length=50, null=False)
+    password = models.CharField(max_length=500, null=False)
 
     class Meta:
         db_table = 'users'
