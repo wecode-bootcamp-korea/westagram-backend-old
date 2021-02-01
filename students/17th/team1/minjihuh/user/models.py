3e6355edade2f6570e00b91a4c3f64c0d03b7b20
mjhuh263 <mjhuh263@gmail.coom>
2021-02-02 02:28:20 +0900

    Created : EMAIL, PHONE, PASSWORD Validation variables and If Statements / Deleted : MINIMUM_PASSWORD_LENGTH Variable / Modified : Validation Variable Scope

--- students/17th/team1/minjihuh/user/models.py
+++ students/17th/team1/minjihuh/user/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class User(models.Model):
-    name              = models.CharField(max_length=50, null=True)
+    name              = models.CharField(max_length=50)
+    phone             = models.CharField(max_length=50, null=True)
     email             = models.EmailField(max_length=100, null=True, unique=True)
     password          = models.CharField(max_length=300)
     username          = models.CharField(max_length=100, unique=True) 
--- students/17th/team1/minjihuh/user/views.py
+++ students/17th/team1/minjihuh/user/views.py
@@ -1,42 +1,67 @@
-import json
+import json, re
 
-from django.http   import JsonResponse, HttpResponse;
+
+from django.http   import JsonResponse, HttpResponse
 from django.views  import View
 
 from user.models   import (
     User
 )
 
+email_regex              = re.compile(r'.*[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+.*')
+password_regex           = re.compile(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$')
+phone_regex              = re.compile(r'[0-9]{3}-[0-9]{4}-[0-9]{4}')
+
 class SignInView(View):
     def post(self, request):
-        try:
-            data = json.loads(request.body)
+        data = json.loads(request.body)
 
-            name          = data['name']
-            email         = data['email']
-            password      = data['password']
-            username      = data['username']
-            MINIMUM_PASSWORD_LEN = 8
+        try:
+            name                     = data['name']
+            phone                    = data['phone']
+            email                    = data['email']
+            password                 = data['password']
+            username                 = data['username']
+
+            if name == "":
+                return JsonResponse({"MESSAGE" : "NAME_REQUIRED"}, status=400)
+
+            if phone == "" and email == "":
+                return JsonResponse({"MESSAGE" : "EMAIL_OR_PHONE_NUMBER_REQUIRED"}, status=400)
+
+            if username == "":
+                return JsonResponse({"MESSAGE" : "USERNAME_REQUIRED"}, status=400)
+
+            if password == "":
+                return JsonResponse({"MESSAGE" : "PASSWORD_REQUIRED"}, status=400)
 
             if User.objects.filter(email=email).exists():
-                    return JsonResponse({"MESSAGE": "EMAIL_EXISTS"}, status=400)
+                return JsonResponse({"MESSAGE": "EMAIL_EXISTS"}, status=400)
                 
-            if '@' and '.' not in email:
-                    return JsonResponse({"MESSAGE" : "INVALID_EMAIL_SYNTAX"}, status=400)
-                
-            if len(password) <= MINIMUM_PASSWORD_LEN:
-                return JsonResponse({"MESSAGE" : "PASSWORD_MUST_BE_LONGER_THAN_EIGHT_LETTERS"}, status=400)
-
             if User.objects.filter(username=username).exists():
                 return JsonResponse({"MESSAGE" : "USERNAME_EXISTS"}, status=400)
+
+            if '@' and '.' not in email:
+                return JsonResponse({"MESSAGE" : "INVALID_EMAIL_SYNTAX"}, status=400)
+
+            if (email_regex.search(str(email)) != None) == False:
+                return JsonResponse({"MESSAGE" : "EMAIL_VALIDATION"}, status=400)
+
+            if (password_regex.match(str(password)) != None) == False:
+                return JsonResponse({"MESSAGE" : "PASSWORD_VALIDATION"}, status=400)
+
+            if (phone_regex.match(str(phone)) != None) == False:
+                return JsonResponse({"MESSAGE" : "PHONE_VALIDATION"}, status=400)
 
             User.objects.create(
                 name     = name, 
                 email    = email, 
+                phone    = phone,
                 password = password, 
                 username = username
             )
 
             return JsonResponse({"MESSAGE" : "SUCCESS"}, status=200)
+        
         except KeyError:
-            return JsonResponse({"MESSAGE" : "USERNAME_EXISTS"}, status=400)
+            return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status=400)
