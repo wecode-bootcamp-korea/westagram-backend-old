2df961b811d4013b11eb1b71a370ade2d5fb70e2
mjhuh263 <mjhuh263@gmail.coom>
2021-02-01 20:41:44 +0900

    Modified : PASSWORD_LENGTH variable, Error messages

--- students/17th/team1/minjihuh/user/views.py
+++ students/17th/team1/minjihuh/user/views.py
@@ -9,30 +9,34 @@ from user.models   import (
 
 class SignInView(View):
     def post(self, request):
-        data = json.loads(request.body)
+        try:
+            data = json.loads(request.body)
 
-        name          = data['name']
-        email         = data['email']
-        password      = data['password']
-        username      = data['username']
+            name          = data['name']
+            email         = data['email']
+            password      = data['password']
+            username      = data['username']
+            MINIMUM_PASSWORD_LEN = 8
 
-        if User.objects.filter(email=email).exists():
-                return JsonResponse({"MESSAGE": "EMAIL_KEY_ERROR"}, status=400)
-             
-        if '@' and '.' not in email:
-                return JsonResponse({"MESSAGE" : "NOT_EMAIL_SYNTAX"}, status=400)
-            
-        if len(password) <= 8:
-            return JsonResponse({"MESSAGE" : "PASSWORD_KEY_ERROR"}, status=400)
+            if User.objects.filter(email=email).exists():
+                    return JsonResponse({"MESSAGE": "EMAIL_EXISTS"}, status=400)
+                
+            if '@' and '.' not in email:
+                    return JsonResponse({"MESSAGE" : "INVALID_EMAIL_SYNTAX"}, status=400)
+                
+            if len(password) <= MINIMUM_PASSWORD_LEN:
+                return JsonResponse({"MESSAGE" : "PASSWORD_MUST_BE_LONGER_THAN_EIGHT_LETTERS"}, status=400)
 
-        if User.objects.filter(username=username).exists():
+            if User.objects.filter(username=username).exists():
+                return JsonResponse({"MESSAGE" : "USERNAME_EXISTS"}, status=400)
+
+            User.objects.create(
+                name     = name, 
+                email    = email, 
+                password = password, 
+                username = username
+            )
+
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status=200)
+        except KeyError:
             return JsonResponse({"MESSAGE" : "USERNAME_EXISTS"}, status=400)
-
-        User.objects.create(
-            name     = name, 
-            email    = email, 
-            password = password, 
-            username = username)
-
-
-        return JsonResponse({"MESSAGE" : "SUCCESS"}, status=200)
