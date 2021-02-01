9c9e745a0e5aa68ec38adfafef5f67f8baf5f57b
namujinju <namujinju@gmail.com>
2021-02-01 16:35:30 +0900

    Modify: exception handling 구문 수정 - 전체 코드를 try하고 except를 최하단에 명시

--- students/17th/team1/jaehyeonpark/user/views.py
+++ students/17th/team1/jaehyeonpark/user/views.py
@@ -1,4 +1,4 @@
-import json
+import json, bcrypt, jwt
 
 from django.http     import JsonResponse
 from django.views    import View
@@ -6,32 +6,31 @@ from django.db.utils import DataError, IntegrityError
 
 from user.models     import User
 
-PASSWORD_MINIMUN_LENGTH = 8
+PASSWORD_MINIMUM_LENGTH = 8
 
 class SignUpView(View):
     def post(self, request):
         try:
             data     = json.loads(request.body)
-        except json.decoder.JSONDecodeError:
-            return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
+            email, password = str(data['email']), str(data['password'])
+                
+            if "" in (email, password):
+                return JsonResponse({'message':'NO_VALUE_ERROR'}, status=400)
+            
+            if '@' and '.' not in email:
+                return JsonResponse({'message':'EMAIL_VALIDATION_ERROR'}, status=400)
 
-        try:
-            email    = str(data['email'])
-            password = str(data['password'])
+            if len(password) < PASSWORD_MINIMUM_LENGTH:
+                return JsonResponse({'message':'PASSWORD_VALIDATION_ERROR'}, status=400)
+
+            user = User.objects.create(email=email, password=hashed_password)
+
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
         
-        if "" in (email, password):
-            return JsonResponse({'message':'NO_VALUE_ERROR'}, status=400)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
         
-        if '@' and '.' not in email:
-            return JsonResponse({'message':'EMAIL_VALIDATION_ERROR'}, status=400)
-
-        if len(password) < PASSWORD_MINIMUN_LENGTH:
-            return JsonResponse({'message':'PASSWORD_VALIDATION_ERROR'}, status=400)
-
-        try:
-            user     = User.objects.create(email=email, password=password)
         except DataError:
             return JsonResponse({'message':'DATA_ERROR'}, status=400)
 
