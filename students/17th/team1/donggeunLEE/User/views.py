a36a1cb791d3d184d9f1fb73b95dff383184bf6f
이동근 <eagle5424228@github.com>
2021-02-02 00:24:36 +0900

    Mission2 회워가입 구현

--- students/17th/team1/donggeunLEE/User/views.py
+++ students/17th/team1/donggeunLEE/User/views.py
@@ -7,25 +7,28 @@ from .models      import Userinfo
 
 # 회원가입
 class UserSignUpView(View):
+    MINIMUM_PASSWORD_LENGTH = 8
+    
     def post(self, request):
         data = json.loads(request.body)
         
         password_validity = data['password']
+        
         try:
             if Userinfo.objects.filter(name = data['name']).exists():
-                return JsonResponse({"MESSAGE" : "Invailed_Name_Syntax"}, status = 403)
+                return JsonResponse({"MESSAGE" : "USER_ALREADY_EXIST"}, status = 403)
         
             if Userinfo.objects.filter(Phone_number = data['phone_number']).exists():
                 return JsonResponse({"MESSAGE": "Inavailed_Number_Syntax"}, status= 403)
         
             if Userinfo.objects.filter(email = data['email']).exists():
-                return JsonResponse({"MESSAGE":"Inavailed_Email_Syntax"}, status = 403)
+                return JsonResponse({"MESSAGE":"INVALID_EMAIL"}, status = 403)
         
             if '@' and '.' not in data['email']:
                 return JsonResponse({"MESSAGE" : "Inavailed_KeyError"}, status= 403)
         
-            if len(password_validity) < 8:
-                return JsonResponse({"MESSAGE" : "Sure?"}, status= 403)
+            if len(password_validity) < MINIMUM_PASSWORD_LENGTH:
+                return JsonResponse({"MESSAGE" : "SHORT_PASSWORD"}, status= 403)
 
             Userinfo.objects.create(
                     name         = data['name'],
@@ -37,13 +40,6 @@ class UserSignUpView(View):
             return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status = 400)
 
         return JsonResponse({'MESSAGE': 'SUCCESS'}, status = 200)
-
-# 로그인
-class UserLoginView(View):
-    def POST(self, request):
-        data = json.loads(request.body)
-
-        if Userinfo.objects.filter(name = data['name']) or Userinfo.objects.filter(Phone_number = data['phone_number']) or Userinfo.objects.filter(email = data['email']) or Userinfo.objects.filter(  )
 
 
 
