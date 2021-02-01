f784e80a7783d4179ef8185673e28643eff617e9
이지윤 <jiyoonlee.913@gmail.com>
2021-02-01 20:54:36 +0900

    ADD: Mission 2 회원가입

--- students/17th/team1/jiyoonlee/user/models.py
+++ students/17th/team1/jiyoonlee/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+password_validator = MinLengthValidator(8, "8자 이상의 비밀번호를 입력하세요.")
+
+class User(models.Model):
+    mobile_number = models.CharField(max_length=14)
+    email = models.EmailField(max_length=100)
+    full_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=100, validators=[password_validator], null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        db_table = 'users'
--- students/17th/team1/jiyoonlee/user/views.py
+++ students/17th/team1/jiyoonlee/user/views.py
@@ -1,3 +1,77 @@
-# from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from .models import User
+
+
+class UserView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        
+        try:
+            mobile_number = data['mobile_number']
+            email         = data['email']
+            full_name     = data['full_name']
+            username      = data['username']
+            password      = data['password']
+
+            if mobile_number is None and email is None:
+                return JsonResponse(
+                    {'message': '휴대폰 번호나 이메일을 입력하세요.'}, 
+                    status=400
+                )
+
+            if email is not None:
+                if '@' not in email or '.' not in email:
+                    return JsonResponse(
+                        {'message': '이메일 형식을 확인하세요.'}, 
+                        status=400
+                    )
+            
+            if len(password) < 8:
+                return JsonResponse(
+                    {'message': '최소 8자 이상으로 구성된 비밀번호를 생성해주세요.'},
+                    status=400
+                )
+            
+            if mobile_number is not None and User.objects.filter(mobile_number=data['mobile_number']).exists():
+                return JsonResponse(
+                    {'message': '이미 등록된 번호입니다.'},
+                    status=400
+                )
+
+            if email is not None and User.objects.filter(email=data['email']).exists():
+                return JsonResponse(
+                    {'message': '이미 등록된 이메일입니다.'},
+                    status=400
+                )
+
+            if User.objects.filter(username=data['username']).exists():
+                return JsonResponse(
+                    {'message': '이미 사용중인 username입니다.'},
+                    status=400
+                )
+
+
+            User.objects.create(
+                mobile_number = data['mobile_number'],
+                email = data['email'],
+                password = data['password'],
+                full_name= data['full_name'],
+                username= data['username']
+            )
+
+
+            return JsonResponse(
+                {'message': '회원가입에 성공하였습니다'},
+                status=200
+            )
+
+
+        except KeyError:
+            return JsonResponse(
+                {'message': 'KEY_ERROR'},
+                status=400
+            )
