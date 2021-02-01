a753fb6b0ddd98abbba47fe7e7e6f7653c52d1cd
sayxyoung <hyjlove11@gmail.com>
2021-02-01 13:16:34 +0900

    MODIFY : models.py 내용 수정          - special method 추가(dunder str)   	 - Meta class 추가
             settings.py 수정 및 db tabels 생성

--- students/17th/team-one/heeyoungjeong/user/models.py
+++ students/17th/team-one/heeyoungjeong/user/models.py
@@ -6,6 +6,11 @@ class User(models.Model):
     email     = models.EmailField(max_length=254)
     password  = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        db_table = 'User'
 
 
 
---
+++ students/17th/team-one/heeyoungjeong/user/views.py
@@ -0,0 +1,8 @@
+# import json
+# from django.http import JsonResponse, HttpResponse
+# from django.views import View
+#
+# from user.models import User
+#
+# class Signup(View):
+#
