c4273800c07f09d47d233f1ac3f8890c6621d548
ique-coder <tjdwns0309@gmail.com>
2021-02-01 23:34:59 +0900

    Add: token generation code

--- students/17th/team3/sungjounjang/user/views.py
+++ students/17th/team3/sungjounjang/user/views.py
@@ -1,10 +1,15 @@
 import json
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from django.views import View
 from django.http  import JsonResponse
-import bcrypt
 
-from .models import Accounts
+import bcrypt
+import jwt
+
+from .models     import Accounts
+import my_settings
 
 
 class AccountView(View):
@@ -14,23 +19,22 @@ class AccountView(View):
         # 필수사항 미입력시
         try:
             email        = data['email']
-            password     = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+            password     = data['password']
             name         = data['name']
             nickname     = data['nickname']
             phone_number = data['phone_number']
 
-            print('-----------------------------------')
-            print(password)
-
             # email, password check
             if email.find('@') == -1 or email.find('.') == -1:
-                return JsonResponse({'message': 'email validation'}, status=400)
+                return JsonResponse({'message': 'EMAIL_VALIDATION'}, status=400)
             if len(password) < 8:
-                return JsonResponse({'message': 'password validation'}, status=400)
+                return JsonResponse({'message': 'PASSWORD_VALIDATION'}, status=400)
+            else:
+                password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
             # duplicate check
             if Accounts.objects.filter(nickname=nickname) or Accounts.objects.filter(email=email) or Accounts.objects.filter(phone_number=phone_number):
-                return JsonResponse({'message': 'input data duplicate'}, status=400)
+                return JsonResponse({'message': 'DATA_DUPLICATE'}, status=400)
             
             Accounts.objects.create(
                 email        = email,
@@ -49,6 +53,7 @@ class AccountView(View):
 class LoginView(View):
     def post(self, request):
         data = json.loads(request.body)
+        access_token = ''
 
         try:
             email        = data.get('email')
@@ -66,8 +71,10 @@ class LoginView(View):
 
             if not bcrypt.checkpw(password.encode('utf-8'), account.password):
                 return JsonResponse({'message': 'INVALID_USER'}, status=401)
+            
+            access_token = jwt.encode({'email' : account.email}, my_settings.SECRET, algorithm = my_settings.ALGORITHM)
 
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-        return JsonResponse({'message': 'SUCCESS'}, status=200)
+        return JsonResponse({'message': 'SUCCESS', 'token': access_token}, status=200)
