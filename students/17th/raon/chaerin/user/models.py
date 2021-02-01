7548a33d73b5daf8d6b7d284fe51c0a491312e89
chaerin <cr434972@gmail.com>
2021-02-01 20:45:51 +0900

    Initial Setting

--- students/17th/raon/chaerin/user/models.py
+++ students/17th/raon/chaerin/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=10)
+    mail = models.EmailField(required=True)
+    password = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'users'
+
