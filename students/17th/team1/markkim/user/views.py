dfd7526d7a6e4307d4dc9861fd414410f4ceacc4
markkimjr <markkimjr96@gmail.com>
2021-02-02 15:17:44 +0900

    Fixed coding style to match WeCode Coding Convention

--- students/17th/team1/markkim/user/views.py
+++ students/17th/team1/markkim/user/views.py
@@ -9,20 +9,28 @@ from . models     import User
 class UserView(View):
     def post(self, request):
         data = json.loads(request.body)
+        phone_number = data.get('phone_number', None)
+
         if 'email' not in data.keys() or 'password' not in data.keys():
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
         if '@' not in data['email'] or '.' not in data['email']:
             return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
+        
         if len(data['password']) < 8:
             return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
+        
         if (data['full_name'],) in User.objects.values_list('full_name'):
             return JsonResponse({'message': 'FULL_NAME_ALREADY_EXISTS'}, status=409)
+        
         if (data['email'],) in User.objects.values_list('email'):
             return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=409)
-        phone_number = data.get('phone_number', None)
+        
         if phone_number:
+            
             if (int(phone_number),) in User.objects.values_list('phone_number'):
                 return JsonResponse({'message': 'PHONE_NUMBER_ALREADY_EXISTS'}, status=409)
+        
         User.objects.create(
                 full_name    = data['full_name'],
                 email        = data['email'],
@@ -30,6 +38,7 @@ class UserView(View):
                 username     = data['username'],
                 password     = data['password']
                 )
+        
         return JsonResponse({'message': 'SUCCESS'}, status=200)
 
 
