a646efaa3fa1326168649b5ffa00e8508c27ec15
이동근 <eagle5424228@github.com>
2021-02-02 15:51:21 +0900

    Mission3 로그인 구현 완료!

--- students/17th/team1/donggeunLEE/User/models.py
+++ students/17th/team1/donggeunLEE/User/models.py
@@ -5,9 +5,10 @@ from django.db.models.constraints import UniqueConstraint
 
 class Userinfo(models.Model):
     name         = models.CharField(max_length=20, unique=True)
-    phone_number = models.CharField(max_length=15, unique=True)
+    phone_number = models.CharField(max_length=15, unique=True, default = 0)
     email        = models.CharField(max_length=25, unique=True)
     password     = models.CharField(max_length=25, default=0)
+
 
     def __str__(self):
         return f'{self.name}'
