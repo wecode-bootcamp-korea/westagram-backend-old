615f59214cdfb762ecd0ad377b490703e4575d41
sayxyoung <hyjlove11@gmail.com>
2021-02-03 01:28:58 +0900

    MODIFY : 1. user/views.py 수정             - exists() 매소드 사용하여조건문 판별 	    - 변수 정의하여 조건문 판별하는 부분 제거             - 초반에 정의한 변수를 최대한 사용하여 request검증에 활용             - response message와 status를 의미있게 매칭

--- students/17th/team-one/heeyoungjeong/user/views.py
+++ students/17th/team-one/heeyoungjeong/user/views.py
@@ -3,9 +3,8 @@ from json.decoder import JSONDecodeError
 import re
 
 from django.db.models import Q
-from django.http import JsonResponse, HttpResponse
-from django.views import View
-
+from django.http      import JsonResponse, HttpResponse
+from django.views     import View
 
 from user.models import User
 
@@ -55,32 +54,24 @@ class SignInView(View):
         try:
             data = json.loads(request.body)
 
-            name = data.get('name', None)
-            user_name = data.get('user_name', None)
-            email = data.get('email', None)
-            password = data.get('password', None)
+            user_name    = data.get('user_name', None)
+            email        = data.get('email', None)
+            password     = data.get('password', None)
             phone_number = data.get('phone_number', None)
 
-            check_lst_pw = 'password'
-            if check_lst_pw not in data.keys():
-                return JsonResponse({'message':'KEY_ERROR'}, status=400)
+            if not (user_name or email or phone_number) and not password:
+                return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-            for value in data.values():
-                if not value:
-                    return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+            if User.objects.filter(Q(user_name=user_name) | Q(email=email) | Q(phone_number=phone_number)).exists():
 
-            user = User.objects.filter(Q(user_name=user_name) | Q(email=email) | Q(phone_number=phone_number))
-            user = user.first()
-            if user:
-                if (user_name == user.user_name or email == user.email or phone_number == user.phone_number) and password == user.password:
+                if User.objects.filter(password=password):
                     return JsonResponse({'message': 'SUCCESS'}, status=200)
 
                 else:
-                    return JsonResponse({'message':'The account is not valid'}, status=400)
+                    return JsonResponse({'message': 'INVALID_ACCOUNT'}, status=401)
 
             else:
-                return JsonResponse({'message':'User_Does_Not_Exist'}, status=400)
+                return JsonResponse({'message': 'INVALID_ACCOUNT'}, status=401)
 
         except JSONDecodeError:
-            return JsonResponse({'message':'The request is not valid'}, status=400)
-
+            return JsonResponse({'message': 'BAD_REQUEST'}, status=400)
