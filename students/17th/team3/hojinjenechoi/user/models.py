2e1833131096a7be0393911bedae846a51026a6a
Hojin Jene Choi <hojinjenechoi@Hojins-MacBook-Pro.local>
2021-02-03 00:35:57 +0900

    Modify: views.py - algorithms

--- students/17th/team3/hojinjenechoi/user/models.py
+++ students/17th/team3/hojinjenechoi/user/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
-#from django.core.validators import RegexValidator
-
-# Create your models here.
 class User(models.Model):
     email           = models.EmailField(max_length=200, unique=True, verbose_name='Username')
     password        = models.CharField(max_length=300, verbose_name='Password') 
     nickname        = models.CharField(max_length=50)
-    phone           = models.CharField(null=True, max_length=11)
+    phone           = models.CharField(null=True, max_length=15)
     registered_time = models.DateTimeField(auto_now_add=True)
     updated_time    = models.DateTimeField(auto_now=True)
 
