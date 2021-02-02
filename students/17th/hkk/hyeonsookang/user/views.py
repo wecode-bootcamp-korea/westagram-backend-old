412f4c76159bad445770c76c301dfae0295bf82f
강 현 수 <gangh84@gmail.com>
2021-02-02 17:33:54 +0900

    1차 수정: True, else삭제, Q object적용을 위한 수정

--- students/17th/hkk/hyeonsookang/user/views.py
+++ students/17th/hkk/hyeonsookang/user/views.py
@@ -3,6 +3,7 @@ import bcrypt
 
 from django.http  import JsonResponse
 from django.views import View
+from django.db.models import Q
 
 from user.models  import User
 MINIMUM_PASSWORD_LENGTH = 8
@@ -15,7 +16,7 @@ class SignUpView(View):
             if '@' not in data['email'] or '.' not in data['email']:
                 return JsonResponse({'MESSAGE': 'INVALID_EMAIL'}, status=401)
 
-            if User.objects.filter(email=data['email']).exists() or User.objects.filter(phonenumber=data['phone_number']).exists() or User.objects.filter(email=data['account']).exists():
+            if User.objects.filter(email=data['email']).exists() or User.objects.filter(phonenumber=data['phonenumber']).exists() or User.objects.filter(account=data['account']).exists():
                 return JsonResponse({'MESSAGE': 'ALREADY_SIGNUP'}, status=409)
 
             if len(data['password']) < MINIMUM_PASSWORD_LENGTH:
@@ -24,7 +25,7 @@ class SignUpView(View):
             User.objects.create(
                     email      = data['email'], 
                     password   = (bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())).decode('utf-8'),
-                    phonenumber= data['phone_number'],
+                    phonenumber= data['phonenumber'],
                     account    = data['account'],
                     )
             return JsonResponse({'MESSAGE': 'SUCCESS'}, status=200)
@@ -34,18 +35,20 @@ class SignUpView(View):
 
 class LogInView(View):
     def post(self, request):
-        try:
-            data = json.loads(request.body)
-        
-            if User.objects.filter(email=data['email']).exists() or User.objects.filter(account='account').exists() or User.objects.filter(phonenumber='phone_number').exists():
-                id = User.objects.get(email=data['email']) or User.objects.get(account='account') or User.objects.get(phonenumber='phone_number')
+        data = json.loads(request.body)
+        account = data.get('account')
+        email = data.get('email')
+        phonenumber = data.get('phonenumber')
+        password = data.get('password')
 
-                if bcrypt.checkpw(data['password'].encode('utf-8'),(id.password).encode('utf-8')) == True:
-                    return JsonResponse({'MESSAGE': 'SUCCESS'}, status=200)
-                else:
-                    return JsonResponse({'MESSAGE': 'CHECK_PASSWORD'}, status=404)
-            else:
-                return JsonResponse({'MESSAGE': 'CHECK_ID'}, status=404)
-
-        except KeyError:
+        if not password and (email or account or phonenumber):
             return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+        if User.objects.filter(Q(email=email) | Q(account=account) | Q(phonenumber=phonenumber)).exists():
+            id = User.objects.get(Q(email=email) | Q(account=account) | Q(phonenumber=phonenumber))
+
+            if bcrypt.checkpw(password.encode('utf-8'),(id.password).encode('utf-8')):
+                return JsonResponse({'MESSAGE': 'SUCCESS'}, status=200)
+            return JsonResponse({'MESSAGE': 'CHECK_PASSWORD'}, status=404)
+        return JsonResponse({'MESSAGE': 'CHECK_ID'}, status=404)
+
