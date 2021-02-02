793f2f18ec61ddf2f31c87bc4bae7db80253f600
Gyubster <kyunbo0910@naver.com>
2021-02-02 18:37:37 +0900

    FIX: added status in JsonResponse, added line to prevent same id issue

--- students/17th/team1/gyuboshim/westagram/user/urls.py
+++ students/17th/team1/gyuboshim/westagram/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls    import path, include
-from .views         import UserView
+from .views         import UserView, LoginView
 
 urlpatterns = [
-    path('', UserView.as_view())
+    path('/signup', UserView.as_view()),
+    path('/signin', LoginView.as_view())
 ]
--- students/17th/team1/gyuboshim/westagram/user/views.py
+++ students/17th/team1/gyuboshim/westagram/user/views.py
@@ -6,19 +6,33 @@ from django.http    import HttpResponse, JsonResponse
 
 from .models        import User
 
+PASSWORD_LEN_LIMIT = 8
+
 class UserView(View):
     def post(self, request):
         try:
-            data = json.loads(request.body)
-            user_data = data['user']
+            data            =   json.loads(request.body)
+            user_data       =   data['user']
+            phone_number    =   user_data['phone_number']
+            email_adress    =   user_data['email_adress']
+            name            =   user_data['name']
+            nickname        =   user_data['nickname']
+            password        =   user_data['password']
+
 
             if user_data['email_adress'] and user_data['password']:
                 
-                if '@' or '.' not in user_data['email_adress']:
-                    return JsonResponse({'message':'@ OR . IS MISSING IN E-MAIL ADRESS'})
+                if user_data['email_adress'] == User.objects.filter(email_adress = email_adress):
+                    return JsonResponse({'message':'CHANGE_E-MAIL_ADRESS'}, status = 409)
+                
+                if user_data['phone_number'] == User.objects.filter(phone_number = phone_number):
+                    return JsonResponse({'message':'CHANGE_PHONE_NUMBER'}, status = 409)
 
-                if len(user_data['password']) < 8:
-                    return JsonResponse({'message': 'PASSWORD HAS TO BE AT LEAST 8-DIGIT'})
+                if '@' or '.' not in user_data['email_adress']:
+                    return JsonResponse({'message':'@_OR_._IS_MISSING_IN_E-MAIL_ADRESS'}, status = 400)
+
+                if len(user_data['password']) < PASSWORD_LEN_LIMIT:
+                    return JsonResponse({'message': 'PASSWORD_HAS_TO_BE_AT_LEAST_8-DIGIT'}, status = 400)
                     
                 user = User.objects.create(
                                             phone_number    =   user_data['phone_number'],
@@ -31,8 +45,24 @@ class UserView(View):
                 return JsonResponse({'message':'SUCESS'}, status = 200)
             
             else:
-                return JsonResponse({'message':'PASSWORD OR E-MAIL IS MISSING'}, status = 401)
+                return JsonResponse({'message':'PASSWORD_OR_E-MAIL_IS_MISSING'}, status = 401)
         
         except:
-            return JsonResponse({'message': 'INVALID_KEYS'}, status = 400)
+            return JsonResponse({'message':'INVALID_KEYS'}, status = 400)
+    
+class LoginView(View):
+    def post(self, request):
+        data            =   json.loads(request.body)
+        phone_number    =   data['phone_number']
+        email_adress    =   data['email_adress']
+        password        =   data['password']
 
+        if User.objects.filter(
+                                phone_number = phone_number,
+                                email_adress = email_adress,
+                                password = password
+                                ).exists():
+            return JsonResponse({'message': 'LOGIN SUCESS'}, status = 200)
+
+        else:
+            return JsonResponse({'message': 'ID OR PASSWORD DOES NOT MATCH'}, status = 401)
