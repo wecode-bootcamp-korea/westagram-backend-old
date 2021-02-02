274157edd6658d31bf564f7417c2e3edd14251b5
markkimjr <markkimjr96@gmail.com>
2021-02-02 18:45:26 +0900

    Mission 2 = Fixed KeyError (used if block and not try/error)

--- students/17th/team1/markkim/user/views.py
+++ students/17th/team1/markkim/user/views.py
@@ -8,37 +8,32 @@ from . models     import User
 
 class UserView(View):
     def post(self, request):
-        data         = json.loads(request.body)
-        email        = data['email']
-        password     = data['password']
-        full_name    = data['full_name']
-        phone_number = data['phone_number']
-        username     = data['username']
+        data = json.loads(request.body)
 
-        if email == '' or password == '':
+        if 'email' not in data or 'password' not in data:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-        if '@' not in email or '.' not in email:
+        if '@' not in data['email'] or '.' not in data['email']:
             return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
         
-        if len(password) < 8:
+        if len(data['password']) < 8:
             return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
         
-        if User.objects.filter(full_name=full_name).exists():
+        if User.objects.filter(full_name=data['full_name']).exists():
             return JsonResponse({'message': 'FULL_NAME_ALREADY_EXISTS'}, status=409)
         
-        if User.objects.filter(email=email).exists():
+        if User.objects.filter(email=data['email']).exists():
             return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=409)
         
-        if User.objects.filter(phone_number=phone_number).exists():
+        if User.objects.filter(phone_number=data['phone_number']).exists():
             return JsonResponse({'message': 'PHONE_NUMBER_ALREADY_EXISTS'}, status=409)
         
         User.objects.create(
-                full_name    = full_name,
-                email        = email,
-                phone_number = phone_number,
-                username     = username,
-                password     = password
+                full_name    = data['full_name'],
+                email        = data['email'],
+                phone_number = data['phone_number'],
+                username     = data['username'],
+                password     = data['password']
                 )
         
         return JsonResponse({'message': 'SUCCESS'}, status=200)
