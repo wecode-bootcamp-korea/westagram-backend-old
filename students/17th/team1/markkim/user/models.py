8b8351ed08d4628a731e7b52430cece4b185c886
markkimjr <markkimjr96@gmail.com>
2021-02-02 16:57:59 +0900

    Mission 2 - used .filter() instead of .values_list, saved data values in variables

--- students/17th/team1/markkim/user/models.py
+++ students/17th/team1/markkim/user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     full_name    = models.CharField(max_length=30)
     email        = models.EmailField()
-    phone_number = models.IntegerField(null=True)
+    phone_number = models.IntegerField()
     username     = models.CharField(max_length=20)
     password     = models.CharField(max_length=20)
 
--- students/17th/team1/markkim/user/views.py
+++ students/17th/team1/markkim/user/views.py
@@ -8,35 +8,37 @@ from . models     import User
 
 class UserView(View):
     def post(self, request):
-        data = json.loads(request.body)
-        phone_number = data.get('phone_number', None)
+        data         = json.loads(request.body)
+        email        = data['email']
+        password     = data['password']
+        full_name    = data['full_name']
+        phone_number = data['phone_number']
+        username     = data['username']
 
         if 'email' not in data.keys() or 'password' not in data.keys():
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-        if '@' not in data['email'] or '.' not in data['email']:
+        if '@' not in email or '.' not in email:
             return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
         
-        if len(data['password']) < 8:
+        if len(password) < 8:
             return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
         
-        if (data['full_name'],) in User.objects.values_list('full_name'):
+        if User.objects.filter(full_name=full_name).exists():
             return JsonResponse({'message': 'FULL_NAME_ALREADY_EXISTS'}, status=409)
         
-        if (data['email'],) in User.objects.values_list('email'):
+        if User.objects.filter(email=email).exists():
             return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=409)
         
-        if phone_number:
-            
-            if (int(phone_number),) in User.objects.values_list('phone_number'):
-                return JsonResponse({'message': 'PHONE_NUMBER_ALREADY_EXISTS'}, status=409)
+        if User.objects.filter(phone_number=phone_number).exists():
+            return JsonResponse({'message': 'PHONE_NUMBER_ALREADY_EXISTS'}, status=409)
         
         User.objects.create(
-                full_name    = data['full_name'],
-                email        = data['email'],
-                phone_number = data['phone_number'],
-                username     = data['username'],
-                password     = data['password']
+                full_name    = full_name,
+                email        = email,
+                phone_number = phone_number,
+                username     = username,
+                password     = password
                 )
         
         return JsonResponse({'message': 'SUCCESS'}, status=200)
