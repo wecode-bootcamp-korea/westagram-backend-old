2cc2441e33111444a68ccc530b891b8c099fba24
Gyubster <kyunbo0910@naver.com>
2021-02-03 12:33:12 +0900

    FIX: fixed models.py, views.py, urls.py in westagram

--- students/17th/team1/gyuboshim/westagram/user/models.py
+++ students/17th/team1/gyuboshim/westagram/user/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class User(models.Model):
     phone_number    =   models.CharField(max_length = 15)
-    email_adress    =   models.CharField(max_length = 50)
-    name            =   models.CharField(max_length = 10)
-    nickname        =   models.CharField(max_length = 10)
-    password        =   models.CharField(max_length = 15)
+    email_adress    =   models.CharField(max_length = 100)
+    name            =   models.CharField(max_length = 50)
+    nickname        =   models.CharField(max_length = 50)
+    password        =   models.CharField(max_length = 300)
 
     class Meta:
         db_table = 'user'
--- students/17th/team1/gyuboshim/westagram/user/views.py
+++ students/17th/team1/gyuboshim/westagram/user/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts   import render
+from django.views       import View
+from django.http        import HttpResponse, JsonResponse
+from django.db.models   import Q
+
+from .models            import User
 
 import json
-from django.views   import View
-from django.http    import HttpResponse, JsonResponse
-
-from .models        import User
 
 PASSWORD_LEN_LIMIT = 8
 
@@ -19,50 +20,57 @@ class UserView(View):
             nickname        =   user_data['nickname']
             password        =   user_data['password']
 
-
-            if user_data['email_adress'] and user_data['password']:
+            if (email_adress or phone_number) and password:
                 
-                if user_data['email_adress'] == User.objects.filter(email_adress = email_adress):
-                    return JsonResponse({'message':'CHANGE_E-MAIL_ADRESS'}, status = 409)
+                if User.objects.filter(email_adress = email_adress).exists():
+                    return JsonResponse({"message":"EMAIL_ALREADY_EXISTS"}, status = 409)
                 
-                if user_data['phone_number'] == User.objects.filter(phone_number = phone_number):
-                    return JsonResponse({'message':'CHANGE_PHONE_NUMBER'}, status = 409)
+                if User.objects.filter(phone_number = phone_number).exists():
+                    return JsonResponse({"message":"PHONENUMBER_ALREADY_EXISTS"}, status = 409)
 
-                if '@' or '.' not in user_data['email_adress']:
-                    return JsonResponse({'message':'@_OR_._IS_MISSING_IN_E-MAIL_ADRESS'}, status = 400)
+                if '@' or '.' not in email_adress:
+                    return JsonResponse({"message":"@_OR_._DOES_NOT_EXIST"}, status = 400)
 
-                if len(user_data['password']) < PASSWORD_LEN_LIMIT:
-                    return JsonResponse({'message': 'PASSWORD_HAS_TO_BE_AT_LEAST_8-DIGIT'}, status = 400)
+                if len(password) < PASSWORD_LEN_LIMIT:
+                    return JsonResponse({"message": "PASSWORD_HAS_TO_BE_AT_LEAST_8-DIGIT"}, status = 400)
                     
                 user = User.objects.create(
-                                            phone_number    =   user_data['phone_number'],
-                                            email_adress    =   user_data['email_adress'],
-                                            name            =   user_data['name'],
-                                            nickname        =   user_data['nickname'],
-                                            password        =   user_data['password']
+                                            phone_number    =   phone_number,
+                                            email_adress    =   email_adress,
+                                            name            =   name,
+                                            nickname        =   nickname,
+                                            password        =   password
                                             )
                 
-                return JsonResponse({'message':'SUCESS'}, status = 200)
+                return JsonResponse({"message":"SUCESS"}, status = 200)
             
             else:
-                return JsonResponse({'message':'PASSWORD_OR_E-MAIL_IS_MISSING'}, status = 401)
+                return JsonResponse({"message":"PASSWORD_OR_E-MAIL_DOES_NOT_EXIST"}, status = 401)
         
-        except:
-            return JsonResponse({'message':'INVALID_KEYS'}, status = 400)
+        except KeyError:
+            return JsonResponse({"message":"INVALID_KEYS"}, status = 400)
     
 class LoginView(View):
     def post(self, request):
         data            =   json.loads(request.body)
-        phone_number    =   data['phone_number']
-        email_adress    =   data['email_adress']
-        password        =   data['password']
+        user_data       =   data['user']
+        phone_number    =   user_data['phone_number']
+        email_adress    =   user_data['email_adress']
+        name            =   user_data['name']
+        password        =   user_data['password']
 
         if User.objects.filter(
-                                phone_number = phone_number,
-                                email_adress = email_adress,
-                                password = password
+                                Q(phone_number  =   phone_number)|
+                                Q(email_adress  =   email_adress)|
+                                Q(name          =   name)
                                 ).exists():
-            return JsonResponse({'message': 'LOGIN SUCESS'}, status = 200)
+            user = User.objects.get(
+                                Q(phone_number  =   phone_number)|
+                                Q(email_adress  =   email_adress)|
+                                Q(name          =   name)
+                                )
+            if user['password'] == password:
+                return JsonResponse({"message": "LOGIN_SUCESS"}, status = 200)
 
         else:
-            return JsonResponse({'message': 'ID OR PASSWORD DOES NOT MATCH'}, status = 401)
+            return JsonResponse({"message": "ID_OR_PASSWORD_DOES_NOT_MATCH"}, status = 401)
