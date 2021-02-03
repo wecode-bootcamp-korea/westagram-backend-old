3e93e62ae3502d445aaea0b9167f385346ac8400
yangahh <toaur6802@naver.com>
2021-02-04 04:07:30 +0900

    Modify: created_at, updated_at의 DateField를 DateTimeField로 변경

--- students/17th/team1/hanahyang/user/models.py
+++ students/17th/team1/hanahyang/user/models.py
@@ -5,8 +5,8 @@ class User(models.Model):
     name       = models.CharField(max_length=20, unique=True) 
     phone      = models.CharField(max_length=15, unique=True)
     password   = models.CharField(max_length=300)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
