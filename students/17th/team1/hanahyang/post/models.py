f191f39da136885b8d1d74cbd5eacc75bf7b82ac
yangahh <toaur6802@naver.com>
2021-02-04 04:08:16 +0900

    Modify: created_at, updated_at의 DateField를 DateTimeField로 변경

--- students/17th/team1/hanahyang/post/models.py
+++ students/17th/team1/hanahyang/post/models.py
@@ -6,8 +6,8 @@ class Post(models.Model):
     user       = models.ForeignKey('user.User', on_delete=models.CASCADE) 
     image_url  = models.URLField(max_length=500)
     content    = models.TextField(null=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'posts'
@@ -16,8 +16,8 @@ class Comment(models.Model):
     user       = models.ForeignKey('user.User', on_delete=models.CASCADE)
     post       = models.ForeignKey('Post', on_delete=models.CASCADE)
     content    = models.CharField(max_length=500)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'comments'
