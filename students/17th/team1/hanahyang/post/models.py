4c941b2e3f52b71323ddeb889323ebc9ee4ee164
yangahh <toaur6802@naver.com>
2021-02-04 02:23:36 +0900

    Add: post/models.py - comments 테이블 추가

--- students/17th/team1/hanahyang/post/models.py
+++ students/17th/team1/hanahyang/post/models.py
@@ -3,7 +3,7 @@ from django.db   import models
 from user.models import User
 
 class Post(models.Model):
-    user       = models.ForeignKey(User, on_delete=models.CASCADE) 
+    user       = models.ForeignKey('user.User', on_delete=models.CASCADE) 
     image_url  = models.URLField(max_length=500)
     content    = models.TextField(null=True)
     created_at = models.DateField(auto_now_add=True)
@@ -11,3 +11,13 @@ class Post(models.Model):
 
     class Meta:
         db_table = 'posts'
+
+class Comment(models.Model):
+    user       = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    post       = models.ForeignKey('Post', on_delete=models.CASCADE)
+    content    = models.CharField(max_length=500)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        db_table = 'comments'
