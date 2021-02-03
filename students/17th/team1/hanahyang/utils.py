f784ffa0a93ad16745cb472a7531c81635665f6b
yangahh <toaur6802@naver.com>
2021-02-04 01:47:35 +0900

    Create: login_decorator 생성

---
+++ students/17th/team1/hanahyang/utils.py
@@ -0,0 +1,32 @@
+import json, jwt
+
+from django.http import JsonResponse
+
+from user.models import User
+from my_settings import SECRET
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        
+        # login check
+        if "Authorization" not in request.headers:
+            return JsonResponse({"message": "NEED_LOGIN"}, status=401)
+        
+        try:
+            access_token = request.headers['Authorization']
+            payload      = jwt.decode(access_token, SECRET['secret'], algorithms=['HS256'])
+            login_user   = User.objects.get(id=payload['user_id'])
+            request.user = login_user
+            return func(self, request, *args, **kwargs)
+        
+        except jwt.DecodeError:
+            return JsonResponse({'message': 'INVALID_TOKEN'}, status=401)
+        
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+    return wrapper
+
+
+
+
