98b9504dae2d68b82c92160b4133d69bd2c79356
안재이 <jaeyiahn@gmail.com>
2021-02-03 12:23:40 +0900

    Modify: 로그인 엔드포인트 구현

--- students/17th/team3/jaeyiahn/user/views.py
+++ students/17th/team3/jaeyiahn/user/views.py
@@ -1,7 +1,8 @@
 import json
 
-from django.views import View
-from django.http  import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Q
 
 from .models import User
 
@@ -14,24 +15,24 @@ class SignUpView(View):
             data     = json.loads(request.body)
 
             username = data.get('username')
-            email    = data.get('email')
             phone    = data.get('phone')
-            password = data.get('password')
-
+            email    = data['email']
+            password = data['password']
+            
             if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'message': 'EMAIL_EXISTS'}, status=400)
             
             if '@' and '.' not in email:
                 return JsonResponse({'message': 'EMAIL_INVALID'}, status=400)
-            
+
             if len(password) < MINIMUM_PASSWORD_LENGTH:
                 return JsonResponse({'message': 'SHORT_PASSWORD'}, status=400)
 
             if User.objects.filter(username=data['username']).exists():
-                    return JsonResponse({'message': 'USERNAME_EXISTS'}, status=400)
+                return JsonResponse({'message': 'USERNAME_EXISTS'}, status=400)
 
             if User.objects.filter(phone=data['phone']).exists():
-                    return JsonResponse({'message': 'PHONE_EXISTS'}, status=400)
+                return JsonResponse({'message': 'PHONE_EXISTS'}, status=400)
 
             User.objects.create(username=username, email=email, phone=phone, password=password)
             return JsonResponse({'message': 'SUCCESS'}, status=200)
@@ -39,9 +40,22 @@ class SignUpView(View):
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
+#로그인    
+class LogInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
 
-
-    
-
-
-
+            username = data.get('username')
+            phone    = data.get('phone')
+            email    = data.get('email')
+            password = data['password']
+            
+            if User.objects.filter(Q(email=email)|Q(username=username)|Q(phone=phone)).exists():
+                if User.objects.get(Q(email=email)|Q(username=username)|Q(phone=phone)).password == password:
+                    return JsonResponse({'message': 'SUCCESS'}, status=200)
+                return JsonResponse({'message': 'INVALID_PASSWORD'}, status=401)
+            return JsonResponse({'message': 'INVALID_USER'}, status=401)
+        
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
