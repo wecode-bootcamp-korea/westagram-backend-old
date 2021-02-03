a612e542ca0e4cc53c0f6fe5fc0ea03b2fe9437d
markkimjr <markkimjr96@gmail.com>
2021-02-03 11:59:10 +0900

    Mission 2 - added try/except block

--- students/17th/team1/markkim/user/models.py
+++ students/17th/team1/markkim/user/models.py
@@ -3,9 +3,12 @@ from django.db import models
 class User(models.Model):
     full_name    = models.CharField(max_length=30)
     email        = models.EmailField()
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=30)
     username     = models.CharField(max_length=20)
-    password     = models.CharField(max_length=20)
+    password     = models.CharField(max_length=300)
 
     class Meta:
         db_table = 'users'
+
+
+
--- students/17th/team1/markkim/user/views.py
+++ students/17th/team1/markkim/user/views.py
@@ -1,4 +1,5 @@
 import json
+import bcrypt
 
 from django.views import View
 from django.http  import JsonResponse, HttpResponse
@@ -8,37 +9,50 @@ from . models     import User
 
 class UserView(View):
     def post(self, request):
-        data = json.loads(request.body)
+        try:
+            data         = json.loads(request.body)
+            email        = data['email']
+            password     = data['password']
+            full_name    = data['full_name']
+            phone_number = data['phone_number']
+            username     = data['username']
+ 
+            if not email or not password:
+                return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-        if 'email' not in data or 'password' not in data:
+            if '@' not in email or '.' not in email:
+                return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
+            
+            if len(password) < 8:
+                return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
+            
+            if User.objects.filter(full_name=full_name).exists():
+                return JsonResponse({'message': 'FULL_NAME_ALREADY_EXISTS'}, status=409)
+            
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=409)
+            
+            if User.objects.filter(phone_number=phone_number).exists():
+                return JsonResponse({'message': 'PHONE_NUMBER_ALREADY_EXISTS'}, status=409)
+            
+            User.objects.create(
+                    full_name    = full_name,
+                    email        = email,
+                    phone_number = phone_number,
+                    username     = username,
+                    password     = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+                    )
+        
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
+        except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-        if '@' not in data['email'] or '.' not in data['email']:
-            return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
-        
-        if len(data['password']) < 8:
-            return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
-        
-        if User.objects.filter(full_name=data['full_name']).exists():
-            return JsonResponse({'message': 'FULL_NAME_ALREADY_EXISTS'}, status=409)
-        
-        if User.objects.filter(email=data['email']).exists():
-            return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=409)
-        
-        if User.objects.filter(phone_number=data['phone_number']).exists():
-            return JsonResponse({'message': 'PHONE_NUMBER_ALREADY_EXISTS'}, status=409)
-        
-        User.objects.create(
-                full_name    = data['full_name'],
-                email        = data['email'],
-                phone_number = data['phone_number'],
-                username     = data['username'],
-                password     = data['password']
-                )
-        
-        return JsonResponse({'message': 'SUCCESS'}, status=200)
-
-
-
-
-
+#class LoginView(View):
+#    def post(self, request):
+#        data             = json.loads(request.body)
+#        hashed_password  = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()) 
+#
+#        if 'password' not in data or 'email' not in data:
+#            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+#        
+#        if  
