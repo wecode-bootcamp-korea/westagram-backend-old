bfa2457c47b583331ef3a66a2cf9eb6f908a1304
chaerin <cr434972@gmail.com>
2021-02-04 18:35:15 +0900

    Modify: Mission2 빈줄제거 및 수정

--- students/17th/raon/chaerin/user/views.py
+++ students/17th/raon/chaerin/user/views.py
@@ -10,18 +10,16 @@ MINIMUN_PASSWORD_LENGTH = 8
 class UserSignUpView(View):
     def post(self, request):
         try:
-            data = json.loads(request.body)
-            username = data.get('username', None)
-            email = data.get('email', None)
-            password = data.get('password', None)
-            phone_num = data.get('phone_num', None)
+            data       = json.loads(request.body)
+            username   = data.get('username', None)
+            email      = data.get('email', None)
+            password   = data.get('password', None)
+            phone_num  = data.get('phone_num', None)
 
             if email and password:
                 email_form = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
                 
-                if email_form.match(str(email)):
-                    email = email
-                else:
+                if not email_form.match(str(email)):
                     return JsonResponse({'MESSSAGE': 'INVALID_EMAIL'}, status=400)
 
                 if len(password) < MINIMUN_PASSWORD_LENGTH:
@@ -31,12 +29,11 @@ class UserSignUpView(View):
                     return JsonResponse({'MESSSAGE': 'ALREADY_USE_EMAIL'}, status=400)
 
                 user_data = Account.objects.create(
-                        username = username,
-                        email = email,
-                        password = password,
-                        phone_num = phone_num
+                        username   = username,
+                        email      = email,
+                        password   = password,
+                        phone_num  = phone_num
                         )
-
                 return JsonResponse({'MESSSAGE': 'SUCCESS'}, status=200)
 
             return JsonResponse({'MESSSAGE': 'KEY_ERROR'}, status=400)
