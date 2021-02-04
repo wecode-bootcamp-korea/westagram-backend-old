bcad1e6c7b1d590b71d7ca4783dc91a832199f00
ScottieKim <fmychioh@gmail.com>
2021-02-04 18:35:31 +0900

    UPDATE: 컨벤션 수정했습니다.

--- students/17th/team1/ChiohKim/user/views.py
+++ students/17th/team1/ChiohKim/user/views.py
@@ -9,23 +9,25 @@ from .models      import Account
 class SignupView(View):
     def post(self, request):
         signup_data = json.loads(request.body)
+        
         MINIMUM_PASSWORD_LENGTH = 8
 
         try:
             if Account.objects.filter(Q(email=signup_data['email']) | Q(phonenumber=signup_data['phonenumber'])|Q(nickname=signup_data['nickname'])):
                 return JsonResponse({"message":"EXIST_USER"}, status=400) 
+        
             if len('password') < MINIMUM_PASSWORD_LENGTH:
                 return JsonResponse({"message":"INVALID_PASSWORD"}, status=400)
+        
             if '@' or '.' not in email:
                 return JsonResponse({"message":"INVALID_EMAIL"}, status=400) 
              
             Account.objects.create(
-                    email       = signup_data['email'],
-                    password    = signup_data['password'],
-                    nickname    = signup_data['nickname'],
-                    name        = signup_data['name'],
-                    phonenumber = signup_data['phonenumber'],
-            
+                email       = signup_data['email'],
+                password    = signup_data['password'],
+                nickname    = signup_data['nickname'],
+                name        = signup_data['name'],
+                phonenumber = signup_data['phonenumber'],
             )
 
             return JsonResponse({"message":"SUCCESS"}, status=200)
