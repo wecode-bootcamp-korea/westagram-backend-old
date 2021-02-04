471111711dcabc2a095c89283160a2d2e40b592e
이준하 <ijunha@ijunhaui-MacBookPro.local>
2021-02-04 20:01:06 +0900

    Update : 파일수정했습니다

--- students/17th/team4/leejunha/user/models.py
+++ students/17th/team4/leejunha/user/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=400)
+
+    class Meta:
+        db_table = "menus"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "categories"
+
+
+class Drink(models.Model):
+    category     = models.ForeignKey('Category', on_delete=models.CASCADE)
+    korean_name  = models.CharField(max_length=45)
+    english_name = models.CharField(max_length=45)
+    description  = models.TextField()
+
+    class Meta:
+        db_table = "drinks"
+
+
+class Nutrition(models.Model):
+    drink            = models.ForeignKey('Drink', on_delete=models.CASCADE)
+    one_serving_kcal = models.DecimalField(max_digits=6, decimal_places=2)
+    sodium_mg        = models.DecimalField(max_digits=6, decimal_places=2)
+    saturated_fat_g  = models.DecimalField(max_digits=6, decimal_places=2)
+    sugars_g         = models.DecimalField(max_digits=6, decimal_places=2)
+    protein_g        = models.DecimalField(max_digits=6, decimal_places=2)
+    caffeine_mg      = models.DecimalField(max_digits=6, decimal_places=2)
+    size_ml          = models.CharField(max_length=45)
+    size_fluid_ounce = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = "nutritions"
+
+
+class Image(models.Model):
+    drink     = models.ForeignKey('Drink', on_delete=models.CASCADE)
+    image_url = models.CharField(max_length=2000)
+
+    class Meta:
+        db_table = "images"
+
+
+class Allergy(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = "allergies"
+
+
+class Allergy_drink(models.Model):
+    allergy = models.ForeignKey('Allergy', on_delete=models.CASCADE)
+    drink   = models.ForeignKey('Drink', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "allergies_drink"
--- students/17th/team4/leejunha/westagram/settings.py
+++ students/17th/team4/leejunha/westagram/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders'
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -77,9 +78,13 @@ WSGI_APPLICATION = 'westagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+        'USER': 'root',
+        'PASSWORD': 'wnskdpdyd',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        }
 }
 
 
