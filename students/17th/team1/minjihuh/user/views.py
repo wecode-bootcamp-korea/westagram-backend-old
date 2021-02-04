08a9566d5e400ce5b552f2ff82063579eec94ae6
mjhuh263 <mjhuh263@gmail.coom>
2021-02-04 09:54:35 +0900

    Modified : Unneccessary If Statemements, user_id Vaiable

--- students/17th/team1/minjihuh/user/views.py
+++ students/17th/team1/minjihuh/user/views.py
@@ -29,31 +29,31 @@ class SignUpView(View):
             username                 = data['username']
 
             if name == "":
-                return JsonResponse({"MESSAGE" : "NAME_REQUIRED"}, status=400)
+                return JsonResponse({"message" : "NAME_REQUIRED"}, status=400)
 
             if phone == "" and email == "":
-                return JsonResponse({"MESSAGE" : "EMAIL_OR_PHONE_NUMBER_REQUIRED"}, status=400)
+                return JsonResponse({"message" : "EMAIL_OR_PHONE_NUMBER_REQUIRED"}, status=400)
 
             if username == "":
-                return JsonResponse({"MESSAGE" : "USERNAME_REQUIRED"}, status=400)
+                return JsonResponse({"message" : "USERNAME_REQUIRED"}, status=400)
 
             if password == "":
-                return JsonResponse({"MESSAGE" : "PASSWORD_REQUIRED"}, status=400)
+                return JsonResponse({"message" : "PASSWORD_REQUIRED"}, status=400)
 
             if User.objects.filter(email=email).exists():
-                return JsonResponse({"MESSAGE": "EMAIL_EXISTS"}, status=400)
+                return JsonResponse({"message": "EMAIL_EXISTS"}, status=400)
                 
             if User.objects.filter(username=username).exists():
-                return JsonResponse({"MESSAGE" : "USERNAME_EXISTS"}, status=400)
+                return JsonResponse({"message" : "USERNAME_EXISTS"}, status=400)
 
             if not email_regex.search(email):
-                return JsonResponse({"MESSAGE" : "EMAIL_VALIDATION"}, status=400)
+                return JsonResponse({"message" : "EMAIL_VALIDATION"}, status=400)
 
             if not password_regex.match(password):
-                return JsonResponse({"MESSAGE" : "PASSWORD_VALIDATION"}, status=400)
+                return JsonResponse({"message" : "PASSWORD_VALIDATION"}, status=400)
 
             if not phone_regex.match(phone):
-                return JsonResponse({"MESSAGE" : "PHONE_VALIDATION"}, status=400)
+                return JsonResponse({"message" : "PHONE_VALIDATION"}, status=400)
 
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8') 
                 
@@ -65,10 +65,11 @@ class SignUpView(View):
                 username = username
             )
 
-            return JsonResponse({"MESSAGE" : "SUCCESS"}, status=200)
+            return JsonResponse({"message" : "SUCCESS"}, status=200)
         
         except KeyError:
-            return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status=400)
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+
 
 class SignInView(View):
     def post(self, request):
@@ -80,32 +81,22 @@ class SignInView(View):
             username    = data.get('username', None)
             password    = data.get('password', None)
 
-            if not (email and password):
-                return JsonResponse({"MESSAGE" : "EMAIl_AND_PASSWORD_REQUIRED"}, status=400)
-
-            if not (username and password):
-                return JsonResponse({"MESSAGE" : "USERNAME_AND_PASSWORD_REQUIRED"}, status=400)
-
-            if not (phone and password):
-                return JsonResponse({"MESSAGE" : "PHONE_AND_PASSWORD_REQUIRED"}, status=400)
-
-            if len(password) < MINIMUM_PASSWORD_LENGTH:
-                return JsonResponse({"MESSAGE" : "PASSWORD_MUST_BE_LONGER_THAN_EIGHT_LETTERS"}, status=400)
+            if not ((email or username or phone) and password):
+                return JsonResponse({"message" : "REQUIRED_FIELD"}, status=400)
 
             if User.objects.filter(Q(email=email) | Q(phone=phone) | Q(username=username)): 
                 user = User.objects.get(Q(email=email) | Q(phone=phone) | Q(username=username))
-                user_id = {'user' : user.id}
-
+                    
                 if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                    access_token = jwt.encode(user_id , SECRET_KEY, algorithm=ALGORITHM)
+                    access_token = jwt.encode({'user' : user.id}, SECRET_KEY, algorithm=ALGORITHM)
                     return JsonResponse({"MESSAGE" : "SUCCESS", "TOKEN" : access_token}, status=200)
 
-                else :
+                else:
                     JsonResponse({"MESSAGE" : "UNAUTHORIZED_APPROACH"}, status=401)
 
                 return JsonResponse({"MESSAGE" : "INVALID_USER"}, status=401)
 
             return JsonResponse({"MESSAGE" : "SUCCESS"}, status=200)
-        
+
         except KeyError:
             return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status=400).decode("UTF-8")
