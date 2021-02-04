4b4bc92b7312495719d22d83466a65f8d606ac35
JELLO-Kim <cogus950403@gmail.com>
2021-02-04 15:40:15 +0900

    "REVIESE - edit token's secret_key"

--- students/17th/hkk/chaehyun-kim/user/views.py
+++ students/17th/hkk/chaehyun-kim/user/views.py
@@ -1,4 +1,6 @@
 import json, re
+import bcrypt
+import jwt
 
 from json.decoder       import JSONDecodeError
 
@@ -7,6 +9,7 @@ from django.views       import View
 from django.db.models   import Q
 
 from .models            import User
+from my_settings        import SECRET_KEY
 
 class UserView(View):
     def post(self, request):
@@ -17,6 +20,7 @@ class UserView(View):
             data            = json.loads(request.body)
             email           = data['email']
             password        = data['password']
+            hash_password   = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
             name            = data['name']
             phone_number    = data['phone_number']
             
@@ -27,12 +31,12 @@ class UserView(View):
             if ' 'in (email and password):
                 return JsonResponse({'message' : 'MEANINLESS_SPACE'}, status=400)
             if not re.search(password_valid, password):
-                return JsonResponse({'message' : 'SHORT_PASSWORD'}, status=400)
+                return JsonResponse({'message' : 'INVALID_PASSWORD'}, status=400)
             if not re.search(name_valid, name):
                 return JsonResponse({'message' : 'INVALID_NAME'}, status=400)
             User.objects.create(
                     email=email,
-                    password=password,
+                    password=hash_password.decode('utf-8'),
                     name=name,
                     phone_number=phone_number
                     )
@@ -49,17 +53,18 @@ class SignInView(View):
             data            = json.loads(request.body)
             password        = data.get('password', None)
             login_id        = data.get('login_id', None)
-
+            
             if not (login_id and password):
-                return JsonResponse({'message' : 'KEY_ERROR'}, status=401)
+                return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
             
             if not User.objects.filter(Q(email=login_id) | Q(name=login_id) | Q(phone_number=login_id)).exists():
                 return JsonResponse({'message' : 'INVALID_USER'}, status=401)
             user = User.objects.get(Q(email=login_id)|Q(name=login_id)|Q(phone_number=login_id))
             
-            if user.password != password:
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
                 return JsonResponse({'message' : 'INVALID_USER'}, status=401)
-            return JsonResponse({'message' : 'SUCCESS'}, status=200)
+            access_token = jwt.encode({'user' : user.id}, SECRET_KEY, algorithm='HS256')
+            return (JsonResponse({'message' : 'SUCCESS', "token" : access_token}, status=200))
 
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
