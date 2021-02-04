246664855111a1526a75af9df65dbdcb63344854
banana1019 <jyho930@gmail.com>
2021-02-04 13:29:46 +0900

    Refactor: user/views.py 수정 - JsonResponse 메세지 부분 수정 - 비밀번호 상수 선언 - data 변수 부분을 try문 내부로 이동시킴

--- students/17th/hkk/jeongyun-heo/user/views.py
+++ students/17th/hkk/jeongyun-heo/user/views.py
@@ -5,32 +5,36 @@ from django.views    import View
 
 from user.models import User
 
+
 class SignUpView(View):
 
+MINIMUM_PASSWORD_LENGTH = 8
+
     def post(self, request):
-        data     = json.loads(request.body)
-        name     = data['name']
-        email    = data['email']
-        password = data['password']
-        phone    = data['phone']
         
         try:
+            data = json.loads(request.body)
+            name = data['name']
+            email = data['email']
+            password = data['password']
+            phone    = data['phone']
+        
             if name and email and password and phone:
                     
                 if User.objects.filter(name=name).exists():
-                    return JsonResponse({'message': 'ID already exists'}, status=400)
+                    return JsonResponse({'message': 'ID_ALREADY_EXISTS'}, status=400)
                     
                 if User.objects.filter(email=email).exists():
-                    return JsonResponse({'message': 'Email already exists'}, status=400)
+                    return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=400)
 
                 if User.objects.filter(phone=phone).exists():
-                    return JsonResponse({'message': 'Phone number already exists'}, status=400)
+                    return JsonResponse({'message': 'PHONE_NUMBER_ALREADY_EXISTS'}, status=400)
                 
                 if '@' not in email or '.' not in email:
-                    return JsonResponse({'message': 'Invalid email'}, status=400)
+                    return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
 
-                if len(password) < 8:
-                    return JsonResponse({'message': 'Password must be at least 8 characters'}, status=400)
+                if len(password) < MINIMUM_PASSWORD_LENGTH:
+                    return JsonResponse({'message': 'PASSWORD_VALIDATION_ERROR'}, status=400)
 
                 User.objects.create(name=name, email=email, password=password, phone=phone)
 
