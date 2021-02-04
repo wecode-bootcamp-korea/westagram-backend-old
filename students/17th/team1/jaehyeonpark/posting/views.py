0d980aebdf9af872c486317ecd9681ce0eae7be2
namujinju <namujinju@gmail.com>
2021-02-04 13:27:59 +0900

    Modify: python convention에 맞춰 수정

--- students/17th/team1/jaehyeonpark/posting/views.py
+++ students/17th/team1/jaehyeonpark/posting/views.py
@@ -2,7 +2,7 @@ import json
 
 from django.http      import JsonResponse
 from django.views     import View
-from django.db.utils  import DataError, IntegrityError
+from django.db.utils  import DataError
 from django.db.models import Q
 
 from posting.models   import Post
@@ -11,11 +11,11 @@ from user.models      import User
 class PostView(View):
     def post(self, request):
         try:
-            data      = json.loads(request.body)
+            data         = json.loads(request.body)
             email        = data.get('email')
             phone_number = data.get('phone_number')
             account      = data.get('account')
-            image_url = data['image_url']
+            image_url    = data['image_url']
 
             if not (email or phone_number or account):
                 return JsonResponse({'message':'NO_USER_VALUE'}, status=400)
@@ -23,7 +23,6 @@ class PostView(View):
             if User.objects.filter(Q(email=email)|Q(phone_number=phone_number)|Q(account=account)).exists():
                 user = User.objects.get(Q(email=email)|Q(phone_number=phone_number)|Q(account=account))
                 Post.objects.create(user=user, image_url=image_url)
-
                 return JsonResponse({'message':'SUCCESS'}, status=200)
             
             return JsonResponse({'message':'INVALID_USER'}, status=400)
@@ -41,8 +40,8 @@ class ShowView(View):
     def get(self, request):
         try:
             posts   = Post.objects.all()
-            
             results = []
+            
             for post in posts:
                 results.append(
                     {
@@ -52,9 +51,6 @@ class ShowView(View):
                 )
 
             return JsonResponse({'results':results}, status=200)
-
-        except json.decoder.JSONDecodeError:
-            return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
         
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
