0a51309b44378a81149d3cd38e4878d6741f9a68
ScottieKim <fmychioh@gmail.com>
2021-02-05 13:37:36 +0900

    CREATE:
    LOGINVIEW in views.py
    path Loiginview in urls.py

--- students/17th/team1/ChiohKim/user/urls.py
+++ students/17th/team1/ChiohKim/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views    import SignupView #,LoginView
+from . views    import SignupView, LoginView
 
 urlpatterns = [
         path('/SignupView', SignupView.as_view()),
+        path('/LoginView', LoginView.as_view()),
 ]
--- students/17th/team1/ChiohKim/user/views.py
+++ students/17th/team1/ChiohKim/user/views.py
@@ -8,11 +8,11 @@ from .models      import Account
 
 class SignupView(View):
     def post(self, request):
-        signup_data = json.loads(request.body)
-        
-        MINIMUM_PASSWORD_LENGTH = 8
-
         try:
+            signup_data = json.loads(request.body)
+
+            MINIMUM_PASSWORD_LENGTH = 8
+            
             if Account.objects.filter(Q(email=signup_data['email']) | Q(phonenumber=signup_data['phonenumber'])|Q(nickname=signup_data['nickname'])):
                 return JsonResponse({"message":"EXIST_USER"}, status=400) 
         
@@ -35,4 +35,16 @@ class SignupView(View):
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
 
-            
+class LoginView(View):
+     def post(self, request):
+         try:
+             login_data = json.loads(request.body)
+             email      = login_data['email']
+             password   = login_data['password']
+
+             if Account.object.filter(email=email, password=password).exists():
+                 return JsonResponse({'message':"SUCCESS"}, status=200)
+             return JsonResponse({'message':"INVALID_USER"}, status=401)
+
+         except KeyError:
+             return JsonResponse({"message":"KEY_ERROR"})
