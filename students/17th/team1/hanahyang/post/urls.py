c8adff0ebcb84dc7546d5ad560d2279e663c3f89
yangahh <toaur6802@naver.com>
2021-02-05 16:01:24 +0900

    Modify: 게시물 수정/삭제, 댓글 수정/삭제 기능 추가

--- students/17th/team1/hanahyang/post/urls.py
+++ students/17th/team1/hanahyang/post/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import PostView, CommentView, PostDetailView, LikeView 
+from .views import (
+    PostView, 
+    PostDetailView, 
+    CommentView, 
+    CommentDetailView,
+    LikeView, 
+)
 
 urlpatterns = [
     path('', PostView.as_view()),
-    path('/comment', CommentView.as_view()),
     path('/<int:post_id>', PostDetailView.as_view()),
+    path('/comment', CommentView.as_view()),
+    path('/comment/<int:comment_id>', CommentDetailView.as_view()),
     path('/like', LikeView.as_view()),
 ]
--- students/17th/team1/hanahyang/post/views.py
+++ students/17th/team1/hanahyang/post/views.py
@@ -1,11 +1,12 @@
 import json
+from json.decoder import JSONDecodeError
 
 from django.views import View
 from django.http  import JsonResponse
 
 from .models      import Post, Comment, Like
 from user.models  import User
-from utils        import login_decorator
+from user.utils   import login_decorator
 
 class PostView(View):
     @login_decorator
@@ -24,7 +25,6 @@ class PostView(View):
             image_url = image_url,
             content   = content
         )
-
         return JsonResponse({'message': 'SUCCESS'}, status=200)
 
     def get(self, request):
@@ -36,7 +36,6 @@ class PostView(View):
                 'updated_at': post.updated_at,
             } for post in Post.objects.all()
         ]
-
         return JsonResponse({'data': post_list}, status=200)
 
 class CommentView(View):
@@ -52,27 +51,66 @@ class CommentView(View):
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
         
         # valid post check
-        if Post.objects.filter(id=post_id).exists():
-            Comment.objects.create(
-                user    = user,
-                post    = Post.objects.get(id=post_id),
-                content = content
-            )
+        if not Post.objects.filter(id=post_id).exists():
+            return JsonResponse({'message': 'INVALID_POST'}, status=400)
+
+        Comment.objects.create(
+            user    = user,
+            post    = Post.objects.get(id=post_id),
+            content = content
+        )
+        return JsonResponse({'message': 'SUCCESS'}, status=200)
+
+
+class CommentDetailView(View):
+    # update
+    @login_decorator
+    def post(self, request, comment_id):
+        try:
+            data = json.loads(request.body)
+
+            # vaild comment check
+            if not Comment.objects.filter(id=comment_id).exists():
+                return JsonResponse({'message': 'INVALID_COMMENT'}, status=400)
+            
+            comment = Comment.objects.get(id=comment_id)
+            
+            # valid user check
+            if comment.user != request.user:
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+            comment.content = data.get('content', comment.content)
+            comment.save()
             return JsonResponse({'message': 'SUCCESS'}, status=200)
 
-        return JsonResponse({'message': 'POST_DOES_NOT_EXISTS'}, status=400)
+        except JSONDecodeError:
+            return JsonResponse({'message': 'REQUEST_BOBY_DOES_NOT_EXISTS'}, status=400)
+
+    @login_decorator
+    def delete(self, request, comment_id):
+        # vaild comment check
+        if not Comment.objects.filter(id=comment_id).exists():
+            return JsonResponse({'message': 'INVALID_COMMENT'}, status=400)
+        
+        comment = Comment.objects.get(id=comment_id)
+        
+        # valid user check
+        if comment.user != request.user:
+            return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+        comment.delete()
+        return JsonResponse({'message': 'SUCCESS'}, status=200)
 
 class PostDetailView(View):
     def get(self, request, post_id):
         # valid post check
-        if not Post.objects.filter(id=post_id):
-            return JsonResponse({'message': 'POST_DOES_NOT_EXISTS'}, status=404)
+        if not Post.objects.filter(id=post_id).exists():
+            return JsonResponse({'message': 'INVALID_POST'}, status=404)
         
         context = {}
-
         # post 정보
         post = Post.objects.get(id=post_id)
-        
+
         context['user']       = post.user.name
         context['image_url']  = post.image_url
         context['content']    = post.content
@@ -90,6 +128,45 @@ class PostDetailView(View):
             context['comment_list'] = comment_list
 
         return JsonResponse({'data': context}, status=200)
+    
+    # update 
+    @login_decorator
+    def post(self, request, post_id):
+        try: 
+            data = json.loads(request.body)
+
+            # valid post check
+            if not Post.objects.filter(id=post_id).exists():
+                return JsonResponse({'message': 'INVALID_POST'}, status=404)
+            
+            post = Post.objects.get(id=post_id)
+
+            # valid user check
+            if post.user != request.user:
+                return JsonResponse({'message': 'INVALID_USER'}, status=400)
+
+            post.image_url = data.get('image_url', post.image_url)
+            post.content   = data.get('content', post.content)
+            post.save()
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
+        
+        except JSONDecodeError:
+            return JsonResponse({'message': 'REQUEST_BOBY_DOES_NOT_EXISTS'}, status=400)
+    
+    @login_decorator
+    def delete(self, request, post_id):
+        # valid post check
+        if not Post.objects.filter(id=post_id).exists():
+            return JsonResponse({'message': 'INVALID_POST'}, status=400)
+        
+        post = Post.objects.get(id=post_id)
+
+        # valid user check
+        if post.user != request.user:
+            return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+        post.delete()
+        return JsonResponse({'message': 'SUCCESS'}, status=200)
 
 class LikeView(View):
     @login_decorator
@@ -103,18 +180,19 @@ class LikeView(View):
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
         # valid post check
-        if Post.objects.filter(id=post_id).exists():
-            post = Post.objects.get(id=post_id)
+        if not Post.objects.filter(id=post_id).exists():
+            return JsonResponse({'message': 'INVALID_POST'}, status=400)
 
-            # like 안 했을 때만 추가, 이미 했으면 삭제
-            if post.liked_users.filter(id=user.id).exists():
-                post.liked_users.remove(user)
-                message = 'Cancle'
-            else:
-                post.liked_users.add(user)
-                message = 'Like'
+        post = Post.objects.get(id=post_id)
 
-            like_count = post.liked_users.count()
-            return JsonResponse({'message': message, 'like_count': like_count}, status=200)
+        if post.liked_users.filter(id=user.id).exists():
+            post.liked_users.remove(user)
+            message = 'Cancle'
+        else:
+            post.liked_users.add(user)
+            message = 'Like'
 
-        return JsonResponse({'message': 'POST_DOES_NOT_EXISTS'}, status=400)
+        like_count = post.liked_users.count()
+
+        return JsonResponse({'message': message, 'like_count': like_count}, status=200)
+
