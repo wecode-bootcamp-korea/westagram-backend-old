e0a590f922e6fef88877bea22e6f49925960525c
namujinju <namujinju@gmail.com>
2021-02-05 11:18:19 +0900

    Modify: login decorator 추가 - 사용자 정보와 비번을 입력받아 user를 반환하는 decorator 작성 - access token은 프론트에 저장됐다가 그쪽에서 보내줘야 하기 때문에 현 단계에선 구현 제외 - SignIn View에 login_decorator 적용

--- students/17th/team1/jaehyeonpark/user/views.py
+++ students/17th/team1/jaehyeonpark/user/views.py
@@ -7,6 +7,7 @@ from django.db.models import Q
 
 from user.models      import User
 from my_settings      import SECRET_KEY
+from westagram.utils  import login_decorator
 
 PASSWORD_MINIMUM_LENGTH = 8
 
@@ -59,31 +60,7 @@ class SignUpView(View):
             return JsonResponse({'message':'INTEGRITY_ERROR'}, status=400)        
 
 class SignInView(View):
-    def post(self, request):
-        try:
-            data         = json.loads(request.body)
-            email        = data.get('email')
-            phone_number = data.get('phone_number')
-            account      = data.get('account')
-            password     = data['password']
-            user_account = [email, phone_number, account]
-
-            if not (email or account or phone_number):
-                return JsonResponse({'message':'KEY_ERROR'}, status=400)
-            
-            if User.objects.filter(Q(email=email)|Q(phone_number=phone_number)|Q(account=account)).exists():
-                user = User.objects.get(Q(email=email)|Q(phone_number=phone_number)|Q(account=account))
-
-                if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                    access_token = jwt.encode({'user_id': user.id}, SECRET_KEY, algorithm='HS256')
-                    return JsonResponse({'message':'SUCCESS', 'access_token':access_token}, status=200)
-                else:
-                    return JsonResponse({'message':'INVALID_PASSWORD'}, status=400)
-
-            return JsonResponse({'message':'INVALID_USER'}, status=401)
-
-        except json.decoder.JSONDecodeError:
-            return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
-        
-        except KeyError:
-            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+    @login_decorator
+    def post(self, request, user):
+        access_token = jwt.encode({'user_id': user.id}, SECRET_KEY, algorithm='HS256')
+        return JsonResponse({'message':'SUCCESS', 'access_token':access_token}, status=200)
---
+++ students/17th/team1/jaehyeonpark/westagram/utils.py
@@ -0,0 +1,34 @@
+import json, bcrypt
+
+from django.db.models import Q
+
+from user.models      import User
+
+def login_decorator(func):
+    def wrapper(self, request):
+        try:
+            data         = json.loads(request.body)
+            email        = data.get('email')
+            phone_number = data.get('phone_number')
+            account      = data.get('account')
+            password     = data['password']
+
+            if not (email or account or phone_number):
+                return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+            if not User.objects.filter(Q(email=email)|Q(phone_number=phone_number)|Q(account=account)).exists():
+                return JsonResponse({'message':'INVALID_USER'}, status=401)
+
+            user = User.objects.get(Q(email=email)|Q(phone_number=phone_number)|Q(account=account))
+
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'message':'INVALID_PASSWORD'}, status=400)
+
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
+            
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+        return func(self, request, user)
+    return wrapper
