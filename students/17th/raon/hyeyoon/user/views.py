bc19946e55a6fbe1c0df9627c7928e6ff3404bc5
hye yoon <hyeyoon@hyeui-MacBookPro.local>
2021-02-07 17:00:45 +0900

    Modify: password hashing

--- students/17th/raon/hyeyoon/user/views.py
+++ students/17th/raon/hyeyoon/user/views.py
@@ -1,4 +1,5 @@
 import json
+import bcrypt
 
 from django                 import views
 from django.views           import View
@@ -11,11 +12,12 @@ PASSWORD_MIN_LENGTH = 8
 class UserSignUpView(View):
     def post(self, request):
         try:
-            data         = json.loads(request.body)
-            name         = data['name']
-            password     = data['password']
-            email        = data['email']
-            phone_number = data['phone_number']
+            data            = json.loads(request.body)
+            name            = data['name']
+            hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt())
+            password        = hashed_password.decode('utf-8')
+            email           = data['email']
+            phone_number    = data['phone_number']
 
             if Account.objects.filter(email = email).exists():
                 return JsonResponse({"MESSAGE":"INVALID_EMAIL"}, status = 400)
@@ -26,7 +28,7 @@ class UserSignUpView(View):
             if Account.objects.filter(phone_number = phone_number).exists():
                 return JsonResponse({"MESSAGE":"INVALID_PHONE_NUMBER"}, status = 400)
         
-            if len(password) < PASSWORD_MIN_LENGTH:
+            if len(data['password']) < PASSWORD_MIN_LENGTH:
                 return JsonResponse({"MESSAGE":"INVALID_PASSWORD"}, status = 400)
         
             if '@' not in email or '.' not in email:
