cbcefea9edee2781575fb03ba0a75d05cd6e1ffc
JELLO-Kim <cogus950403@gmail.com>
2021-02-08 17:58:43 +0900

    CREATE : Follow url path

--- students/17th/hkk/chaehyun-kim/user/urls.py
+++ students/17th/hkk/chaehyun-kim/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from .views import UserView, SignInView
+from .views import UserView, SignInView, FollowView
 
 urlpatterns = [
     path('/signup', UserView.as_view()),
-    path('/signin', SignInView.as_view())
+    path('/signin', SignInView.as_view()),
+    path('/follow', FollowView.as_view()),
 ]
