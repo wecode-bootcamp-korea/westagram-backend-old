842d12cf9b3e63e9f4e1b49a7369e987b49b1e9f
JELLO-Kim <cogus950403@gmail.com>
2021-02-08 15:23:43 +0900

    CREATE : LikeView.as_view

--- students/17th/hkk/chaehyun-kim/posting/urls.py
+++ students/17th/hkk/chaehyun-kim/posting/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PostingView, CommentView
+from .views import PostingView, CommentView, LikeView
 
 urlpatterns = [
     path('', PostingView.as_view()),
-    path('/comment', CommentView.as_view())
+    path('/comment', CommentView.as_view()),
+    path('/like', LikeView.as_view())
 ]
