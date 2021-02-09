e863c175d3cf8d8a16ce812051857a98877c81f4
JELLO-Kim <cogus950403@gmail.com>
2021-02-10 03:20:52 +0900

    CREATE : recomment field in Commnt model

--- students/17th/hkk/chaehyun-kim/posting/models.py
+++ students/17th/hkk/chaehyun-kim/posting/models.py
@@ -18,6 +18,7 @@ class Comment(models.Model):
     user        = models.ForeignKey(User, on_delete=models.CASCADE)
     comment     = models.CharField(max_length=100)
     create_at   = models.DateTimeField(auto_now_add=True)
+    parent      = models.ForeignKey('self', on_delete=models.CASCADE, related_name='recomment', null=True)
 
     class Meta:
         db_table = 'comments'
