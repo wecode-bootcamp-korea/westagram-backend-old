9dfc56a8313a0193990dbd58805edddc48c8dc79
sayxyoung <hyjlove11@gmail.com>
2021-02-09 17:39:38 +0900

    ADD : user/models.py에 모델 추가
          follow 기능구현을 위한 모델추가
          1. class User 속성추가
          	- relationship(MTOM)
          2. class Relationship 생성

--- students/17th/team-one/heeyoungjeong/user/models.py
+++ students/17th/team-one/heeyoungjeong/user/models.py
@@ -7,7 +7,9 @@ class User(models.Model):
     email        = models.EmailField(max_length=30)
     password     = models.CharField(max_length=300)
     phone_number = models.CharField(max_length=20)
-
+    relationship = models.ManyToManyField('self', through='Relationship',
+                                          symmetrical=False,
+                                          )
 
     def __str__(self):
         return f'{self.name}'
@@ -16,5 +18,12 @@ class User(models.Model):
         db_table = 'User'
 
 
+class Relationship(models.Model):
+    from_user = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='from_user')
+    to_user = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='to_user')
 
+    def __str__(self):
+        return f'{self.from_user}:{self.to_user}'
 
+    class Meta:
+        db_table = 'Relationship'
