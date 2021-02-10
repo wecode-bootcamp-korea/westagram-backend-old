42291d02a7842ace12926360eee95bda2e95b113
banana1019 <jyho930@gmail.com>
2021-02-10 17:22:02 +0900

    Refactor: 비밀번호 해싱, 토큰 발행 - models.py에서 비밀번호 길이 늘림 - bcrypt로 비밀번호 해싱 - jwt으로 access token 발행

--- students/17th/hkk/jeongyun-heo/user/models.py
+++ students/17th/hkk/jeongyun-heo/user/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     name     = models.CharField(max_length=20)
     email    = models.EmailField(max_length=100)
-    password = models.CharField(max_length=20)
+    password = models.CharField(max_length=100)
     phone    = models.CharField(max_length=20)
 
     class Meta:
--- students/17th/hkk/jeongyun-heo/user/views.py
+++ students/17th/hkk/jeongyun-heo/user/views.py
@@ -1,16 +1,16 @@
 import json
+import bcrypt
+import jwt
 
 from django.http     import JsonResponse, HttpResponse
 from django.views    import View
 
 from user.models import User
-
+from my_settings import SECRET_KEY, ALGORITHM
 
 class SignUpView(View):
-
-    MINIMUM_PASSWORD_LENGTH = 8
-
     def post(self, request):
+        MINIMUM_PASSWORD_LENGTH = 8
         try:
             data     = json.loads(request.body)
             name     = data['name']
@@ -35,7 +35,9 @@ class SignUpView(View):
                 if len(password) < MINIMUM_PASSWORD_LENGTH:
                     return JsonResponse({'message': 'PASSWORD_VALIDATION_ERROR'}, status=400)
 
-                User.objects.create(name=name, email=email, password=password, phone=phone)
+                hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+                User.objects.create(name=name, email=email, password=hashed_password, phone=phone)
 
                 return JsonResponse({'message': 'SUCCESS'}, status=200)
             
@@ -53,8 +55,10 @@ class SignInView(View):
                 return JsonResponse({'message': 'INVALID_USER'}, status=401)
             user = User.objects.get(email=email)
             
-            if user.password == password:
-                return JsonResponse({'message': 'SUCCESS'}, status=200)
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+            # if user.password == password:
+                token = jwt.encode({'user_id': user.id}, SECRET_KEY, ALGORITHM)
+                return JsonResponse({'message': 'SUCCESS', 'access_token': token}, status=200)
             return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
         except KeyError:
