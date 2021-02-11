156ae8b7c1ea39b286ccd12ebf31d8a7f4994763
sua cho <devsu7477@gmail.com>
2021-02-11 16:15:06 +0900

    MODIFY: utils.py 로그인 데코레이터  수정

--- students/17th/team1/suacho/user/utils.py
+++ students/17th/team1/suacho/user/utils.py
@@ -8,7 +8,7 @@ from user.models import User
 def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try:
-            access_token = request.headers['Authorization']
+            access_token = request.headers.get('Authorization', None)
             payload = jwt.decode(access_token, SECRET, algorithms=ALGORITHM)
             user = User.objects.get(id=payload['id'])
             request.user = user
