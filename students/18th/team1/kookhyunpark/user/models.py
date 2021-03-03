456de67638366e67f2e7087694e6bf74fbbf198f
parkgood1020 <parkgood1020@gmail.com>
2021-03-04 00:31:56 +0900

    MODIFY : models.py, views.py

--- students/18th/team1/kookhyunpark/user/models.py
+++ students/18th/team1/kookhyunpark/user/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     full_name     = models.CharField(max_length=40, null=True)
     user_name     = models.CharField(max_length=20, unique=True)
     password      = models.CharField(max_length=20)
-    date_of_birth = models.DateField()
+    date_of_birth = models.DateField(null=True)
 
     class Meta:
         db_table = 'users'
--- students/18th/team1/kookhyunpark/user/views.py
+++ students/18th/team1/kookhyunpark/user/views.py
@@ -8,16 +8,37 @@ from user.models import User
 class SignUpView(View):
 
     def post(self, request):
-        data = json.loads(request.body)
-        user = User.objects.create(
-            email         = data['email'],
-            phone         = data['phone'],
-            full_name     = data['full_name'],
-            user_name     = data['user_name'],
-            password      = data['password'],
-            date_of_birth = data['date_of_birth'],
-        )
-        
-        #user = User.objects.get
 
-        return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+        try:
+            data = json.loads(request.body)
+
+            #email = data['email']
+            #phone = data['phone'].replace('-','')
+            #user_name     = data['user_name']
+            #password = data['password']
+
+            if not data['email'] or not data['password']:
+                return JsonResponse({'message':'KEY_ERROR'}, status=400)
+            elif '@' not in data['email'] or '.' not in data['email']:
+                return JsonResponse({'message':'EMAIL VALIDATION ERROR'}, status=400)
+            elif len(data['password']) < 8:
+                return JsonResponse({'message':'PASSWORD VALIDATION ERROR'}, status=400)
+            elif User.objects.filter(email=data['email']).exists() or User.objects.filter(phone=data['phone']).exists() or User.objects.filter(user_name=data['user_name']).exists():
+                return JsonResponse({'message':'USER ALREADY EXISTS'}, status=400)
+
+
+            user = User.objects.create(
+                email         = data['email'],
+                phone         = data['phone'].replace('-',''),
+                full_name     = data['full_name'],
+                user_name     = data['user_name'],
+                password      = data['password'],
+                date_of_birth = data['date_of_birth'],
+            )
+            return JsonResponse({'message':'SUCCESS'}, status=202)        
+        #except KeyError:
+        #    return JsonResponse({"message": "KEY_ERROR"}, status=400)
+        except:
+            return JsonResponse({"message": "VIEW ERROR"}, status=400)
+
+        #return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
