79f5ea1f0bdbf8302302e7916b54f7351d28e67f
parkgood1020 <parkgood1020@gmail.com>
2021-03-03 20:05:49 +0900

    Add: model.py create

--- students/18th/team1/kookhyunpark/user/models.py
+++ students/18th/team1/kookhyunpark/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SignUp(models.Model):
+    email         = models.EmailField(max_length=50)
+    phone         = models.CharField(max_length=11)
+    full_name     = models.CharField(max_length=40)
+    user_name     = models.CharField(max_length=20)
+    password      = models.CharField(max_length=20)
+    date_of_birth = models.DateField() 
