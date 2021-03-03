34b9834181e91dd8912189be6c4972f3b3c6ec04
daminan <damin0320@gmail.com>
2021-03-03 16:12:00 +0900

    Update : SECRET_KEY

--- students/18th/team1/daminan/westagram/settings.py
+++ students/18th/team1/daminan/westagram/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from my_settings import SECRET_KEY
+# 여기까지 하고 깃을 올려줘야 함.
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9&+pmz!x#b2k(u%444twr$50pd0$ded&fru3l)r&v^$tu!-4u&'
-
+SECRET_KEY = SECRET_KEY
+# 인증 인가 때 자세히 할 예정
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
