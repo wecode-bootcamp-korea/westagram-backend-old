b3eab4c83af70d1fd7baa4dccb16208d1cf1af80
taekhyang <xorgid1195@gmail.com>
2021-03-03 20:54:25 +0900

    MODIFY : modified email, password validation logic

--- students/18th/team1/taekhyang/account/views.py
+++ students/18th/team1/taekhyang/account/views.py
@@ -14,16 +14,16 @@ class SignUpView(View):
         password = json_data['password']
 
         if not email or not password:
-            return JsonResponse('{"message": "KEY_ERROR"}', status=400)
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
         p_email    = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9_]+\.[a-zA-Z-.]+$')
         p_password = re.compile(r'.{8,45}')
 
         # TODO : modify password validation check
-        is_valid_email    = True if p.match(email) else False 
-        is_valid_password = True if p.match(email) else False
+        is_valid_email    = True if p_email.match(email) else False 
+        is_valid_password = True if p_password.match(email) else False
 
         if not is_valid_email or not is_valid_password:
-            return JsonResponse('{"message": "WRONG_FORMAT"}', status=400)
+            return JsonResponse({'message': 'WRONG_FORMAT'}, status=400)
 
-        
+        return JsonResponse({'message': 'SUCCESS'}, status=200)
