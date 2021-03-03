5a4de1d68638c7ec0e36498c419c5a965693099b
ByungSoo Ye <ybs680318@gmail.com>
2021-03-03 20:02:02 +0900

    Add: User클래스 추가

--- students/18th/team1/byungsoo/user/models.py
+++ students/18th/team1/byungsoo/user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email    = models.CharField(max_length=45)
+    password = models.CharField(max_length=30)
+    
