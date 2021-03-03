2035322e0df51afd745db6cda820266d718d097e
Won <jungwon1423@gamil.com>
2021-03-03 16:11:32 +0900

    Add: my_settings.py 파일 추가

--- students/18th/team1/hur_jung_won/westargram/settings.py
+++ students/18th/team1/hur_jung_won/westargram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from my_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!&two=@&tfg#1)86^(4-=5z^w+geo(zrhd-x-s43&l%$!hl9_3'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
