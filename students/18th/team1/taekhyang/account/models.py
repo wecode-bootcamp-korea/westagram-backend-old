f29900689a0cef7dd29d1a063757ee74a0a81315
taekhyang <xorgid1195@gmail.com>
2021-03-03 19:06:55 +0900

    ADD : added User model

--- students/18th/team1/taekhyang/account/models.py
+++ students/18th/team1/taekhyang/account/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(max_length=128)
+    password = models.CharField(max_length=50)
+ 
