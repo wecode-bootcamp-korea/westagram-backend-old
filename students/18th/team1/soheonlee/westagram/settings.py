c9f15fbe45133e0f567688182ae26cab3728b404
James Seung Hyun Soun <james@gracefulrain.co>
2021-03-03 16:11:21 +0900

    Add: my_settings.py

--- students/18th/team1/soheonlee/westagram/settings.py
+++ students/18th/team1/soheonlee/westagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from my_settings import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_^y(2w+daxrd!emc-gnmk@q3io92f4rri%x(!#2nqmta-aq3eb'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
