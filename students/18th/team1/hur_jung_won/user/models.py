31764b9394d0138db4be67c6c09f4df51f6734ec
Won <jungwon1423@gamil.com>
2021-03-03 18:29:59 +0900

    Add: requirements.txt 추가

--- students/18th/team1/hur_jung_won/user/models.py
+++ students/18th/team1/hur_jung_won/user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 
+class User(models.Model):
+  name = models.CharField(max_length=45)
+  email = models.EmailField(max_length=100)
+  password = models.CharField(max_length=45)
