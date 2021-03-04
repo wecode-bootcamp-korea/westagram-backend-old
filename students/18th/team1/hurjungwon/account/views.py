70abb505d01247bd6ecb415295716b76fc911036
Won <jungwon1423@gamil.com>
2021-03-04 14:07:16 +0900

    Modify: password email 정규식 표현 사용

--- students/18th/team1/hurjungwon/account/views.py
+++ students/18th/team1/hurjungwon/account/views.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from django.views import View
 from django.http  import JsonResponse
@@ -11,18 +12,22 @@ class SignUpView(View):
         data = json.loads(request.body)
 
         try:
-            user_name    = data.get('user_name')
-            email        = data.get('email')
-            password     = data.get('password')
+            user_name    = data['user_name']
+            email        = data['email']
+            password     = data['password']
             name         = data.get('name', None)
             phone_number = data.get('phone_number', None)
+            
+            email_match = re.match('[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]+', email)
+            
+            if not (email_match):
+                return JsonResponse({'message': 'invalid email'}, status=400)
 
-            if not '@' or not '.' in data['email']:
-                return JsonResponse({'message': 'not valid email'}, status=400)
+            password_match = re.match('\S{8,}', password)
+            
+            if not(password_match):
+                return JsonResponse({'message': 'invalid password'}, status=400)
 
-            if len(data['password']) < 8:
-                return JsonResponse({'message': 'not valid password'}, status=400)
-    
             if User.objects.filter(user_name = user_name).exists():
                 return JsonResponse({'message': 'user_name already exists'}, status=400)
 
