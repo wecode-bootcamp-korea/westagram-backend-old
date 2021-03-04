02f3f10f196ee9d1be26587b66ed772c77eea345
taekhyang <xorgid1195@gmail.com>
2021-03-04 20:21:16 +0900

    ADD : created & migrated Posting models

---
+++ students/18th/team1/taekhyang/posting/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Posting(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True)
+    user         = models.ForeignKey('account.User', on_delete=models.CASCADE)
+
+    class Meta(object):
+        db_table = 'postings'
+
+
+class PostingImage(models.Model):
+    image   = models.TextField(max_length=1000)
+    posting = models.ForeignKey('Posting', on_delete=models.CASCADE)
+
+    class Meta(object):
+        db_table = 'posting_images'
--- students/18th/team1/taekhyang/project_westagram/settings.py
+++ students/18th/team1/taekhyang/project_westagram/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'account',
+    'posting',
 ]
 
 MIDDLEWARE = [
