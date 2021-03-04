55fb080ecf8980848bbbad2fece9848e2755951f
taekhyang <xorgid1195@gmail.com>
2021-03-04 21:41:25 +0900

    ADD : added post method in UploadView

--- students/18th/team1/taekhyang/posting/views.py
+++ students/18th/team1/taekhyang/posting/views.py
@@ -10,7 +10,7 @@ from account.models  import User
 from utils.debugger  import debugger
 
 
-TEST_USER_ID = 1
+TEST_USER_ID = 5
 
 class UploadView(View):
     def post(self, request):
@@ -18,18 +18,31 @@ class UploadView(View):
             data      = request.body
             json_data = json.loads(data)
 
-            image_url    = data['image_url']
-            
+            image_url = json_data['image_url']
+            content   = json_data['content']
+
             # temporary test User object
-            user = User.objects.filter(id=TEST_USER_ID).exists()
+            user = User.objects.filter(id=TEST_USER_ID).first()
             if not user:
                 return JsonResponse({'message': 'INVALID_USER'}, status=400)
 
-            posting = Posting(user=user)
-            for img in image_url:
-                posting_images = PostingImage(image_url=img, posting=posting)
+            posting = Posting.objects.create(user=user, content=content)
+            
+            if type(image_url) == list:
+                for img in image_url:
+                    PostingImage.objects.create(image_url=img, posting=posting)
+            else:
+                PostingImage.objects.create(image_url=image_url, posting=posting)
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)            
         except JSONDecodeError:
             return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
         return JsonResponse({'message': 'SUCCESS'}, status=200)
+
+    def get(self, request):
+        try:
+            data      = request.body
+            json_data = json.loads(data)
+
+        except:
+            pass 
