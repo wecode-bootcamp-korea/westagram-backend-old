66264138749a3dd8de93c69f5ad5c6150e00b799
taekhyang <xorgid1195@gmail.com>
2021-03-04 15:18:30 +0900

    ADD : added debugger for the project

--- students/18th/team1/taekhyang/account/views.py
+++ students/18th/team1/taekhyang/account/views.py
@@ -1,9 +1,10 @@
 import json
 import re
 
-from django.views import View
-from django.http  import JsonResponse
-from .models      import User
+from django.views    import View
+from django.http     import JsonResponse
+from .models         import User
+from utils.debugger  import debugger
 
 
 class SignUpView(View):
@@ -14,31 +15,37 @@ class SignUpView(View):
         try:
             email    = json_data['email']
             password = json_data['password']
-        except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+            
+            # check empty string
+            if not email or not password:
+                return JsonResponse({'message': 'EMPTY_VALUE'}, status=400)
 
-        # check empty string
-        if not email or not password:
-            return JsonResponse({'message': 'EMPTY_VALUE'}, status=400)
+            p_email    = re.compile(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z-.]+$')
+            p_password = re.compile(r'.{8,45}')
 
-        p_email    = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9_]+\.[a-zA-Z-.]+$')
-        p_password = re.compile(r'.{8,45}')
+            # TODO : modify password validation check
+            is_valid_email    = True if p_email.match(email) else False 
+            is_valid_password = True if p_password.match(password) else False
 
-        # TODO : modify password validation check
-        is_valid_email    = True if p_email.match(email) else False 
-        is_valid_password = True if p_password.match(password) else False
+            if not is_valid_email or not is_valid_password:
+                return JsonResponse({'message': 'INVALID_FORMAT'}, status=400)
 
-        if not is_valid_email or not is_valid_password:
-            return JsonResponse({'message': 'INVALID_FORMAT'}, status=400)
+            # check existing user
+            is_existing_user = User.objects.filter(email=email).exists()
+            if is_existing_user:
+                return JsonResponse({'message': 'EXISTING_USER'}, status=400)
 
-        # check existing user
-        is_existing_user = User.objects.filter(email=email).exists()
-        if is_existing_user:
-            return JsonResponse({'message': 'EXISTING_USER'}, status=400)
-
-        # not sure if it may throw unexpected error when inserting data
-        try:
             User.objects.create(email=email, password=password)
+
+        except KeyError:
+            debugger.exception('KeyError')
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
         except:
+            debugger.exception()
             return JsonResponse({'message': 'INVALID_FORMAT'}, status=400)
         return JsonResponse({'message': 'SUCCESS'}, status=200)
+
+
+class LoginView(View):
+    def post(self, request):
+        pass
---
+++ students/18th/team1/taekhyang/utils/debugger.py
@@ -0,0 +1,35 @@
+import logging
+import datetime
+import os
+
+from logging.handlers import RotatingFileHandler
+
+
+debugger = logging.getLogger("Debugger")
+
+# to make directory for log files
+try:
+    now = str(datetime.datetime.now().date().replace('-', ''))
+    if not os.path.isdir('./logs'):
+        os.mkdir('./logs')
+    os.mkdir('./logs/log-{}'.format(now))
+
+    f_hdlr = RotatingFileHandler('./logs/log-{}/Debugger.log'.format(now),
+                                encoding='UTF-8',
+                                maxBytes=10 * 1024 * 1024,
+                                backupCount=3)
+except Exception as e:
+    f_hdlr = RotatingFileHandler('Debugger.log',
+                                encoding='UTF-8',
+                                maxBytes=10 * 1024 * 1024,
+                                backupCount=3)
+
+s_hdlr = logging.StreamHandler()
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+s_hdlr.setFormatter(formatter)
+f_hdlr.setFormatter(formatter)
+
+debugger.addHandler(f_hdlr)
+debugger.addHandler(s_hdlr)
+debugger.setLevel(logging.DEBUG)
