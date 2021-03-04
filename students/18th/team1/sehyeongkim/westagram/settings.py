f08183416bdcc8579521632b5b141fe51b0f6643
sehyeong <sehyeong@sehyeongui-MacBookAir.local>
2021-03-04 10:59:38 +0900

    settings.py database 수정

--- students/18th/team1/sehyeongkim/westagram/settings.py
+++ students/18th/team1/sehyeongkim/westagram/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from my_settings import SECRET_KEY
+from my_settings import SECRET_KEY, DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +75,7 @@ WSGI_APPLICATION = 'westagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASES 
 
 
 # Password validation
