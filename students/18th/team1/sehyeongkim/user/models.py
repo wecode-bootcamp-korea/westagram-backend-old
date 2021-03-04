1739315c1370bd814e18b7ed075212657b98105a
sehyeong <sehyeong@sehyeongui-MacBookAir.local>
2021-03-04 14:06:19 +0900

    user app 생성 후 models.py 작성

--- students/18th/team1/sehyeongkim/user/models.py
+++ students/18th/team1/sehyeongkim/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length= 130)
+    password = models.CharField(max_length= 40)
+
+    class Meta:
+        db_table = 'users'
+
