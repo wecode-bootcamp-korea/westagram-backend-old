d5f225b1f677514d0f4d61eb0f9d6c65c300c0c4
hlrrr <twfvtn@gmail.com>
2021-03-04 18:14:02 +0900

    modified: settings.databse

--- students/18th/team2/HoYeol/westagram/settings.py
+++ students/18th/team2/HoYeol/westagram/settings.py
@@ -77,12 +77,7 @@ WSGI_APPLICATION = 'westagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASES
 
 
 # Password validation
