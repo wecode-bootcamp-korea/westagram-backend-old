77798da5a2c88836de30e7a60bd67ab631a4bc8d
damin0320 <damin0320@gmail.com>
2021-03-04 15:03:57 +0900

    mission2 2nd

--- students/18th/team1/daminan/account/models.py
+++ students/18th/team1/daminan/account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email    = models.EmailField(max_length=50)
+    password = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = "users"
