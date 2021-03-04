59687b9af0769b84686bd10a28ee2ba1f4f136bc
damin0320 <damin0320@gmail.com>
2021-03-04 18:42:18 +0900

    mission3 2nd models.py완성

--- students/18th/team1/daminan/account/models.py
+++ students/18th/team1/daminan/account/models.py
@@ -6,3 +6,8 @@ class User(models.Model):
     
     class Meta:
         db_table = "users"
+        
+
+class Login(models.Model):
+    email    = models.EmailField(max_length=50)
+    password = models.CharField(max_length=100)
