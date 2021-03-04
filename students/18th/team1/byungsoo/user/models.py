6523f9aa378762d31e793870301bce174cf69411
ByungSoo Ye <ybs680318@gmail.com>
2021-03-04 15:48:37 +0900

    Update: 테이블명 user -> users

--- students/18th/team1/byungsoo/user/models.py
+++ students/18th/team1/byungsoo/user/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class User(models.Model):
     email    = models.CharField(max_length=45)
     password = models.CharField(max_length=30)
-
+    
     class Meta:
-        db_table = "user"
+        db_table = "users"
