e699ea4c8298495e131036f86a0ad8df2c438433
Seung Hyun Son <james@gracefulrain.co>
2021-03-04 15:35:28 +0900

    Delete students/17th/team1/donggeunLEE directory

--- students/17th/team1/donggeunLEE/User/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views      import UserSignUpView, UserlonginView, FollowView
-
-urlpatterns = [
-        path('/user', UserSignUpView.as_view()),
-        path('/login', UserlonginView.as_view()),
-        path('/follow', FollowView.as_view())
-    ]
--- students/17th/team1/donggeunLEE/westagram/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-        path('user', include('User.urls')),
-        path('user', include('Posting.urls')),
-    ]
