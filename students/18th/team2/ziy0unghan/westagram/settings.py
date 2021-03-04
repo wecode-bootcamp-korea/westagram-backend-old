dd1d68070ddfb819fc323645bfcf9393bd88e6aa
MacBook Pro <macbookpro@ziy0ung.local>
2021-03-04 17:15:32 +0900

    Mission 1: 초기세팅 완료

--- students/18th/team2/ziy0unghan/westagram/settings.py
+++ students/18th/team2/ziy0unghan/westagram/settings.py
@@ -31,20 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'westagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ziy0unghan',
+        'USER': 'root',
+        'PASSWORD': 'Hjy9800!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
