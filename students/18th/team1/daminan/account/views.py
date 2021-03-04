558fab4b460f2931917c6b0b28ed685687df568e
damin0320 <damin0320@gmail.com>
2021-03-05 00:05:05 +0900

    change : mission2 views.py 내용 전반적 수정

--- students/18th/team1/daminan/account/views.py
+++ students/18th/team1/daminan/account/views.py
@@ -8,17 +8,19 @@ from .models import User
 class SignupView(View):
     def post(self, request):
         data = json.loads(request.body)
-        if User.objects.filter(email=data['email']).exists():
-            return JsonResponse({"message": "EMAIL_ERROR"}, status=400)
-        if User.objects.filter(password=data['password']).exists():
-            return JsonResponse({"message": "PASSOWRD_ERROR"}, status=400)
-        if '@' in data['email'] and '.' in data['email'] and len(data['password']) >= 8:
-            user     = User.objects.create(
-            email    = data['email'],
-            password = data['password']
-        )
-            return JsonResponse({"message": "SUCCESS"}, status=200)
-        else:
+        try:
+            user = User.objects.filter(email=data['email'],password=data['password'])
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({"message": "EMAIL_ERROR"}, status=400)
+            if User.objects.filter(password=data['password']).exists():
+                return JsonResponse({"message": "PASSOWRD_ERROR"}, status=400)
+            if '@' in data['email'] and '.' in data['email'] and len(data['password']) >= 8:
+                user     = User.objects.create(
+                email    = data['email'],
+                password = data['password']
+            )
+                return JsonResponse({"message": "SUCCESS"}, status=200)
+        except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
    
 
