1bbeefb5e2f29e255ba699cdf2dba4abf99a20e4
damin0320 <damin0320@gmail.com>
2021-03-04 17:32:24 +0900

    mission2 3rd view완성

--- students/18th/team1/daminan/account/views.py
+++ students/18th/team1/daminan/account/views.py
@@ -9,6 +9,10 @@ from .models import User
 class UserView(View):
     def post(self, request):
         data = json.loads(request.body)
+        if User.objects.filter(email=data['email']).exists():
+            return JsonResponse({"message": "EMAIL_ERROR"}, status=400)
+        if User.objects.filter(password=data['password']).exists():
+            return JsonResponse({"message": "PASSOWRD_ERROR"}, status=400)
 
         if '@' in data['email'] and '.' in data['email'] and len(data['password']) >= 8:
             user     = User.objects.create(
@@ -17,9 +21,6 @@ class UserView(View):
         )
             return JsonResponse({"message": "SUCCESS"}, status=200)
         else:
-            return JsonResponse({"message": "KEY_ERROR"}, status=200)
-        
-        
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+   
 
-        
-        
