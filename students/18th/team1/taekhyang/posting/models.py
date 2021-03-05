e02da4402eb8267f816816945b5753b1608c1ef0
taekhyang <xorgid1195@gmail.com>
2021-03-05 16:29:52 +0900

    ADD : added Comment model in posting.models with migration being executed

--- students/18th/team1/taekhyang/posting/models.py
+++ students/18th/team1/taekhyang/posting/models.py
@@ -16,3 +16,14 @@ class PostingImage(models.Model):
 
     class Meta:
         db_table = 'posting_images'
+
+
+class Comment(models.Model):
+    posting      = models.ForeignKey('Posting', on_delete=models.CASCADE)
+    user         = models.ForeignKey('account.User', on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    content      = models.CharField(max_length=2000)
+
+    class Meta:
+        db_table = 'comments'
+        
