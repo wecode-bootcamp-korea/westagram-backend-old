b7e7ff1612b2b7856007dcf24ea3fe278989ca6b
damin0320 <damin0320@gmail.com>
2021-03-05 13:24:45 +0900

    mission2 models.py 이메일유니크

--- students/18th/team1/daminan/account/models.py
+++ students/18th/team1/daminan/account/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    email    = models.EmailField(max_length=50)
+    email    = models.EmailField(max_length=50, unique=True)
     password = models.CharField(max_length=100)
     
     class Meta:
