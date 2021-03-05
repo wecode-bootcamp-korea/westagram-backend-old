63e0a381a272d8b8f34ec6fef72e2ed49ecc6e93
Won <jungwon1423@gamil.com>
2021-03-05 16:44:33 +0900

    Add: 게시물 댓글 표출

--- students/18th/team1/hurjungwon/posting/views.py
+++ students/18th/team1/hurjungwon/posting/views.py
@@ -54,7 +54,7 @@ class CommentView(View):
     def post(self, request):
         
         if not request.body:
-                return JsonResponse({'message': 'Empty Value'}, status=400)
+            return JsonResponse({'message': 'Empty Value'}, status=400)
         
         data = json.loads(request.body)
 
@@ -72,3 +72,36 @@ class CommentView(View):
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
+    def get(self, request):
+
+        if not request.body:
+            return JsonResponse({'message': 'Type Post_ID'}, status=400)
+        
+        data = json.loads(request.body)
+
+        try :
+            post_id = data['post_id']
+            
+            if not Post.objects.filter(id=post_id):
+                return JsonResponse({'massage': 'Invalid Post_ID'}, status=400)
+
+            comments = Comment.objects.filter(post=post_id)
+            
+            if not comments:
+                return JsonResponse({'massage': 'No Comments'}, status=200)
+            
+            result = []
+
+            for comment in comments:
+                comment_dict = {
+                    'user_name'  : comment.user_name.user_name,
+                    'content'    : comment.content,
+                    'create_date': comment.create_date,
+                }
+                result.append(comment_dict)
+
+            return JsonResponse({'result': result}, status=200)
+            
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        
