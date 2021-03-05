bbde874594c56385ec1470e1b536f8f7e3268745
damin0320 <damin0320@gmail.com>
2021-03-05 16:24:43 +0900

    mission4 3rd views.py

---
+++ students/18th/team1/daminan/posting/views.py
@@ -0,0 +1,17 @@
+import json
+
+from django.views import View
+from django.http  import JsonResponse
+
+from .models import Posting
+
+class PostingView(View):
+    def post(self, request):
+        data    = json.loads(request.body)
+        user    = Posting.objects.get(email=data['email'])
+        posting = Posting.objects.create(
+            time    = data["time"],
+            img_url = data["img_url"],
+            user    = user
+        )
+    return JsonResponse({"message" : "SUCCESS"}, status=200)
