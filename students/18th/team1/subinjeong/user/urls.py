a7cea0ed4e602e9e527f377175bc3515a87fe931
jadyJeong <coldfeet0816@gmail.com>
2021-03-05 20:00:00 +0900

    Update: urls properly configured for Views

--- students/18th/team1/subinjeong/user/urls.py
+++ students/18th/team1/subinjeong/user/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import SignupView
+from .views import SignupView, LoginView
 
 urlpatterns = [
     path('/signup',SignupView.as_view()),
-    path('/login',SignupView.as_view())
+    path('/login',LoginView.as_view())
 ]
