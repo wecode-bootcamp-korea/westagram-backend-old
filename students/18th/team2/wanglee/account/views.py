bcf5aa596f25581176b6c98171b3ea8c46a181ae
WANG LEE <wanglee@WANGs-MacBook-Pro.local>
2021-03-05 16:29:45 +0900

    Using objects.filter()

--- students/18th/team2/wanglee/account/views.py
+++ students/18th/team2/wanglee/account/views.py
@@ -5,12 +5,12 @@ from .models      import User
 
 class UserSignup(View):
     def post(self, request):
-        users    = User.objects.all()
         data     = json.loads(request.body)
         email    = data['email']
         password = data['password']
         username = data['username']
         phone    = data['phone']
+        temps = User.objects.filter(username=username) | User.objects.filter(email=email) | User.objects.filter(phone=phone)
 
         if email == '' or password == '':
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
@@ -20,17 +20,19 @@ class UserSignup(View):
         
         elif len(password) < 8:
             return JsonResponse({"message":"password must be at least 8 characters"}, status=400)
-        else:
-            for user in users:
-                if username == user.username:
-                    return JsonResponse({"message":"That username is taken. Try another"}, status=400)
-                elif email  == user.email:
-                    return JsonResponse({"message":"That email is taken. Try another"}, status=400)
-                elif phone  == user.phone:
-                    return JsonResponse({"message":"That phone number is taken. Try another"}, status=400)
+        
+        for temp in temps:
+            if username == temp.username:
+                return JsonResponse({"message":"That username is taken. Try another"}, status=400)
+        
+            elif email == temp.email:
+                return JsonResponse({"message":"That email is taken. Try another"}, status=400)
+        
+            elif phone == temp.phone:
+                return JsonResponse({"message":"That phone is taken. Try another"}, status=400)
 
-            User.objects.create(username=username, email=email, password=password, phone=phone)
-            return JsonResponse({'result': 'SUCCESS'}, status=200)
+        User.objects.create(username=username, email=email, password=password, phone=phone)
+        return JsonResponse({'result': 'SUCCESS'}, status=200)
         
 class UserSignin(View):
     def post(self, request):
@@ -40,18 +42,17 @@ class UserSignin(View):
         password = data['password']
         username = data['username']
         phone    = data['phone']
+        temps = User.objects.filter(username=username) | User.objects.filter(email=email) | User.objects.filter(phone=phone) | User.objects.filter(password=password)
 
         if password == '':
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
 
         elif email == '' and username == '' and phone == '':
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
-        
+            
         else:
-            for user in users:
-                if username == user.username or email == user.email or phone == user.phone:
-                    if password == user.password:
+            for temp in temps:
+                if username == temp.username or email == temp.email or phone == temp.phone:
+                    if password == temp.password:
                         return JsonResponse({'result': 'SUCCESS'}, status=200)
             return JsonResponse({"message":"INVALID_USER"}, status=401)
-
-                
