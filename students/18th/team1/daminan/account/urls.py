d9fa326bb79c950faca33a224f2cccdeaa3ae8c8
damin0320 <damin0320@gmail.com>
2021-03-05 13:43:59 +0900

    mission2 urls.py / 수정

--- students/18th/team1/daminan/account/urls.py
+++ students/18th/team1/daminan/account/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from .views      import SignupView, LoginView
 
 urlpatterns = [
-    path('user', SignupView.as_view()),
-    path('login', LoginView.as_view()),    
+    path('/user', SignupView.as_view()),
+    path('/login', LoginView.as_view()),    
 ]
-'''urls.py 슬래시 위치 확인
-(urls.w002) your url pattern '/user' has a route beginning with a '/'. remove this slash as it is unnecessary. if this pattern is targeted in an include(), ensure the include() pattern has a trailing '/'.
-에러 나서 / 사용 위와 같이 함.'''
+'''Error가 아니라 django의 권장사항. 하지만 패턴 컨벤션에는 위 사항이 맞다.'''
