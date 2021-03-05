fbfdda17e56fd6cab12a16ea0ab5ee999017f992
MacBook Pro <macbookpro@ziy0ung.local>
2021-03-05 20:00:24 +0900

    ADD: models, views

---
+++ students/18th/team2/ziy0unghan/user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username= models.CharField(max_length=45, unique = True)
+    email = models.EmailField(max_length=100, unique = True)
+    password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=45, unique=True)
+
+    class Meta:
+        db_table='users'
--- students/18th/team2/ziy0unghan/user/views.py
+++ students/18th/team2/ziy0unghan/user/views.py
@@ -1,2 +1,35 @@
+import json
 
+from django.http  import JsonResponse
+from django.views import View
+
+from .models import User
+
+class SignUpView(View):
+    def post(self, request):
+
+            #이메일 패스워드 확인
+            data = json.loads(request.body)
+
+            username     = data['username']
+            email        = data['email']
+            password     = data['password']
+            phone_number = data['phone_number']
+
+            if username == ''or email == '' or password == '' or phone_number == '':
+                return JsonResponse({'message': 'KEY-ERROR' }, status=400)
+            elif '@' not in email or '.' not in email:
+                return JsonResponse({'message': '잘못된 형식입니다.'}, status=400)
+            elif len(password) < 8:
+                return JsonResponse({'message': '비밀번호 8자리 이상 입력해 주세요.'}, status=400)
+            elif data in User.objects.filter(email=email) or User.objects.filter(password=password):
+                return JsonResponse({'message' : '이미 가입된 계정입니다.'}, status=401)
+            else:
+                User.objects.create(username=username, email=email, password=password, phone_number=phone_number)
+                return JsonResponse({'message': '회원가입 되었습니다.'}, status=200)
+
+ 
+
+        
+  
 
