8530aab2511f4c818da8ed56f8f09056b959e997
gyudong <zlsrbrhd@gmail.com>
2021-03-05 11:02:35 +0900

    FIX:DATABASES 변경, my_setting.py로 이동후 import방식으로 변경

--- students/18th/team2/kimgyudong/user/models.py
+++ students/18th/team2/kimgyudong/user/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email    = EmailField(max_length=50)
+    password = CharField(max_length=30)
+
--- students/18th/team2/kimgyudong/user/views.py
+++ students/18th/team2/kimgyudong/user/views.py
@@ -1,1 +1,24 @@
+import json
+
+from django.views import View
+from django.http  import JsonResponse
+
+from .models import User
+
+class UserSignUp(View):
+    def post(self,request):
+        data = json.loads(request.body)
+        
+        if "email" not in data or "password" not in data:
+            return JsonResponse({"message":"KEY_ERROR"}, status = 400)
+
+        email    = data["email"]
+        password = data["password"]
+
+        
+        
+        if len(password) < 8:
+            pass
+
+
 
