ce2500304376040b3ebbd8280eeaf1676848b7eb
parkgood1020 <parkgood1020@gmail.com>
2021-03-05 18:53:20 +0900

    MODIFY : model.py unique option

--- students/18th/team1/kookhyunpark/user/models.py
+++ students/18th/team1/kookhyunpark/user/models.py
@@ -1,11 +1,10 @@
 from django.db              import models
 
-# Create your models here.
 class User(models.Model):
     email         = models.EmailField(max_length=50)
-    phone         = models.CharField(max_length=11, null=True)
+    phone         = models.CharField(max_length=11, null=True, unique=True)
     full_name     = models.CharField(max_length=40, null=True)
-    user_name     = models.CharField(max_length=20, null=True)
+    user_name     = models.CharField(max_length=20, null=True, unique=True)
     password      = models.CharField(max_length=20)
     date_of_birth = models.DateField(null=True)
 
--- students/18th/team1/kookhyunpark/user/views.py
+++ students/18th/team1/kookhyunpark/user/views.py
@@ -1,8 +1,9 @@
 import json
 import re
 
-from django.views import View
-from django.http  import JsonResponse, request
+from django.views     import View
+from django.db.models import Q
+from django.http      import JsonResponse, request
 
 from user.models import User
 
@@ -12,10 +13,10 @@ class SignUpView(View):
             data = json.loads(request.body)
 
             email         = data['email']
+            password      = data['password']
             phone         = data.get('phone', None)
             full_name     = data.get('full_name', None)
             user_name     = data.get('user_name', None)
-            password      = data['password']
             date_of_birth = data.get('date_of_birth', None)
 
             if phone:
@@ -30,7 +31,7 @@ class SignUpView(View):
                 if phone and User.objects.filter(phone=phone):
                     return JsonResponse({'message':'PHONE ALREADY EXISTS'}, status=400)
                 elif user_name and User.objects.filter(user_name=user_name):
-                    return JsonResponse({'message':'USER_NAME ALREADY EXISTS'}, status=400)
+                    return JsonResponse({'message':'USER_NAME ALREADY EXISTS'}, status=400))
             else:
                 return JsonResponse({'message':'EMAIL ALREADY EXISTS'}, status=400)
 
@@ -42,28 +43,29 @@ class SignUpView(View):
                 password      = password,
                 date_of_birth = date_of_birth,
             )
-            return JsonResponse({'message':'SUCCESS'}, status=200)            
+
+            return JsonResponse({'message':'SUCCESS'}, status=200)     
+
         except KeyError:
             return JsonResponse({'message':'KEY ERROR'}, status=400)
-        except:
-            return JsonResponse({"message":"RESPONSE ERROR"}, status=400)
+        except Exception as e:
+            return JsonResponse({"message":"RESPONSE ERROR"+e}, status=400)
 
 class LoginView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
 
-            user_name = data['user_name']
+            user_id = data['user_id']
             password  = data['password']
 
-            if not User.objects.get(user_name=user_name) and not User.objects.get(email=user_name) and not User.objects.get(phone=user_name):
-                return JsonResponse({"message":"INVALID_USER"}, status=401)
+            if User.objects.filter(user_name=user_id) or User.objects.filter(email=user_id) or User.objects.filter(phone=user_id):
+                if not User.objects.filter(password=password):
+                    return JsonResponse({"message":"INVALID_USER"}, status=401)                
             else:
-                if not User.objects.get(password=password):
-                    return JsonResponse({"message":"INVALID_USER"}, status=401)
+                return JsonResponse({"message":"INVALID_USER"}, status=401)
 
             return JsonResponse({"message":"SUCCESS"}, status=200)
-
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
         except:
