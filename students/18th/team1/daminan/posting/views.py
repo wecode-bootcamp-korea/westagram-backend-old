60b070445652b428456e97fc64060dcff90f03a5
damin0320 <damin0320@gmail.com>
2021-03-05 17:28:33 +0900

    mission4 3rd post

--- students/18th/team1/daminan/posting/views.py
+++ students/18th/team1/daminan/posting/views.py
@@ -3,15 +3,17 @@ import json
 from django.views import View
 from django.http  import JsonResponse
 
-from .models import Posting
+from .models        import Posting
+from account.models import User
 
 class PostingView(View):
     def post(self, request):
+        #딕셔너리 모양으로 받아들여진 정보(httpie에 입력한 정보)
         data    = json.loads(request.body)
-        user    = Posting.objects.get(email=data['email'])
+        user    = User.objects.get(email=data['email'])
         posting = Posting.objects.create(
-            time    = data["time"],
             img_url = data["img_url"],
             user    = user
+            #Time은 알아서 import, 입력 필요 없음.
         )
         return JsonResponse({"message" : "SUCCESS"}, status=200)
