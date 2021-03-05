5027a50720a5418b18a6ed0c90c6d814342ec8c0
MacBook Pro <macbookpro@ziy0ung.local>
2021-03-05 20:13:05 +0900

    UPDATE : wecode convention

--- students/18th/team2/ziy0unghan/user/models.py
+++ students/18th/team2/ziy0unghan/user/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username= models.CharField(max_length=45, unique = True)
-    email = models.EmailField(max_length=100, unique = True)
-    password = models.CharField(max_length=100)
+    username     = models.CharField(max_length=45, unique = True)
+    email        = models.EmailField(max_length=100, unique = True)
+    password     = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=45, unique=True)
 
     class Meta:
