cd98bb59bca051daf4aca524c96706ab10587e2f
jadyJeong <coldfeet0816@gmail.com>
2021-03-05 15:38:33 +0900

    Add: table class User created in models.py

--- students/18th/team1/subinjeong/user/models.py
+++ students/18th/team1/subinjeong/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+
+    username  = models.CharField(max_length=45)
+    email     = models.CharField(max_length=45)
+    password  = models.CharField(max_length=45)
+
+    class Meta(object):
+        db_table = "usernames"  
+        db_table = "emails"
+        db_table = "passwords"
+
+
+        
+        
+        
+
+        
+
