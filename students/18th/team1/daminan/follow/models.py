14ae660669dddb42419d4a7c064db883d4179c6c
damin0320 <damin0320@gmail.com>
2021-03-07 17:31:55 +0900

    mission7 modeling중

--- students/18th/team1/daminan/follow/models.py
+++ students/18th/team1/daminan/follow/models.py
@@ -5,5 +5,7 @@ from account.models import User
 
 class Follow(models.Model):
     follower = models.ManyToManyField(User, related_name="following") 
-    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    user     = models.ForeignKey(User, on_delete=models.CASCADE)
     
+    class Meta:
+        db_table = "follows"
