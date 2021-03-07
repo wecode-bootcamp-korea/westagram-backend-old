908d4326086572f7e5e64e4a1de20cbac3191cd3
taekhyang <xorgid1195@gmail.com>
2021-03-07 15:50:01 +0900

    ADD : added username, phone_number in User model

--- students/18th/team1/taekhyang/account/models.py
+++ students/18th/team1/taekhyang/account/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    email    = models.EmailField(max_length=255, unique=True)
-    password = models.CharField(max_length=50)
+    username     = models.CharField(max_length=50, unique=True)
+    email        = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=50, unique=True, null=True)
+    password     = models.CharField(max_length=50)
     
     class Meta:
         db_table = 'users'
