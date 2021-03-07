8b9d4bad28317c91e713e8332ac43a89b41af1d7
taekhyang <xorgid1195@gmail.com>
2021-03-07 15:46:36 +0900

    MODIFY : added unique option for email field in User model

--- students/18th/team1/taekhyang/account/models.py
+++ students/18th/team1/taekhyang/account/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    email    = models.EmailField(max_length=255)
+    email    = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=50)
     
     class Meta:
