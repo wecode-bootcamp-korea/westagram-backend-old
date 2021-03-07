f6f74e640e6f96250b1f42305873518510e0fd43
taekhyang <xorgid1195@gmail.com>
2021-03-07 15:26:10 +0900

    MODIFY : changed max_length for email field in User model

--- students/18th/team1/taekhyang/account/models.py
+++ students/18th/team1/taekhyang/account/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    email    = models.EmailField(max_length=256)
+    email    = models.EmailField(max_length=255)
     password = models.CharField(max_length=50)
     
     class Meta:
