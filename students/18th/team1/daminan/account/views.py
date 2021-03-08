a6f0c8f0cd21290e6b4323687a6d34fdd9246b64
damin0320 <damin0320@gmail.com>
2021-03-08 22:18:10 +0900

    account view 수정

--- students/18th/team1/daminan/account/views.py
+++ students/18th/team1/daminan/account/views.py
@@ -38,7 +38,8 @@ class LoginView(View):
             if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
                  
                 token = jwt.encode({'email' : data['email']}, SECRET_KEY, algorithm="HS256")
-                
+                #라이브러리에서 만들어줌. 이메일 부분 암호화 클라이언트에게 보내줌.(json body에 담아서 보냄)
+                #프->백 헤더에 토큰을 담아서 보내줘야 함.
                 return JsonResponse({"message":"SUCCESS"}, status=200)
             else:
                 return JsonResponse({"message":"INVALID_USER"}, status=401)
