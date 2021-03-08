57d0660d3dd432a76c07ea18f5329b9a9a633476
jadyJeong <coldfeet0816@gmail.com>
2021-03-08 17:23:23 +0900

    Add: posting.models created.

--- students/18th/team1/subinjeong/posting/models.py
+++ students/18th/team1/subinjeong/posting/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 
-from user import models
-
+from user.models import User
 
 class Posting(models.Model):
 
-    username     = models.ForeignKey('User',on_delete=models.CASCADE)
-    time_dreated = models.CharField(max_length=45)
-    img_url      = models.CharField(max_length=45)
+    username     = models.ForeignKey('user.User',on_delete=models.CASCADE)
+    title        = models.CharField(max_length=50)
+    img_url      = models.CharField(max_length=1000)
+    time_created = models.DateTimeField(auto_now_add=True)    
 
     class Meta(object):
         db_table = "postings"
-
-
-        
