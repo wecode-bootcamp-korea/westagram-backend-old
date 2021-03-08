5bba3762b7cdbb157f864b1c415fba1af57edfd4
damin0320 <damin0320@gmail.com>
2021-03-08 19:13:53 +0900

    mission2~3 인증인가 적용

--- students/18th/team1/daminan/account/views.py
+++ students/18th/team1/daminan/account/views.py
@@ -1,9 +1,12 @@
 import json
+import bcrypt
+import jwt
 
 from django.views import View
 from django.http  import JsonResponse
 
-from .models import User
+from .models     import User
+from my_settings import SECRET_KEY
 
 class SignupView(View):
     def post(self, request):
@@ -14,10 +17,13 @@ class SignupView(View):
             if User.objects.filter(password=data['password']).exists():
                 return JsonResponse({"message": "PASSOWRD_ERROR"}, status=400)
             if '@' in data['email'] and '.' in data['email'] and len(data['password']) >= 8:
+                byted_password = data['password'].encode('utf-8')
+                hash_password = bcrypt.hashpw(byted_password, bcrypt.gensalt())
+                password = hash_password.decode('utf-8')
                 user     = User.objects.create(
                 email    = data['email'],
-                password = data['password']
-            )
+                password = password
+            ).save()
                 return JsonResponse({"message": "SUCCESS"}, status=200)
             return JsonResponse({"message":"MAKE_FAIL"}, status=400)
         except KeyError:
@@ -28,7 +34,14 @@ class LoginView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            if User.objects.all().filter(email=data['email'], password=data['password']).exists() == True:
+            if User.objects.filter(email=data['email']).exists():
+                user = User.objects.get(email=data['email'])
+                if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                 
+                    token = jwt.encode({'email' : data['email']}, SECRET_KEY, algorithm="HS256")
+                    #token = token.decode('utf-8')
+                    # h5yp 버전 문제로 디코드 안 해도 됨.
+                
                 return JsonResponse({"message":"SUCCESS"}, status=200)
             else:
                 return JsonResponse({"message":"INVALID_USER"}, status=401)
