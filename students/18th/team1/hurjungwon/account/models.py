edc0a39a9c0ef12088bd73fffff4cf45db7ced2f
Won <jungwon1423@gamil.com>
2021-03-08 09:37:07 +0900

    Add: follow 테이블 구축

--- students/18th/team1/hurjungwon/account/models.py
+++ students/18th/team1/hurjungwon/account/models.py
@@ -7,8 +7,14 @@ class User(models.Model):
     phone_number = models.CharField(max_length=25, null=True, unique=True)
     email        = models.EmailField(max_length=245, unique=True)
     password     = models.CharField(max_length=300)
+    follow       = models.ManyToManyField('self', through='Follow')
 
     class Meta:
         db_table = 'users'
 
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+    followee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followee')
 
+    class Meta:
+        db_table = 'follows'
