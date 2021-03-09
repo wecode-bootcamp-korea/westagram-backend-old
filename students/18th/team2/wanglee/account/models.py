a4c174efaf862fdd8fb2a3be2f7697d758990188
WANG LEE <wanglee@WANGs-MacBook-Pro.local>
2021-03-09 21:00:31 +0900

    key error exception on singin

--- students/18th/team2/wanglee/account/models.py
+++ students/18th/team2/wanglee/account/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    email     = models.EmailField(max_length=30, unique = True)
+    email     = models.EmailField(max_length=50, unique = True)
     password  = models.CharField(max_length=500)
-    username  = models.CharField(max_length=20, unique = True)
-    phone_num = models.CharField(max_length=20, unique = True)
+    username  = models.CharField(max_length=50, unique = True)
+    phone_num = models.CharField(max_length=50, unique = True)
 
     class Meta:
         db_table  = 'users'
--- students/18th/team2/wanglee/account/views.py
+++ students/18th/team2/wanglee/account/views.py
@@ -10,11 +10,11 @@ from .models      import User
 class UserSignup(View):
     def post(self, request):
         try:
-            data     = json.loads(request.body)
-            email    = data['email']
-            password = data['password']
-            username = data['username']
-            phone_num    = data['phone_num']
+            data      = json.loads(request.body)
+            email     = data['email']
+            password  = data['password']
+            username  = data['username']
+            phone_num = data['phone_num']
 
             if ('@' and '.') not in email:
                 return JsonResponse({"message":"email must contain the '@' symbol and the period'.'"}, status=400)
@@ -43,23 +43,54 @@ class UserSignup(View):
         
 class UserSignin(View):
     def post(self, request):
-        users    = User.objects.all()
-        data     = json.loads(request.body)
-        email    = data['email']
-        password = data['password']
-        username = data['username']
-        phone_num    = data['phone_num']
-        temps = User.objects.filter(username=username) | User.objects.filter(email=email) | User.objects.filter(phone_num=phone_num) | User.objects.filter(password=password)
+        try:
+            users     = User.objects.all()
+            data      = json.loads(request.body)
+            email     = data['email']
+            password  = data['password']
+            username  = data['username']
+            phone_num = data['phone_num']
+        except KeyError:
+            print("==========================")
+            print(email, password)
+            print("==========================")
+            return JsonResponse({"message":"KEY_ERROR"}, status=400)
 
-        if password == '':
+        except ValueError:
+            return JsonResponse({"message":"VAlue_ERROR"}, status=400)
+        
+        except User.ObjectDoesNotExist:
+            return JsonResponse({"message":"INVALID_USER"}, status=401) 
+
+
+        # temps = User.objects.filter(username=username) | User.objects.filter(email=email) | User.objects.filter(phone_num=phone_num) | User.objects.filter(password=password)
+
+        if not password or not email:
+            print("**************************")
+            print(email, password)
+            print("**************************")
+
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
 
         elif email == '' and username == '' and phone_num == '':
+            print("--------------------------")
+            print(email, password)
+            print("--------------------------")
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
-            
-        else:
-            for temp in temps:
-                if username == temp.username or email == temp.email or phone_num == temp.phone_num:
-                    if password == temp.password:
-                        return JsonResponse({'result': 'SUCCESS'}, status=200)
-            return JsonResponse({"message":"INVALID_USER"}, status=401)
+        
+        user=User.objects.filter(Q(username=username) | Q(email=email) | Q(phone_num=phone_num))
+        print(user)
+        user=user.get()
+        print("8888888888888888888888888888888")
+        print(user)
+        print("8888888888888888888888888888888")
+        if bcrypt.checkpw(password.encode('utf-8'), user.password):
+            return JsonResponse({'result': 'SUCCESS'}, status=200)
+        # else:
+        #     for temp in temps:
+        #         if username == temp.username or email == temp.email or phone_num == temp.phone_num:
+        #             if password == temp.password:
+        #                 return JsonResponse({'result': 'SUCCESS'}, status=200)
+        #     return JsonResponse({"message":"INVALID_USER"}, status=401) 
+
+
