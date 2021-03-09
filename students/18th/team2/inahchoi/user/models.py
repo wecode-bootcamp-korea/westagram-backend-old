b38a88cdcea8507b90af3d8e7721910640e4481a
InahChoi <hyejooworld@gmail.com>
2021-03-09 14:22:49 +0900

    models, views, app.urls, project.urls 추가

--- students/18th/team2/inahchoi/user/models.py
+++ students/18th/team2/inahchoi/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email    = models.EmailField(max_length=100, unique=True, default='')
+    password = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'users'
--- students/18th/team2/inahchoi/user/views.py
+++ students/18th/team2/inahchoi/user/views.py
@@ -1,1 +1,31 @@
+import json, re
+from django.http    import JsonResponse
 
+from django.views   import View
+from .models        import User
+
+
+class SignupView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        email    = data['email']
+        password = data['password']
+
+        if not data['email']:
+            return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
+        elif not data['password']:
+            return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
+        
+        if User.objects.filter(email=email):
+            return JsonResponse({'message' : 'Already Exists'}, status = 400)
+
+        regex_email    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]+$'
+        regex_password = '\S{8,25}'
+        if not re.match(regex_email, email):
+            return JsonResponse({'message' : 'Invalid Email'}, status = 400)
+        elif not re.match(regex_password, password):
+            return JsonResponse({'message' : 'Invalid Password'}, status = 400) 
+
+        User.objects.create(email=email, password=password)
+        return JsonResponse({'message': 'Signup Success!'}, status=200)
