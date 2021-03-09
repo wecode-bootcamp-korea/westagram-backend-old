4b7b7f52932e08df8c66c9147efbd485f5569ab8
damin0320 <damin0320@gmail.com>
2021-03-09 10:55:30 +0900

    리뷰:account/views.py 띄어쓰기

--- students/18th/team1/daminan/account/views.py
+++ students/18th/team1/daminan/account/views.py
@@ -16,6 +16,7 @@ class SignupView(View):
                 return JsonResponse({"message": "EMAIL_ERROR"}, status=400)
             if User.objects.filter(password=data['password']).exists():
                 return JsonResponse({"message": "PASSOWRD_ERROR"}, status=400)
+            
             if '@' in data['email'] and '.' in data['email'] and len(data['password']) >= 8:
                 byted_password = data['password'].encode('utf-8')
                 hash_password = bcrypt.hashpw(byted_password, bcrypt.gensalt())
