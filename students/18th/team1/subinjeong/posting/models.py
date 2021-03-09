8c998badd8aab614e51e4ac2e6e69925103de929
jadyJeong <coldfeet0816@gmail.com>
2021-03-09 14:39:51 +0900

    Update: renamed username -> user in posting.models.

--- students/18th/team1/subinjeong/posting/models.py
+++ students/18th/team1/subinjeong/posting/models.py
@@ -4,7 +4,7 @@ from user.models import User
 
 class Posting(models.Model):
 
-    username     = models.ForeignKey('user.User',on_delete=models.CASCADE)
+    user         = models.ForeignKey('user.User',on_delete=models.CASCADE)
     title        = models.CharField(max_length=50)
     img_url      = models.CharField(max_length=1000)
     time_created = models.DateTimeField(auto_now_add=True)    
