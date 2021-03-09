f55a1aa1ae48303f11495a03b28baf587610daf9
InahChoi <hyejooworld@gmail.com>
2021-03-10 01:12:19 +0900

    class SignupView 추가

--- students/18th/team2/inahchoi/user/urls.py
+++ students/18th/team2/inahchoi/user/urls.py
@@ -1,7 +1,8 @@
 # from django.conf import settings
 from django.urls import path
-from .views      import SignupView
+from .views      import SignUpView, SignInView
 
 urlpatterns = [
-    path('/signup', SignupView.as_view()),
+    path('/signup', SignUpView.as_view()),
+    path('/signin', SignInView.as_view())
 ]
--- students/18th/team2/inahchoi/user/views.py
+++ students/18th/team2/inahchoi/user/views.py
@@ -1,19 +1,17 @@
-import json
-import bcrypt
-import re
+import json, bcrypt, jwt, re
 
-from django.http    import JsonResponse
 from django.views   import View
+from django.http    import JsonResponse, HttpResponse
 
 from .models        import User
+from my_settings    import SECRET_KEY, ALGORITHM
 
-
-class SignupView(View):
+class SignUpView(View):
     def post(self, request):
         data = json.loads(request.body)
 
-        email    = data['email']
-        password = data['password']
+        email        = data['email']
+        password     = data['password']
 
         if not data['email']:
             return JsonResponse({'message': 'KEY_ERROR'}, status = 400)
@@ -26,13 +24,35 @@ class SignupView(View):
         regex_email    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]+$'
         regex_password = '\S{8,25}'
         if not re.match(regex_email, email):
-            return JsonResponse({'message' : 'Invalid Email'}, status = 400)
+            return JsonResponse({'message' : 'INVALID_EMAIL'}, status = 400)
         elif not re.match(regex_password, password):
-            return JsonResponse({'message' : 'Invalid Password'}, status = 400)
+            return JsonResponse({'message' : 'INVALID_PASSWORD'}, status = 400)
 
         password       = data['password'].encode('utf-8')
         password_crypt = bcrypt.hashpw(password, bcrypt.gensalt())
         password_crypt = password_crypt.decode('utf-8')
 
         User.objects.create(email=email, password=password_crypt)
-        return JsonResponse({'message': 'Signup Success!'}, status=200)
+        return JsonResponse({'message': 'SUCCESS!'}, status=200)
+
+
+class SignInView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        try:
+            if User.objects.get(email = data['email']):
+                user = User.objects.get(email = data['email'])
+
+            if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                token = jwt.encode({ 'password' : data['password'] }, SECRET_KEY, algorithm='HS256')
+            
+            else:
+                return JsonResponse({'message' : 'INVALID_USER'}, status=400)
+
+            return JsonResponse({'message' : 'WOW!!!!SUCCESS!!!!', 'access_token' : token.decode('utf-8')}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_USER'}, status=401)
