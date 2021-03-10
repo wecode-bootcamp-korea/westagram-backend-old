289bb7a2e133be380ce689e4da1c9977f628e75b
hlrrr <twfvtn@gmail.com>
2021-03-10 13:58:47 +0900

    mission2 review

--- students/18th/team2/HoYeol/user/models.py
+++ students/18th/team2/HoYeol/user/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
 
-    email   = models.CharField(max_length=50)
-    name    = models.CharField(max_length=50)
-    phone   = models.CharField(max_length=50)
-    pswd    = models.CharField(max_length=50)
+    email   = models.EmailField(max_length=50, unique=True)
+    name    = models.CharField(max_length=50, unique=True)
+    phone   = models.CharField(max_length=50, unique=True)
+    password= models.CharField(max_length=500)
 
     class Meta:
         db_table='users'
--- students/18th/team2/HoYeol/user/views.py
+++ students/18th/team2/HoYeol/user/views.py
@@ -1,72 +1,82 @@
 # from westagram.settings import AUTH_PASSWORD_VALIDATORS
 # from django.core.exceptions import ViewDoesNotExist
-import json
+import json, bcrypt, jwt
+from json.decoder import JSONDecodeError
 from django.db.models.expressions import Exists
-from django.views    import View
-from django.http     import JsonResponse
-from .models         import User
+from django.db.models import Q
+from django.views     import View
+from django.http      import JsonResponse
+from .models          import User
+from my_settings      import SECRET_KEY, ALGORITHM
+
 
 class SignUpView(View):
     def post(self, request):
-        
-        data  = json.loads(request.body)
-        name  = data['name']
-        email = data['email']
-        phone = data['phone']
-        pswd  = data['pswd']
-        
-        db_email = User.objects.filter(email=email).exists()
-        db_phone = User.objects.filter(phone=phone).exists()
-        db_name  = User.objects.filter(name=name).exists()
+        try:
+            data    = json.loads(request.body)
+            name    = data['name']
+            email   = data['email']
+            phone   = data['phone']
+            password= data['password']
 
-        # 조건들 ()로 묶으면 안되는 이유?
-      
-        if ('@' or '.') not in list(email):
-            return JsonResponse({'ERROR':'email_form'}, status = 401)            
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            decoded_password= hashed_password.decode('utf-8')
 
-        if len(list(pswd))<8:
-            return JsonResponse({'ERROR':'pswd_form'}, status = 401)            
+            if '.' not in email or '@' not in email:
+                return JsonResponse({'ERROR':'email_form'}, status = 401)            
 
-        if db_name is True:
-            return JsonResponse({'ERROR':'name_exist'}, status = 401)            
-        
-        if db_email is True:
-            return JsonResponse({'ERROR':'email_exist'}, status = 401)            
-        
-        if db_phone is True:
-            return JsonResponse({'ERROR':'phone_exist'}, status = 401)            
-        
-        if name or email or phone is None:
-            return JsonResponse({'ERROR':'feild_empty'}, status = 401)            
+            if len(list(password))<8:
+                return JsonResponse({'ERROR':'password_form'}, status = 401)            
 
-        User.objects.create(name=name, email=email, phone=phone, pswd=pswd)
-        return JsonResponse({'SUCCESS':'signup'}, status = 201)
+            if User.objects.filter(name=name).exists():
+                return JsonResponse({'ERROR':'name_exist'}, status = 401)            
+            
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({'ERROR':'email_exist'}, status = 401)            
+            
+            if User.objects.filter(phone=phone).exists():
+                return JsonResponse({'ERROR':'phone_exist'}, status = 401)            
+            
+            else: 
+                User.objects.create(name=name, email=email, phone=phone, password=decoded_password)
+                return JsonResponse({'SUCCESS':'signup'}, status = 201)
+    
+        except KeyError:
+            return JsonResponse({'ERROR': 'KEY_ERROR'}, status=400)            
+        
+        except JSONDecodeError:
+            return JsonResponse({'ERROR': 'JSONDecodeError'}, status=400)        
         
 
 class SignInView(View):
     def post(self, request):
-   
-        data  = json.loads(request.body)
-        id    = data['id']
-        pswd  = data['pswd']
+        try:    
+            data    = json.loads(request.body)
+            email   = data['email']
+            password= data['password']
+            
+            encoded_password = password.encode('utf-8')
+            decoded_password = User.objects.get(email=email).password
 
-        db_email = User.objects.filter(email=id).exists()
-        db_phone = User.objects.filter(phone=id).exists()
-        db_name  = User.objects.filter(name=id).exists()
-        db_pswd  = User.objects.filter(pswd=pswd).exists()
+            if not User.objects.filter(email=email).exists():
+                return JsonResponse({'ERROR':'id_wrong'}, status = 401)   
+            
+            if User.objects.filter(email=email).exists():
+                if  bcrypt.checkpw(encoded_password, decoded_password.encode('utf-8')):
 
-        if db_email or db_phone or db_name is False:
-            return JsonResponse({'ERROR':'id_not_match'}, status = 401)   
+                    token = jwt.encode({'id':email}, SECRET_KEY, ALGORITHM)
+                    # print(type(token),token,"====================================")
+                    pay=jwt.decode(token, SECRET_KEY, ALGORITHM)
+                    # print(pay,'===========================')
+                    return JsonResponse({'SUCCESS':'signin'}, status = 201)
 
-        if id or pswd is None:
-            return JsonResponse({'ERROR':'feild_empty'}, status = 401)  
+                return JsonResponse({'ERROR':'password_wrong'}, status = 401)
 
-        if db_email or db_phone or db_name is True:
-            if db_pswd is False:
-                return JsonResponse({'ERROR':'pswd_not_match'}, status = 401) 
-            return JsonResponse({'SUCCESS':'signin'}, status = 201)
-   
+        except KeyError:
+            return JsonResponse({'ERROR': 'KEY_ERROR'}, status=400)            
+
+        except JSONDecodeError:
+            return JsonResponse({'ERROR': 'JSONDecodeError'}, status=400)            
 
 
-               
 
