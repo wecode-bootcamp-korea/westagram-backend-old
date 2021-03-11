eb6d9c4be8739b5824a7d1a2d17c72a1b534f916
hlrrr <twfvtn@gmail.com>
2021-03-11 19:29:56 +0900

    3/11 review

--- students/18th/team2/HoYeol/user/models.py
+++ students/18th/team2/HoYeol/user/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     email   = models.EmailField(max_length=50, unique=True)
     name    = models.CharField(max_length=50, unique=True)
     phone   = models.CharField(max_length=50, unique=True)
-    password= models.CharField(max_length=500,) 
+    password= models.CharField(max_length=500) 
 
     class Meta:
         db_table='users'
--- students/18th/team2/HoYeol/user/views.py
+++ students/18th/team2/HoYeol/user/views.py
@@ -1,11 +1,10 @@
-# from westagram.settings import AUTH_PASSWORD_VALIDATORS
-# from django.core.exceptions import ViewDoesNotExist
 import json,bcrypt,jwt
 from json.decoder     import JSONDecodeError
-from django.db.models.expressions import Exists
+
 from django.db.models import Q
 from django.views     import View
 from django.http      import JsonResponse
+
 from .models          import User
 from my_settings      import SECRET_KEY, ALGORITHM
 
@@ -19,30 +18,23 @@ class SignUpView(View):
             phone   = data['phone']
             password= data['password']
 
-            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            decoded_password= hashed_password.decode('utf-8')
-
             if '.' not in email or '@' not in email:
-                return JsonResponse({'message':'error_email_form'}, status = 401)            
+                return JsonResponse({'message':'error_email_form'}, status=401)            
 
             if len(password)<8: 
-                return JsonResponse({'message':'error_password_form'}, status = 401)            
+                return JsonResponse({'message':'error_password_form'}, status=401)            
 
-            if User.objects.filter(name=name).exists():
-                return JsonResponse({'message':'error_email_exist'}, status = 401)     
-
-            elif User.objects.filter(email=email).exists():
-                return JsonResponse({'message':'name_exist'}, status = 401)   
-           
-            elif User.objects.filter(phone=phone).exists():
-                return JsonResponse({'message':'phone_exist'}, status = 401)            
+            if User.objects.filter(Q(name=name) | Q(email=email) | Q(phone=phone)).exists:
+                return JsonResponse({'message':'id_exist'}, status = 401)           
+               
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            decoded_password= hashed_password.decode('utf-8')
             
-            else: 
-                User.objects.create(email=email, name=name, phone=phone, password=decoded_password)
-                return JsonResponse({'message':'success_signup'}, status = 201)
-    
+            User.objects.create(email=email, name=name, phone=phone, password=decoded_password)
+            return JsonResponse({'message':'success_signup'}, status = 201)
+
         except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)            
+                return JsonResponse({'message': 'KEY_ERROR'}, status=400)            
         
         except JSONDecodeError:
             return JsonResponse({'message': 'JSONDecodeError'}, status=400)        
@@ -54,68 +46,22 @@ class SignInView(View):
             data    = json.loads(request.body)
             id      = data['id']
             password= data['password']
+            user  = User.objects.get(Q(name=id) | Q(email=id) | Q(phone=id))
+
+            if user is not id:
+                return JsonResponse({'message':'error_id_matching'}, status=401)
+
+            if user is id:
+                decoded_password = user.password
+                encoded_password = password.encode('utf-8')
+                bcrypt.checkpw(encoded_password, decoded_password.encode('utf-8'))
+                token = jwt.encode({'user':user.id}, SECRET_KEY, ALGORITHM)
+                return JsonResponse({'message':'success_signin', 'access_token':token}, status=201)
             
-            id_set    =  User.objects.filter(Q(name=id) | Q(email=id) | Q(phone=id))
-            name_set  =  User.objects.filter(name=id)
-            email_set =  User.objects.filter(email=id)
-            phone_set =  User.objects.filter(phone=id)
+            return JsonResponse({'message':'error_password_matching'}, status = 401)
 
-            # encoded_password = password.encode('utf-8')
-            # decoded_password = User.objects.get(email=email).password
-
-            if not id_set.exists():
-                return JsonResponse({'message':'error_id_matching'}, status = 401)
-
-            else:
-                if name_set.exists():
-                    for name in name_set.iterator():
-                        decoded_password = User.objects.get(name=id).password
-                        encoded_password = password.encode('utf-8')
-
-                        if bcrypt.checkpw(encoded_password, decoded_password.encode('utf-8')):
-                            token = jwt.encode({'name': id}, SECRET_KEY, ALGORITHM)
-                            return JsonResponse({'message':'success_signin', 'access_token':token}, status = 201)
-                        else:
-                             return JsonResponse({'message':'error_password_matching'}, status = 401)
-
-                elif email_set.exists():
-                    for email in email_set.iterator():
-                        decoded_password = User.objects.get(email=id).password
-                        encoded_password = password.encode('utf-8')
-
-                        if bcrypt.checkpw(encoded_password, decoded_password.encode('utf-8')):
-                            token = jwt.encode({'email': id}, SECRET_KEY, ALGORITHM)
-                            return JsonResponse({'message':'success_signin', 'access_token':token}, status = 201)
-                        else:
-                             return JsonResponse({'message':'error_password_matching'}, status = 401)
-
-                elif phone_set.exists():
-                    for phone in phone_set.iterator():
-                        decoded_password = User.objects.get(phone=id).password
-                        encoded_password = password.encode('utf-8')
-
-                        if bcrypt.checkpw(encoded_password, decoded_password.encode('utf-8')):
-                            token = jwt.encode({'phone': id}, SECRET_KEY, ALGORITHM)
-                            return JsonResponse({'message':'success_signin', 'access_token':token}, status = 201)
-                        else:
-                             return JsonResponse({'message':'error_password_matching'}, status = 401)
-
-
-
-
-            # if not User.objects.filter(id=email).exists():
-            #     return JsonResponse({'message':'error_email_matching'}, status = 401)   
-            
-            # if User.objects.filter(email=email).exists():
-            #     if bcrypt.checkpw(encoded_password, decoded_password.encode('utf-8')):
-
-            #         token = jwt.encode({'id':email}, SECRET_KEY, ALGORITHM)
-            #         print(token,"====================================")
-            #         # pay=jwt.decode(token, SECRET_KEY, ALGORITHM)
-            #         # print(pay,'===========================')
-            #         return JsonResponse({'message':'success_signin', 'access_token':token}, status = 201)
-
-            #     return JsonResponse({'message':'error_password_matching'}, status = 401)
+        except User.MultipleObjectsReturned:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=401)
 
         except User.DoesNotExist:
             return JsonResponse({'message': 'KEY_ERROR'}, status=401)            
