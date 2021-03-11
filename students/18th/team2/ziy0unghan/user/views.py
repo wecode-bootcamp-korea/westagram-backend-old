5341893681dee3a1fe22f41469569ac0b2f08362
MacBook Pro <macbookpro@ziy0ung.local>
2021-03-11 16:20:00 +0900

    UPDATE: dict get() / exception

--- students/18th/team2/ziy0unghan/user/views.py
+++ students/18th/team2/ziy0unghan/user/views.py
@@ -9,7 +9,7 @@ from django.db.models import Q
 
 
 from .models     import User
-from my_settings import SECRET_KEY
+from my_settings import SECRET_KEY, ALGORITHM
 
 class SignUpView(View):
     def post(self, request):
@@ -26,8 +26,8 @@ class SignUpView(View):
 
             if '@' not in email or '.' not in email :
                 return JsonResponse({'message':'잘못된 형식입니다. 이메일을 다시한번 확인해 주세요'}, status=400)
-            
-            if User.objects.filter(Q(email=email) | Q(username=username) | Q(phone_number=phone_number)):
+
+            if User.objects.filter(Q(email=email) | Q(username=username) | Q(phone_number=phone_number)).exists():
                 return JsonResponse({'message':'이미 가입되어 있습니다.'}, status=400)
 
             if len(password) < 8:
@@ -35,11 +35,11 @@ class SignUpView(View):
 
             password = data['password'].encode('utf-8')
             password_crypt = bcrypt.hashpw(password, bcrypt.gensalt())
-            User.objects.create(username=username, email=email, password=password_crypt.decode('utf-8'), phone_number=phone_number)
+            User.objects.create(email=email, password=password_crypt.decode('utf-8'), username=username, phone_number=phone_number)
             return JsonResponse({'message':'회원가입 되었습니다.'}, status=200)
 
         except json.JSONDecodeError:
-            return JsonResponse({"message": "JSONDECODE_ERROR"}, status = 400)
+            return JsonResponse({"message": "JSONDECODE_ERROR"}, status=400)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
@@ -48,29 +48,30 @@ class SignInView(View):
     def post(self, request):
 
         try:
-            data          = json.loads(request.body)
-            user_email    = data['email']
-            user_password = data['password']
-            user_name     = data['username']
-            user_phone    = data['phone_number']
+            data         = json.loads(request.body)
+            username     = data.get('username')
+            email        = data.get('email')
+            phone_number = data.get('phone_number')
+            password     = data['password']
 
-            if user_email == '':
-                return JsonResponse({'message':'이메일을 입력해주세요.'}, status=400)
-            
-            if user_password == '':
-                return JsonResponse({'message':'비밀번호를 채워주세요.'}, status=400)
+            user = User.objects.get(Q(email=email) | Q(username=username) | Q(phone_number=phone_number))
+            print(user.email)
 
-            user_account = User.objects.get(email=user_email)
-            if bcrypt.checkpw(user_password.encode('utf-8'), user_account.password.encode('utf-8')):
-                access_token = jwt.encode({'user_id':'user_acount.id'}, 'SECRET', algorithm='HS256')
-                return JsonResponse({'token': access_token, 'message':'SUCCESS'}, status=200)
-            return JsonResponse({'message':'INVALID_USER'}, status=401)
-            
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'message':'INVALID_USER'}, status=401)
+                
+            access_token = jwt.encode({'user_id': user.id}, SECRET_KEY, ALGORITHM)
+
+            return JsonResponse({'token': access_token,'username':username, 'message':'SUCCESS'}, status=200)   
+
         except json.JSONDecodeError:
-            return JsonResponse({"message": "JSONDECODE_ERROR"}, status = 400)
+            return JsonResponse({"message": "JSONDECODE_ERROR"}, status=400)
 
         except User.DoesNotExist:
             return JsonResponse({'message':'INVALID_USER'}, status=401)
+
+        except User.MultipleObjectsReturned:
+            return JsonResponse({'message':'INVALID_USER, get() returned more than one -- it returned 2!'}, status=401)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
--- students/18th/team2/ziy0unghan/westagram/settings.py
+++ students/18th/team2/ziy0unghan/westagram/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from my_settings import SECRET_KEY, DATABASES
+from my_settings import SECRET_KEY, DATABASES, ALGORITHM
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,6 +78,8 @@ WSGI_APPLICATION = 'westagram.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = DATABASES
+
+ALGORITHM = ALGORITHM
 
 
 # Password validation
