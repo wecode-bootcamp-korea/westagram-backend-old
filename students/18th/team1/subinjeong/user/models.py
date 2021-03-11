720619adeaaa1faf2eb2cbabbbb56e5eefa8dd2c
jadyJeong <coldfeet0816@gmail.com>
2021-03-11 13:40:02 +0900

    Update: user.models empty lines cleared up.

--- students/18th/team1/subinjeong/user/models.py
+++ students/18th/team1/subinjeong/user/models.py
@@ -2,17 +2,9 @@ from django.db import models
 
 class User(models.Model):
 
-    username  = models.CharField(max_length=45)
-    email     = models.CharField(max_length=45)
-    password  = models.CharField(max_length=45)
+    username  = models.CharField(max_length=254)
+    email     = models.EmailField(max_length=254)
+    password  = models.CharField(max_length=254)
 
     class Meta(object):
         db_table = "users"
-
-
-        
-        
-        
-
-        
-
