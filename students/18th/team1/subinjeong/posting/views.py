4edf479dda689524ab63f9027a5a5f0be9de45c7
jadyJeong <coldfeet0816@gmail.com>
2021-03-11 13:38:54 +0900

    Add: views for posting app created.

--- students/18th/team1/subinjeong/posting/views.py
+++ students/18th/team1/subinjeong/posting/views.py
@@ -3,17 +3,42 @@ import json
 from django.http  import JsonResponse
 from django.views import View
 
-from .models import *
+from .models     import Posting
+from user.models import User
 
 
-class UploadView(View):
-
+class PostingView(View):
     def post(self,request):   
-        login_data = json.loads(request.body)
-
         try:
-            
+            upload_data = json.loads(request.body)
 
-        except
-            # key error
-            return JsonResponse({'message': 'INVALID_USER'}, status = 401)
+            title       = upload_data['title']
+            img_url     = upload_data['img_url']
+            user_object = User.objects.get(username=username)
+
+            Posting.objects.create(
+                user    = user_object,
+                title   = upload_data['title'],
+                img_url = upload_data['img_url']
+            )
+            return JsonResponse({'message': 'SUCCESS'}, status = 200)
+
+        except:
+            return JsonResponse({'message': 'ERROR'}, status = 400)
+
+    def get(self,request):
+
+        posting_objects = Posting.objects.all()
+        response = []
+
+        for posting_object in posting_objects:
+            dictionarize = (
+                {
+                    'title'          : posting_object.title,
+                    'img_url'        : posting_object.img_url,
+                    'time_created'   : posting_object.time_created,
+                    'username'       : posting_object.user.username
+                }
+            )
+            response.append(dictionarize)
+        return JsonResponse(response, safe=False, status= 200)
--- students/18th/team1/subinjeong/westgram/settings.py
+++ students/18th/team1/subinjeong/westgram/settings.py
@@ -26,9 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
